b32d5a5d3ad9ca465e2e1784610690be11535d5a
Slava Brover <vbrover@gmail.com>
2017-08-01 20:57:12 -0400

    fastme.sh

---
+++ newick2tree.cpp
@@ -0,0 +1,51 @@
+// newick2tree.cpp
+
+#undef NDEBUG
+#include "common.inc"
+
+#include "common.hpp"
+using namespace Common_sp;
+#include "distTree.hpp"
+using namespace DistTree_sp;
+
+
+
+namespace 
+{
+
+
+struct ThisApplication : Application
+{
+	ThisApplication ()
+	: Application ("Print a tree'")
+	{
+	  // Input
+	  addPositional ("input_tree", "Tree in Newick format");
+	}
+
+
+
+	void body () const final
+  {
+		const string input_tree = getArg ("input_tree");    
+
+    const DistTree tree (input_tree);
+    tree. qc ();         
+    tree. saveText (cout);
+	}
+};
+
+
+
+}  // namespace
+
+
+
+int main (int argc, 
+          const char* argv[])
+{
+  ThisApplication app;
+  return app. run (argc, argv);
+}
+
+
