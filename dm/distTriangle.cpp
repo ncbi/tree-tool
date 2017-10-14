b74b7f6e9ff90710a0ed33b8ce98cd3d4ab4cbbd
Slava Brover <vbrover@gmail.com>
2017-10-14 14:30:01 -0400

    distTriangle.cpp is moved to dm/

--- dm/distTriangle.cpp
+++ dm/distTriangle.cpp
@@ -5,9 +5,9 @@
 
 #include "../common.hpp"
 using namespace Common_sp;
-#include "../dm/numeric.hpp"
-#include "../dm/matrix.hpp"
-#include "../dm/dataset.hpp"
+#include "numeric.hpp"
+#include "matrix.hpp"
+#include "dataset.hpp"
 using namespace DM_sp;
 
 
