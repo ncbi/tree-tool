2eaf1a4d67afc6ebb25cfd8d696b0421bce5cd5a
Slava Brover <vbrover@gmail.com>
2017-10-19 17:58:28 -0400

    distTree_inc: last iteration

--- phylogeny/distTree_inc.sh
+++ phylogeny/distTree_inc.sh
@@ -13,16 +13,13 @@ endif
 rm -f $1.log
 while (1)
   set N = `ls $1/new/ | wc -l`
-  echo "# New left: $N[1]  `date`  `date +%s`" >> $1.log  
- #if ($N[1] == 0)  break
+  echo "# To add: $N[1]  `date`  `date +%s`" >> $1.log  
+  if ($N[1] == 0) break
   
   echo ""
   echo ""
-  echo ""
-  echo "Adding total $N[1] leaves ..."
+  echo "# Total objects to add: $N[1] ..."
   distTree_inc_new.sh $1
-  set S = $?
-  if ($S == 2) break
-  if ($S) exit 1
+  if ($?) exit 1
 end
   
