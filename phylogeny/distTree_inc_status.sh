6e81cd8cb6a5f1d3ba45eead7e3088351a9db0bb
Slava Brover <vbrover@gmail.com>
2017-10-19 17:57:07 -0400

    inc/outlier/

--- phylogeny/distTree_inc_status.sh
+++ phylogeny/distTree_inc_status.sh
@@ -7,11 +7,25 @@ if ($# != 1) then
 endif
 
 
+echo "Version: `cat $1/version`"
+echo""
+
 set OBJS = `grep -vc '^ *0x' $1/tree`
 echo "# Objects: $OBJS"  
 
 set N = `ls $1/new/ | wc -l`
 echo "# To add: $N[1]"
+
+set N = `ls $1/search/ | wc -l`
+echo "# Being searched: $N[1]"
+
+set N = `wc -l $1/leaf`
+echo "# Being added: $N[1]"
+
+wc -l $1/outlier
+
+echo ""
+wc -l $1/dissim
 
 echo ""
 grep 'absCriterion =' -n $1/old/makeDistTree.* | sed 's|^'$1'/old/makeDistTree\.||1' | sed 's/:18:/ /1' | grep -v ':19:' | sort -n | head
@@ -23,7 +37,5 @@ head $1.log
 echo "..."
 tail $1.log
 
-echo ""
-wc -l $1/outlier
 
 
