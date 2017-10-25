59fa031f4a677f010ce04e54ee4092a5ea496f17
Slava Brover <vbrover@gmail.com>
2017-10-25 14:42:08 -0400

    parameter change

--- phylogeny/tree2obj.sh
+++ phylogeny/tree2obj.sh
@@ -1,10 +1,10 @@
 #!/bin/csh -f
 
 if ($# != 1) then
-  echo "#1: Distance tree directory"
+  echo "#1: Distance tree"
   exit 1
 endif
 
-grep -v '^ *0x' $1/tree | sed 's/^ *//1' | sed 's/:.*$//1' | sort
+grep -v '^ *0x' $1 | sed 's/^ *//1' | sed 's/:.*$//1' | sort
 if ($?) exit 1
 
