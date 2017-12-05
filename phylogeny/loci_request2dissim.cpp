534d7c734a08851e6655e3f2282ca30bdc2e76a6
Slava Brover <vbrover@gmail.com>
2017-12-05 18:07:14 -0500

    loci_request2dissim.cpp

---
+++ phylogeny/loci_request2dissim.cpp
@@ -0,0 +1,125 @@
+// loci_request2dissim.cpp
+
+#undef NDEBUG
+#include "../common.inc"
+
+#include "../common.hpp"
+using namespace Common_sp;
+
+
+
+namespace 
+{
+
+
+constexpr size_t locus_max = 30000;  // PAR
+typedef array<size_t,locus_max> Loci;  
+
+
+
+Loci getLoci (const string &fName)
+{
+  LineInput f (fName);
+  Loci loci;  
+  loci. fill (0);
+  string locus;
+  size_t n = 0;
+  while (f. nextLine ())
+  {
+    istringstream iss (f. line);
+    size_t locusNum, allele;
+    iss >> locus >> allele;
+    ASSERT (iss. eof ());
+    ASSERT (locus [0] == 'G');
+    locusNum = str2<size_t> (locus. substr (1));
+    ASSERT (locusNum);
+    ASSERT (allele);
+    if (locusNum > locus_max)
+      throw runtime_error ("Too large locus number: " + toString (locusNum));
+    loci [locusNum] = allele;
+    n++;
+  } 
+  if (! n)
+    throw runtime_error ("No loci for " + fName);
+    
+  return loci;
+}
+    
+
+
+
+struct ThisApplication : Application
+{
+  ThisApplication ()
+    : Application ("Compute loci dissimilarities for requested pairs")
+    {
+  	  addPositional ("pairs", "File with pairs of objects");
+  	  addPositional ("loci_dir", "Directory with a file of loci for each object. File line format: G<locus number> <allele number>");
+  	  addPositional ("out", "Output file");
+  	}
+
+
+
+	void body () const
+	{
+		const string pairsFName = getArg  ("pairs");
+		const string loci_dir   = getArg  ("loci_dir");
+		const string out        = getArg  ("out");
+		ASSERT (! loci_dir. empty ());
+		ASSERT (! out. empty ());
+		
+		
+    LineInput input (pairsFName, 100 * 1024, 1000);  // PAR
+    OFStream output (out);
+    ONumber on (output, 6, true);  // PAR
+    while (input. nextLine ())
+    {
+      istringstream iss (input. line);
+      string name1, name2;
+      iss >> name1 >> name2;
+      if (name1 == name2)
+        continue;
+      const Loci loci1 (getLoci (loci_dir + "/" + name1));
+      const Loci loci2 (getLoci (loci_dir + "/" + name2));
+      size_t sameLoci = 0;
+      size_t diffLoci = 0;
+      FOR (size_t, i, locus_max)
+        if (   loci1 [i]
+            && loci2 [i]
+           )
+        {
+          if (loci1 [i] == loci2 [i])
+    	      sameLoci++;
+    	    else
+            diffLoci++;
+        }
+      ASSERT (sameLoci + diffLoci);
+      const double dissim = sameLoci 
+                              ? log ((double) (sameLoci + diffLoci) / (double) sameLoci)
+                              : numeric_limits<double>::infinity ();        
+      ASSERT (dissim >= 0);        
+      output << name1 << '\t' << name2 << '\t' << dissim;
+      if (verbose ())
+        output << '\t' << sameLoci << '\t' << diffLoci;
+      output << endl;
+    }
+	}
+};
+
+
+
+}  // namespace
+
+
+
+
+int main (int argc, 
+          const char* argv[])
+{
+  ThisApplication app;
+  return app. run (argc, argv);
+
+}
+
+
+
