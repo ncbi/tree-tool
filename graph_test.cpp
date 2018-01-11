ae936fe37d5a3b1f2f1ce299a05f45795a5f2117
Slava Brover <vbrover@gmail.com>
2018-01-10 21:06:02 -0500

    graph_test

--- graph_test.cpp
+++ graph_test.cpp
@@ -1,81 +1,100 @@
-// test.cpp
+// graph_test.cpp
 
-#include <iostream>
-using namespace std;
-#include <common.hpp>
-#include <common.inc>
-using namespace common;
+#undef NDEBUG
+#include "common.inc"
+
+#include "common.hpp"
+using namespace Common_sp;
 
 
-class City: public Graph::Node
+
+
+namespace 
+{
+
+
+struct City : DiGraph::Node
 {
   string name;
 
-public:
-  City (const string &name_arg) :
-    Graph::Node (),
-    name (name_arg)
+  City (DiGraph &g,
+        const string &name_arg) 
+    : DiGraph::Node (g)
+    , name (name_arg)
     {}
 
-
-  void printContent () const
-    { cout << name; }
+  void saveContent (ostream &os) const final
+    { os << name; }
 };
 
 
 
-class Distance: public Graph::Edge
+struct Distance : DiGraph::Arc
 {
   int miles;
 
-public:
   Distance (City* start_arg,
             City* end_arg,
-            int miles_arg) :
-    Graph::Edge (start_arg, end_arg),
-    miles (miles_arg)
+            int miles_arg) 
+    : DiGraph::Arc (start_arg, end_arg)
+    , miles (miles_arg)
     {}
 
 
-  void printContent () const
-    { cout << miles; }
+  void saveContent (ostream &os) const final
+    { os << miles; }
 };
 
 
 
-int main (int argc,
-          char* argv[])
+struct ThisApplication : Application
 {
-  Graph g;
-
-  City* london = new City ("London");
-  City* paris = new City ("Paris");
-  City* berlin = new City ("Berlin");
-  City* madrid = new City ("Madrid");
-  g. nodes. push_back (london);
-  g. nodes. push_back (paris);
-  g. nodes. push_back (berlin);
-  g. nodes. push_back (madrid);
-
-  Distance* london_paris = new Distance (london, paris, 100);
-  Distance* london_berlin = new Distance (london, berlin, 200);
-  Distance* london_madrid = new Distance (london, madrid, 50);
-  Distance* berlin_paris = new Distance (berlin, paris, 30);
-  Distance* berlin_paris2 = new Distance (berlin, paris, 35);
-  Distance* berlin_berlin = new Distance (berlin, berlin, 0);
-  Distance* berlin_madrid = new Distance (berlin, madrid, 200);
-
-  g. print ();
-
-  cout << endl;
-  delete berlin_paris;
-  delete berlin_paris2;
-  delete london_paris;
-  delete berlin_berlin;
-
-  g. print ();
-
+	ThisApplication ()
+	: Application ("DiGraph test")
+	{
+	  addPositional ("go", "Go");
+	}
+	
+	
+	
+	void body () const final
+  {
+    DiGraph g;
   
-  return 0;
+    auto london = new City (g, "London");
+    auto paris  = new City (g, "Paris");
+    auto berlin = new City (g, "Berlin");
+    auto madrid = new City (g, "Madrid");
+  
+    auto london_paris  =   new Distance (london, paris, 100);
+  /*auto london_berlin =*/ new Distance (london, berlin, 200);
+  /*auto london_madrid =*/ new Distance (london, madrid, 50);
+    auto berlin_paris  =   new Distance (berlin, paris, 30);
+    auto berlin_paris2 =   new Distance (berlin, paris, 35);
+    auto berlin_berlin =   new Distance (berlin, berlin, 0);
+  /*auto berlin_madrid =*/ new Distance (berlin, madrid, 200);
+  
+    g. print (cout);  
+    cout << endl;
+    
+    delete berlin_paris;
+    delete berlin_paris2;
+    delete london_paris;
+    delete berlin_berlin;
+  
+    g. print (cout);
+  }
+};
+
+
+}  // namespace
+
+
+
+int main (int argc, 
+          const char* argv[])
+{
+  ThisApplication app;
+  return app. run (argc, argv);
 }
 
