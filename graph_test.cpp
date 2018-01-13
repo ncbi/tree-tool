eb8000a16bbcaa717adba664945790bdf13c3035
Slava Brover <vbrover@gmail.com>
2018-01-13 15:31:27 -0500

    delete paris

--- graph_test.cpp
+++ graph_test.cpp
@@ -24,7 +24,7 @@ struct City : DiGraph::Node
     {}
 
   void saveContent (ostream &os) const final
-    { os << name; }
+    { os << ' ' << name; }
 };
 
 
@@ -61,27 +61,30 @@ struct ThisApplication : Application
   {
     DiGraph g;
   
+    // Node's
     auto london = new City (g, "London");
     auto paris  = new City (g, "Paris");
     auto berlin = new City (g, "Berlin");
     auto madrid = new City (g, "Madrid");
-  
+    // Arc's
     auto london_paris  =   new Distance (london, paris, 100);
   /*auto london_berlin =*/ new Distance (london, berlin, 200);
   /*auto london_madrid =*/ new Distance (london, madrid, 50);
     auto berlin_paris  =   new Distance (berlin, paris, 30);
     auto berlin_paris2 =   new Distance (berlin, paris, 35);
     auto berlin_berlin =   new Distance (berlin, berlin, 0);
-  /*auto berlin_madrid =*/ new Distance (berlin, madrid, 200);
-  
+  /*auto berlin_madrid =*/ new Distance (berlin, madrid, 200);  
     g. print (cout);  
     cout << endl;
     
     delete berlin_paris;
     delete berlin_paris2;
     delete london_paris;
-    delete berlin_berlin;
-  
+    delete berlin_berlin;  
+    g. print (cout);
+    cout << endl;
+    
+    delete paris;
     g. print (cout);
   }
 };
