9fea3844b4b0c8549964e9c04c16ea657cecf1f5
Slava Brover <vbrover@gmail.com>
2018-01-24 16:17:38 -0500

    cosmetic

--- phylogeny/bootstrapReport.cpp
+++ phylogeny/bootstrapReport.cpp
@@ -5,8 +5,6 @@
 
 #include "../common.hpp"
 using namespace Common_sp;
-//#include "distTree.hpp"
-//using namespace DistTree_sp;
 
 
 
@@ -20,10 +18,9 @@ struct ThisApplication : Application
 	: Application ("Analyze bootstrap result")
 	{
 		// Input
-	//addPositional ("input_tree", "Tree");
 	  addPositional ("bootstrap", "Bootstrap result: file with lines: <N> match[+-]: <node LCA-name>");
 	  addKey ("replicas", "# replicas (match+ + match-) - for QC", "0");
-	  addKey ("support_min", "Min. match+ / (match+ + match-) to report a node", "0.333");   // =1.0/3.0
+	  addKey ("support_min", "Min. match+ / (match+ + match-) to report a node", "0.3333333");   // =1.0/3.0
 	  addKey ("stable_min", "Min. match+ / (match+ + match-) for a node to be stable", "0.92");   // PAR
 	  addFlag ("print_nodes", "Print support for each interior arc identified by a child node");
 	}
@@ -32,7 +29,6 @@ struct ThisApplication : Application
 
 	void body () const
   {
-  //const string input_tree        = getArg ("input_tree");
 		const string bootstrap         = getArg ("bootstrap");
 		const size_t replicas_expected = str2<size_t> (getArg ("replicas"));
 		const double support_min       = str2<double> (getArg ("support_min"));
@@ -45,12 +41,6 @@ struct ThisApplication : Application
 		ASSERT (stable_min <= 1);
     
 
-  #if 0
-    DistTree tree (input_tree, string (), string (), false);  
-    if (verbose ())
-      tree. qc ();     
-  #endif
-      
     typedef  Vector<size_t>  Support;  // size() = 2
     map <string/*node name*/, Support>  node2support;
     {
@@ -65,23 +55,15 @@ struct ThisApplication : Application
         ASSERT (match == "match+" || match == "match-");
         ASSERT (! name. empty ());
         const bool matchP = (match == "match+");
-      #if 1
         Support& support = node2support [name];
         if (support. empty ())
           support. resize (2, 0);
         ASSERT (! support [matchP]);
         support [matchP] = n;
-      #else
-        const Steiner* node = tree. lcaName2node (name) -> asSteiner ();
-        ASSERT (node);
-        ASSERT (! node->bootstrap [matchP]);
-        const_cast <Steiner*> (node) -> bootstrap [matchP] = n;
-      #endif
       }
     }
     
     
-  #if 1 
     double supportSum = 0;
     size_t stable = 0;
     double varSum = 0;
@@ -91,10 +73,7 @@ struct ThisApplication : Application
  	    const size_t replicas = it. second [0] + it. second [1];
  	    ASSERT (replicas);
  	    if (replicas != replicas_expected)
- 	    {
- 	      cout << it. first << ' ' << replicas << endl;
- 	      ERROR;
- 	    }
+ 	      throw runtime_error (it. first + ": # replicas = " +  toString (replicas) + ", # expected = "  + toString (replicas_expected));
  	    // Probability of a Bernoulli random variable
  	    const double support = (double) it. second [1] / (double) replicas;
  	    if (support >= stable_min)  
@@ -112,19 +91,10 @@ struct ThisApplication : Application
              << endl;
     }
     ASSERT (stable <= n);
-    ONumber on (cout, 3, false);
+    const ONumber on (cout, 3, false);
     cout << "Sum: " << supportSum << endl;
     cout << "Var: " << varSum / (double) n << endl;
     cout << stable_min * 100 << "%-Stable: " << stable << endl;
-  #else
-   	for (const DiGraph::Node* node : tree. nodes)  
-   	  if (const Steiner* st = static_cast <const DTNode*> (node) -> asSteiner ())
-   	  {
-   	    const size_t replicas = st->bootstrap [0] + st->bootstrap [1];
-   	    if (replicas >= replicas_min)
-   	      cout << st->getLcaName () << ' ' << st->bootstrap [0] << ' ' << st->bootstrap [1] << endl;
-   	  }
-  #endif
 	}
 };
 
