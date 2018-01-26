0a9aa3c76bd0445477210706326103d5e19d329c
Slava Brover <vbrover@gmail.com>
2018-01-26 17:56:31 -0500

    /distTree_inc_quality_phen.sh

---
+++ phylogeny/distTree_inc_quality_phen.sh
@@ -0,0 +1,77 @@
+#!/bin/csh -f
+
+if ($# != 5) then
+  echo "Phenotypic quality of an incremental tree"
+  echo "Requires: no adding outliers"
+  echo "#1: incremental tree"
+  echo "#2: start tree version"
+  echo "#3: target tree version (>= #2)"
+  echo "#4: version increment (>= 1)"
+  echo "#5: seed (>= 1)"
+  exit 1
+endif
+
+
+if ($2 > $3)  exit 1
+if ($4 < 1)   exit 1
+if ($5 <= 0)  exit 1
+
+
+set tmp = `mktemp`
+if ($?) exit 1
+echo $tmp
+
+
+tree2obj.sh $1/old/tree.$2 > $tmp.start
+if ($?) exit 1
+tree2obj.sh $1/old/tree.1 > $tmp.init
+if ($?) exit 1
+setMinus $tmp.start $tmp.init > $tmp.start-new
+if ($?) exit 1
+tree2obj.sh $1/tree > $tmp.final
+if ($?) exit 1
+setIntersect.sh $tmp.start-new $tmp.final 0 > $tmp.start-good
+if ($?) exit 1
+
+
+set N = `wc -l $tmp.start-good`
+echo "Tree size: $N[1]"
+@ S = $N[1] / 2
+set S_max = 300  # PAR
+if ($S > $S_max)  set S = $S_max
+echo "Sample size: $S"
+
+setRandOrd $tmp.start-good $5 | head -$S | sort > $tmp.test
+
+
+echo ""
+echo "Complete optimization  ..."
+list2pairs $tmp.test > $tmp.pairs
+if ($?) exit 1
+
+distTree_inc_dissim_request.sh $1 $tmp.pairs $tmp.dissim
+if ($?) exit 1
+
+pairs2attr2 $tmp.dissim 1 cons 6 -distance > $tmp.dm
+if ($?) exit 1
+
+makeDistTree  -data $tmp  -dissim cons  -topology  -output_tree $tmp.tree  -output_feature_tree $tmp.feature_tree1 | grep "# Discernible leaves:"
+if ($?) exit 1
+
+makeFeatureTree  -input_tree $tmp.feature_tree1  -features $1/phen  -output_core $tmp.core  -qual $tmp.qual  
+if ($?) exit 1
+
+
+mkdir $tmp.trees
+if ($?) exit 1
+set i = $2
+set n = 0
+while ($i <= $3)
+	distTree_inc_quality_phen_step.sh $1 $tmp.test $i $tmp
+	if ($?) exit 1
+	@ i = $i + $4
+	@ n = $n + 1
+end
+
+
+rm -fr $tmp.*
---
+++ phylogeny/distTree_inc_quality_phen_step.sh
@@ -0,0 +1,39 @@
+#!/bin/csh -f
+
+if ($# != 4) then
+  echo "Phenotypic quality of an incremental tree"
+  echo "Requires: no adding outliers"
+  echo "#1: incremental tree"
+  echo "#2: test objects"
+  echo "#3: target tree version"
+  echo "#4: temporary file prefix"
+  exit 1
+endif
+
+
+set tmp = $4
+
+
+echo ""
+echo "Target: $3"
+
+tree2obj.sh $1/old/tree.$3 > $tmp.target
+if ($?) exit 1
+
+# QC
+setMinus $2 $tmp.target > $tmp.zero
+if ($?) exit 1
+if (! -z $tmp.zero)  exit 1
+
+setMinus $tmp.target $2 > $tmp.remove
+if ($?) exit 1
+
+makeDistTree  -input_tree $1/old/tree.$3  -remove $tmp.remove  -output_tree $tmp.trees/$3.tree  -output_feature_tree $tmp.feature_tree > /dev/null
+if ($?) exit 1
+
+makeFeatureTree  -input_tree $tmp.feature_tree  -features $1/phen  -output_core $tmp.core  -qual $tmp.qual | grep "Feature agreement:"
+if ($?) exit 1
+
+echo "match+:"
+compareTrees $tmp.tree $tmp.trees/$3.tree -frequency none | grep -c '^match+'
+if ($?) exit 1
