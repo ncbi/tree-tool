c5118a4ca9abc3680ad053d3768d129781a3b5c6
Slava Brover <vbrover@gmail.com>
2018-01-28 18:05:38 -0500

    incremental tree directory: stop, log

--- phylogeny/distTree_inc.sh
+++ phylogeny/distTree_inc.sh
@@ -1,24 +1,26 @@
 #!/bin/csh -f
 
-if ($# != 1) then
+if ($# != 2) then
   echo "Build a distance tree incrementally"
   echo "Update: #1/"
   echo "Output: #1.log"
-  echo "#1: distance tree data"
+  echo "#1: incremental distance tree directory"
+  echo "#2: seed (>=1)"
+  echo 'Time: ??'
   exit 1
 endif
 
 
 
-rm -f $1.log
 while (1)
+  if (-e $1/stop)  exit 2
   set N = `ls $1/new/ | wc -l`
-  echo "# To add: $N[1]  `date`  `date +%s`" >> $1.log  
+  echo "# To add: $N[1]  `date`  `date +%s`" >> $1/log  
   echo ""
   echo ""
   echo "# Total objects to add: $N[1] ..."
   if ($N[1] == 0) break  
-  distTree_inc_new.sh $1
+  distTree_inc_new.sh $1 $2
   if ($?) exit 1
 end
   
