85961e9fc67437fb6a5b3e64f6e4f48778c5f748
Slava Brover <vbrover@gmail.com>
2018-01-28 18:04:54 -0500

    qresub

--- qstat_wait.sh
+++ qstat_wait.sh
@@ -1,7 +1,21 @@
 #!/bin/csh -f
 
+set N = 0
 while (1)
   sleep 10  # PAR
   set Q = `qstat | grep -v '^job-ID' | grep -v '^---' | grep -v '   d[tr]   ' | head -1 | wc -l`
   if ($Q[1] == 0)  break
+  
+  @ N = $N + 1
+  if ($N > 200) then  # PAR
+    set N = 0
+    set L = `qstat | grep -v '^job-ID' | grep -v '^---' | grep -v '   d[tr]   ' | grep '  r  ' | sed 's/^ *//1' | cut -f 1 -d ' '`
+    echo "Re-submitting $#L grid jobs ..."
+    while ($#L)
+      qresub $L[1]
+      qdel $L[1]
+      shift L
+    end
+  endif
 end
+
