5d6b0c63aec2ef13323b98ef7da8c4572accf2a5
Slava Brover <vbrover@gmail.com>
2018-01-29 14:04:19 -0500

    old -> hist

--- phylogeny/distTree_inc_time.sh
+++ phylogeny/distTree_inc_time.sh
@@ -10,16 +10,16 @@ endif
 
 
 
-set Leaves = `grep '^# Leaves: ' $1/old/makeDistTree.$2`
+set Leaves = `grep '^# Leaves: ' $1/hist/makeDistTree.$2`
 if ($?) exit 1
 
-set Dissim = `grep '^# Dissimilarities: ' $1/old/makeDistTree.$2`
+set Dissim = `grep '^# Dissimilarities: ' $1/hist/makeDistTree.$2`
 if ($?) exit 1
 
-set CHRON_topology = `grep '^CHRON: Initial topology: ' $1/old/makeDistTree.$2`
+set CHRON_topology = `grep '^CHRON: Initial topology: ' $1/hist/makeDistTree.$2`
 if ($?) exit 1
 
-set CHRON_Leaves = `grep '^CHRON: Optimizing new leaves: ' $1/old/makeDistTree.$2`
+set CHRON_Leaves = `grep '^CHRON: Optimizing new leaves: ' $1/hist/makeDistTree.$2`
 if ($?) exit 1
 
 
@@ -29,29 +29,29 @@ set CHRON_tree2subgraphDissim = 0
 set CHRON_subgraph = 0
 set CHRON_subgraph2tree = 0
 if ($3) then
-  set L = `grep '^CHRON: getBestChange: ' $1/old/makeDistTree.$2`
+  set L = `grep '^CHRON: getBestChange: ' $1/hist/makeDistTree.$2`
   if ($?) exit 1
   set CHRON_getBestChange = $L[3]
   
-  set L = `grep '^CHRON: tree2subgraph: ' $1/old/makeDistTree.$2`
+  set L = `grep '^CHRON: tree2subgraph: ' $1/hist/makeDistTree.$2`
   if ($?) exit 1
   set CHRON_tree2subgraph = $L[3]
   
-  set L = `grep '^CHRON: tree2subgraphDissim: ' $1/old/makeDistTree.$2`
+  set L = `grep '^CHRON: tree2subgraphDissim: ' $1/hist/makeDistTree.$2`
   if ($?) exit 1
   set CHRON_tree2subgraphDissim = $L[3]
   
-  set L = `grep '^CHRON: subgraphOptimize: ' $1/old/makeDistTree.$2`
+  set L = `grep '^CHRON: subgraphOptimize: ' $1/hist/makeDistTree.$2`
   if ($?) exit 1
   set CHRON_subgraph = $L[3]
   
-  set L = `grep '^CHRON: subgraph2tree: ' $1/old/makeDistTree.$2`
+  set L = `grep '^CHRON: subgraph2tree: ' $1/hist/makeDistTree.$2`
   if ($?) exit 1
   set CHRON_subgraph2tree = $L[3]
 endif
 
 
-set Radius = `grep '^Ave. radius: ' $1/old/makeDistTree.$2`
+set Radius = `grep '^Ave. radius: ' $1/hist/makeDistTree.$2`
 if ($?) exit 1
 
 
