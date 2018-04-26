cbe1ce1a9642255a8ff28ad34a47a17a7d494ae3
Slava Brover <vbrover@gmail.com>
2018-04-26 18:49:09 -0400

    qrls

--- qstat_wait.sh
+++ qstat_wait.sh
@@ -9,13 +9,14 @@ while (1)
   @ N = $N + 1
   if ($N > 200) then  # PAR
     set N = 0
-    set L = `qstat | grep -v '^job-ID' | grep -v '^---' | grep -v '   d[tr]   ' | grep '  r  ' | sed 's/^ *//1' | cut -f 1 -d ' '`
+    set L = `qstat | grep -v '^job-ID' | grep -v '^---' | grep -v '   d[tr]   ' | grep '  [rT]  ' | sed 's/^ *//1' | cut -f 1 -d ' '`
     echo "Re-submitting $#L grid jobs ..."
     while ($#L)
-      qresub $L[1]
-      qdel $L[1]
+      qresub $L[1] -h u
+      if ($? == 0)  qdel -f $L[1]
       shift L
     end
+    qrls  -h u  -u $USER > /dev/null
   endif
 end
 
