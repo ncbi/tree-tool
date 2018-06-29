7cc67c3c3c9b83b3ea0e317d6b61f6c966b5a1a6
Slava Brover <vbrover@gmail.com>
2018-06-29 18:12:04 -0400

    cosmetic

--- phylogeny/distTree_inc_quality_phen_step.sh
+++ phylogeny/distTree_inc_quality_phen_step.sh
@@ -1,12 +1,13 @@
 #!/bin/csh -f
 
-if ($# != 4) then
+if ($# != 5) then
   echo "Phenotypic quality of an incremental tree vs. #4.tree"
   echo "Requires: no adding outliers"
   echo "#1: incremental tree data structure"
   echo "#2: test objects"
   echo "#3: target tree version|'opt'"
   echo "#4: temporary file prefix"
+  echo "#5: Directory for output trees"
   exit 1
 endif
 
@@ -21,8 +22,8 @@ endif
 set tmp = $4
 
 
-if (! -e $tmp.trees/) then
-  echo "Directory $tmp.trees/ does not exist"
+if (! -e $5/) then
+  echo "Directory $5/ does not exist"
   exit 1
 endif
 
@@ -38,17 +39,21 @@ if ($?) exit 1
 # QC
 setMinus $2 $tmp.target > $tmp.zero
 if ($?) exit 1
-if (! -z $tmp.zero)  exit 1
+#if (! -z $tmp.zero)  exit 1
+set N = `wc -l $tmp.zero`
+echo "# Outliers removed: $N[1]"
 
 setMinus $tmp.target $2 > $tmp.remove
 if ($?) exit 1
 
-makeDistTree  -input_tree $INTREE  -remove $tmp.remove  -output_tree $tmp.trees/$3.tree  -output_feature_tree $tmp.feature_tree > /dev/null
+makeDistTree  -input_tree $INTREE  -delete $tmp.remove  -output_tree $5/$3.tree  -output_feature_tree $tmp.feature_tree > $5/$3.makeDistTree
 if ($?) exit 1
 
-makeFeatureTree  -input_tree $tmp.feature_tree  -features $1/phen  -output_core $tmp.core  -qual $tmp.qual | grep "Non-paraphyletic disagreements:"
+makeFeatureTree  -input_tree $tmp.feature_tree  -features $1/phen  -output_core $tmp.core  -qual $5/$3.qual > $5/$3.makeFeatureTree
+if ($?) exit 1
+grep ' !' $5/$3.makeFeatureTree
 if ($?) exit 1
 
 echo "match+:"
-compareTrees $tmp.tree $tmp.trees/$3.tree  -frequency none | grep -c '^match+'
+compareTrees $tmp.tree $5/$3.tree  -frequency none | grep -c '^match+'
 if ($?) exit 1
--- phylogeny/distTree_inc_quality_phen_whole.sh
+++ phylogeny/distTree_inc_quality_phen_whole.sh
@@ -1,11 +1,12 @@
 #!/bin/csh -f
 
-if ($# != 3) then
-  echo "Phenotypic quality of an incremental tree"
+if ($# != 4) then
+  echo "Phenotypic quality of an incremental tree: initial tree"
   echo "Requires: no added outliers"
   echo "#1: incremental tree data structure"
   echo "#2: target tree version"
   echo "#3: version increment (>= 1)"
+  echo "#4: Directory for output trees: #2.tree, #2.makeDistTree, #2.makeFeatureTree, #2.qual"
   exit 1
 endif
 
@@ -26,16 +27,13 @@ tree2obj.sh $1/hist/tree.1 > $tmp.init
 if ($?) exit 1
 
 
-mkdir $tmp.trees
 if ($?) exit 1
 set i = 1
-set n = 0
 while ($i <= $2)
-	distTree_inc_quality_phen_step.sh $1 $tmp.init $i $tmp
+	distTree_inc_quality_phen_step.sh $1 $tmp.init $i $tmp $4
 	if ($?) exit 1
 	@ i = $i + $3
-	@ n = $n + 1
 end
 
 
-rm -fr $tmp.*
+rm -fr $tmp*
