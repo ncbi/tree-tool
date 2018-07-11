e55d6518dc4e230cda3f3d9895b2b7cd3396846f
Slava Brover <vbrover@gmail.com>
2018-07-11 12:54:09 -0400

    new

---
+++ phylogeny/distTree_inc_unhybrid.sh
@@ -0,0 +1,30 @@
+#!/bin/csh -f
+
+if ($# != 2) then
+  echo "Process #1/hybrid"
+  echo "Output: #1/delete-hybrid if #3 exists"
+  echo "#1: incremental distance tree directory"
+  echo "#2: tree version"
+  exit 1
+endif
+
+
+if (-e $1/unhybrid)  exit 1
+
+
+$1/db2unhybrid.sh $1/unhybrid
+if ($?) exit 1
+
+if (-z $1/unhybrid) then
+  rm $1/unhybrid
+else
+  echo ""
+  wc -l $1/unhybrid
+  trav $1/unhybrid "mv $1/outlier/%f $1/new/"
+	if ($?) exit 1
+  $1/objects_in_tree.sh $1/unhybrid null
+	if ($?) exit 1
+	mv $1/unhybrid $1/hist/unhybrid.$2
+	if ($?) exit 1
+endif
+
