8e8a93113a676fe57bcb71c7a4e570098dd82f90
Slava Brover <vbrover@gmail.com>
2018-08-04 15:14:55 -0400

    hybrids

---
+++ phylogeny/hybrid2list.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+set -o posix
+set -o pipefail
+export LC_ALL=C
+
+if [ $# != 1 ]; then
+  echo "Convert hybrid output of makeDistTree into a list and print"
+  echo "#1: hybrid file"
+  exit 1
+fi
+
+
+tmp=`mktemp`
+
+cat $1 | awk '$7 == 1' | cut -f 1 >  $tmp
+cat $1 | awk '$8 == 1' | cut -f 3 >> $tmp
+cat $1 | awk '$9 == 1' | cut -f 4 >> $tmp
+uniq.sh $tmp
+cat $tmp
+
+rm -rf $tmp*
