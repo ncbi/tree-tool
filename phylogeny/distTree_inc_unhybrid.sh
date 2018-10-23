4308ce39e6451302df9d74c72a0ad1351544e2c8
Slava Brover <vbrover@gmail.com>
2018-10-23 19:45:05 -0400

    mv -> rm, cp

--- phylogeny/distTree_inc_unhybrid.sh
+++ phylogeny/distTree_inc_unhybrid.sh
@@ -17,9 +17,10 @@ $1/db2unhybrid.sh $1/unhybrid
 
 if [ -s $1/unhybrid ]; then
   echo ""
+	uniq.sh $1/unhybrid
   wc -l $1/unhybrid
-  # -> search/ ??
-  trav $1/unhybrid "mv $1/outlier/%f $1/new/"
+  trav $1/unhybrid "rm -f $1/outlier/%f"
+  trav $1/unhybrid "cp /dev/null $1/new/%f"
   $1/objects_in_tree.sh $1/unhybrid null
 	mv $1/unhybrid $1/hist/unhybrid.$2
 else
