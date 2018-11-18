c6899ab243383b54ee43517f8d675187dcecda7b
Slava Brover <vbrover@gmail.com>
2018-11-17 20:33:42 -0500

    bash

--- dm/mdsTree.sh
+++ dm/mdsTree.sh
@@ -1,73 +1,70 @@
-#!/bin/csh -f
-
-if ($# != 3) then
+#!/bin/bash
+source bash_common.sh
+if [ $# -ne 3 ]; then
   echo "Hierarchical clustering by MDS. Output directory: #1/"
   echo "#1: Input .dm file without .dm"
   echo "#2: Pairwise measurement attribute in #1"
   echo "#3: mds -attrType"
   exit 1
-endif
+fi
 
 
-set EXEC = ~brovervv/code
+EXEC=~brovervv/code
 
 
-set DEBUG = 0  # or 1 
+DEBUG=0  # or 1 
 
 
-# TmpFNam, Verbose
-if ($DEBUG) then
-  set TmpFNam = tmp
-  set Verbose = 2
+# TMP, VERBOSE
+if [ $DEBUG == 1 ]; then
+  TMP=tmp
+  VERBOSE=2
 else
-  set TmpFNam = `mktemp` 
-  set Verbose = 0
-endif
+  TMP=`mktemp` 
+  VERBOSE=0
+fi
 
-set exitcode = 1
+EXITCODE=1
 
 
 mkdir $1.dir
-if ($?) exit 1
 
-mkdir $TmpFNam.tmp
-if ($?) exit 1
+rm -rf $TMP.tmp
+mkdir $TMP.tmp
 
-cp $1.dm $TmpFNam.tmp/
-if ($?) exit 1
+cp $1.dm $TMP.tmp/
 
-echo "" > $TmpFNam.mds
+echo "" > $TMP.mds
 
-while (1)
+while [ 1 == 1 ]; do
   echo ""
-  set F = `ls $TmpFNam.tmp | head -1`
-  if ($F == "")  break
-  set F = `basename $F .dm`
+  F=`ls $TMP.tmp | head -1`
+  if [ ! "$F" ]; then
+    break
+  fi
+  F=`basename $F .dm`
   echo $F ...
-  set N = `grep -v '^#' $TmpFNam.tmp/$F.dm | head -1`
-  echo "# objects = " $N[2]
-  if ($N[2] <= 5) then  # PAR
-    mv $TmpFNam.tmp/$F.dm $1.dir/
-    if ($?) exit 1
+  set +o errexit
+  N=(`grep -v '^#' $TMP.tmp/$F.dm | head -1`)
+  set -o errexit
+  echo "# objects =  ${N[1]}"
+  if [ ${N[1]} -le 5 ]; then  # PAR
+    mv $TMP.tmp/$F.dm $1.dir/
   else
-    echo "" >> $TmpFNam.mds
-    echo "" >> $TmpFNam.mds
-    set L_old = `ls $TmpFNam.tmp/ | wc -l`
-    $EXEC/cpp/dm/mds -verbose $Verbose  -attrType $3  -maxClusters 6  -clusteringDir $TmpFNam.tmp  -attr $2  $TmpFNam.tmp/$F >> $TmpFNam.mds
-    if ($?) exit 1
-    set L_new = `ls $TmpFNam.tmp/ | wc -l`
-    if ($L_old == $L_new) then
-      mv $TmpFNam.tmp/$F.dm $1.dir/
-      if ($?) exit 1
+    echo "" >> $TMP.mds
+    echo "" >> $TMP.mds
+    L_old=`ls $TMP.tmp/ | wc -l`
+    $EXEC/cpp/dm/mds -verbose $VERBOSE  -attrType $3  -maxClusters 6  -clusteringDir $TMP.tmp  -attr $2  $TMP.tmp/$F >> $TMP.mds
+    L_new=`ls $TMP.tmp/ | wc -l`
+    if [ $L_old == $L_new ]; then
+      mv $TMP.tmp/$F.dm $1.dir/
     else
-      rm $TmpFNam.tmp/$F.dm
-      if ($?) exit 1
-    endif
-  endif
-end
+      rm $TMP.tmp/$F.dm
+    fi
+  fi
+done
 
 
-set exitcode = 0
-quit:
-if (! $DEBUG)  rm -fr $TmpFNam*   
-exit $exitcode
+if [ $DEBUG == 0 ]; then
+  rm -fr $TMP*   
+fi
