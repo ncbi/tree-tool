844383629ff20286ce60b34f54416f137395f0a7
Slava Brover <vbrover@gmail.com>
2018-11-17 20:38:22 -0500

    inc/{contaminated,dissim.bad,genosubspecies_boundary}

---
+++ phylogeny/distTree_inc_search2bad.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+source bash_common.sh
+if [ $# -ne 2 ]; then
+  echo "Remove a new contaminated object in a distance tree data structure"
+  echo "Output (append): #1/hybrid"
+  echo "#1: incremental distance tree directory"
+  echo "#2: New object"
+  exit 1
+fi
+
+
+DIR=$1/search/$2
+
+
+if [ ! -e $DIR/dissim ]; then
+  exit 1
+fi
+
+awk '$3 == "nan"' $DIR/dissim > $DIR/dissim.bad
+
+if [ -s $DIR/dissim.bad ]; then
+  wc -l $DIR/dissim.bad
+  cat $DIR/dissim.bad >> $1/dissim.bad
+  echo $2 >> $1/contaminated
+  distTree_inc_unsearch.sh $1 $2
+else
+	rm $DIR/dissim.bad
+fi
