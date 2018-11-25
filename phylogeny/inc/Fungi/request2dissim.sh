58cc8897099646bfde9080a4590e29bb10068f4c
Slava Brover <vbrover@gmail.com>
2018-11-24 22:23:36 -0500

    inc

--- phylogeny/inc/Fungi/request2dissim.sh
+++ phylogeny/inc/Fungi/request2dissim.sh
@@ -10,8 +10,8 @@ endif
 
 set DIR = /home/brovervv/panfs/GenBank/Fungi
 # was:                                             200
-~/code/genetics/combine_dissims.sh $1 $2 $DIR/hash  10 0.1 $DIR/dissim_scale $DIR/hmm-univ.stat 1 0.57 1  $3
-#                                  1  2  3          4  5   6                 7                  8 9    10 11
+~/code/genetics/combine_dissims.sh $1 $2 $DIR/hash  10 0.1 $DIR/dissim_scale $DIR/hmm-univ.stat 1 0.57 $3
+#                                  1  2  3          4  5   6                 7                  8 9    10
 if ($?) exit 1
 
 
--- phylogeny/inc/bacteria/genogroup2db.sh
+++ phylogeny/inc/bacteria/genogroup2db.sh
@@ -2,7 +2,7 @@
 source bash_common.sh
 if [ $# -ne 1 ]; then
   echo "$0"
-  echo "#1: File genospecies_table"
+  echo "#1: File genogroup_table"
   exit 1
 fi
 
@@ -10,7 +10,7 @@ fi
 loadLISTC $1
 
 sqsh-ms  -S PROTEUS  -D uniColl  -L exit_failcount=1 << EOF | sed 's/|$//1' 
-  EXEC Genospecies2outliers 2 /*PAR*/
+  EXEC Genogroup2outliers 2 /*PAR*/, 1;
   go -m bcp
 EOF
 
