42c2647e9ca5e074c752c5f82207d9f4c3598693
Slava Brover <vbrover@gmail.com>
2018-12-26 17:43:58 -0500

    new

---
+++ phylogeny/tree2_quality_phen.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+source bash_common.sh
+if [ $# -ne 3 ]; then
+  echo "Phenotypic quality comparison of 2 distance trees"
+  echo "#1: distance tree 1"
+  echo "#2: distance tree 2"
+  echo "#3: phen/"
+  exit 1
+fi
+T1=$1
+T2=$2
+PHEN=$3
+
+
+TMP=`mktemp`
+#echo $TMP
+
+
+tree2obj.sh $T1 > $TMP.list1
+tree2obj.sh $T2 > $TMP.list2
+
+wc -l $TMP.list1
+wc -l $TMP.list2
+
+setMinus $TMP.list1 $TMP.list2 > $TMP.list1-del
+setMinus $TMP.list2 $TMP.list1 > $TMP.list2-del
+
+makeDistTree -input_tree $T1  -delete $TMP.list1-del  -check_delete  -output_feature_tree $TMP.feature_tree1 > /dev/null
+makeDistTree -input_tree $T2  -delete $TMP.list2-del  -check_delete  -output_feature_tree $TMP.feature_tree2 > /dev/null
+
+echo ""
+makeFeatureTree -input_tree $TMP.feature_tree1  -features $PHEN  -prefer_gain  -output_core $TMP.core1  -qual $TMP.qual1  -gain_nodes $TMP.gain_nodes1  -disagreement_nodes $TMP.disagreement_nodes1
+echo ""
+makeFeatureTree -input_tree $TMP.feature_tree2  -features $PHEN  -prefer_gain  -output_core $TMP.core2  -qual $TMP.qual2  -gain_nodes $TMP.gain_nodes2  -disagreement_nodes $TMP.disagreement_nodes2
+
+
+rm -f $TMP*
