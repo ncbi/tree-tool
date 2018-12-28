437caa12ffb509feda1f6f4925c7c738327c91ae
Slava Brover <vbrover@gmail.com>
2018-12-28 16:26:11 -0500

    bash

--- phylogeny/distTree_inc_quality_phen_whole.sh
+++ phylogeny/distTree_inc_quality_phen_whole.sh
@@ -1,6 +1,6 @@
-#!/bin/csh -f
-
-if ($# != 4) then
+#!/bin/bash
+source bash_common.sh
+if [ $# -ne 4 ]; then
   echo "Phenotypic quality of an incremental tree: initial tree"
   echo "Requires: no added outliers"
   echo "#1: incremental tree data structure"
@@ -8,32 +8,32 @@ if ($# != 4) then
   echo "#3: version increment (>= 1)"
   echo "#4: Directory for output trees: #2.tree, #2.makeDistTree, #2.makeFeatureTree, #2.qual"
   exit 1
-endif
+fi
 
 
-if ($2 < 1)  exit 1
-if ($3 < 1)  exit 1
+if [ $2 < 1 ]; then
+  exit 1
+fi
+if [ $3 < 1 ]; then  
+  exit 1
+fi
 
 
-set tmp = `mktemp`
-if ($?) exit 1
-echo $tmp  # ??
+TMP=`mktemp`
+#echo $TMP  
 
 
-cp $1/hist/tree.1 $tmp.tree
-if ($?) exit 1
+cp $1/hist/tree.1 $TMP.tree
 
-tree2obj.sh $1/hist/tree.1 > $tmp.init
-if ($?) exit 1
+tree2obj.sh $1/hist/tree.1 > $TMP.init
 
 
-if ($?) exit 1
-set i = 1
-while ($i <= $2)
-	distTree_inc_quality_phen_step.sh $1 $tmp.init $i $tmp $4
-	if ($?) exit 1
-	@ i = $i + $3
-end
+i=1
+while [ $i <= $2 ]
+do
+	distTree_inc_quality_phen_step.sh $1 $TMP.init $i $TMP $4
+	i = $(( $i + $3 ))
+done
 
 
-rm -fr $tmp*
+rm -fr $TMP*
