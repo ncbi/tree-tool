2d62760add9b3a7f226af6e8ab14f40711bf35ce
Slava Brover <vbrover@gmail.com>
2018-12-28 16:27:15 -0500

    bash

--- phylogeny/feature_qual_nomin2stat.sh
+++ phylogeny/feature_qual_nomin2stat.sh
@@ -1,8 +1,8 @@
-#!/bin/csh -f
-
-if ($# != 1) then
+#!/bin/bash
+source bash_common.sh
+if [ $# != 1 ]; then
   echo "#1: qual-file of makeFeatureTree"
   exit 1
-endif
+fi
 
 grep ':' $1 | grep -v " +1 -0 /" | sed 's/:.*$//1' | sort | uniq -c
