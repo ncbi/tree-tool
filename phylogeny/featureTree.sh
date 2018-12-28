8a917b15271b28c1580119110cb454565c9e8020
Slava Brover <vbrover@gmail.com>
2018-12-28 16:27:01 -0500

    bash

--- phylogeny/featureTree.sh
+++ phylogeny/featureTree.sh
@@ -1,38 +1,33 @@
-#!/bin/csh -f
-
-if ($# != 2) then
+#!/bin/bash
+source bash_common.sh
+if [ $# != 2 ]; then
   echo "Output: #1.{tree,core} - feature tree"
   echo "#1: #1.list - list of genomes"
   echo "#2: directory with features of genomes"
   exit 1
-endif
+fi
 
 
 
 feature2dissim $1.list $2 > $1.dm
-if ($?) exit 1
 
 distTree.sh $1 cons 
-if ($?) exit 1 
 rm $1.dm
 rm $1.makeDistTree
 
 # Gain/loss tree
 makeDistTree  -input_tree $1.tree  -output_feature_tree $1-init.tree  
-if ($?) exit 1 
 rm $1.tree 
 
 echo ""
 echo ""
 makeFeatureTree  -input_tree $1-init.tree  -features $2  -output_tree $1-maxParsimony.tree  -output_core $1-maxParsimony.core  -optim_iter_max 100
-if ($?) exit 1 
 rm $1-init.tree
 
 # MLE
 echo ""
 echo ""
 makeFeatureTree  -input_tree $1-maxParsimony.tree  -features $2  -input_core $1-maxParsimony.core  -output_tree $1.tree  -output_core $1.core  -optim_iter_max 100  -use_time
-if ($?) exit 1 
 rm $1-maxParsimony.tree 
 rm $1-maxParsimony.core
 
