af7faa006d1670e358519c2ecf0ef817c7d455fa
Slava Brover <vbrover@gmail.com>
2019-01-03 21:03:45 -0500

    #4: list of objects to intersect with

--- phylogeny/tree2_quality_phen.sh
+++ phylogeny/tree2_quality_phen.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 source bash_common.sh
-if [ $# -ne 3 ]; then
+if [ $# -ne 4 ]; then
   echo "Phenotypic quality comparison of 2 distance trees"
   echo "#1: distance tree 1"
   echo "#2: distance tree 2"
   echo "#3: phen/"
+  echo "#4: list of objects to intersect with (sorted) | ''"
   exit 1
 fi
 T1=$1
 T2=$2
 PHEN=$3
+TARGET="$4"
 
 
 TMP=`mktemp`
-#echo $TMP  
+echo $TMP  
 
 
 tree2obj.sh $T1 > $TMP.list1
@@ -22,8 +24,34 @@ tree2obj.sh $T2 > $TMP.list2
 wc -l $TMP.list1
 wc -l $TMP.list2
 
-setMinus $TMP.list1 $TMP.list2 > $TMP.list1-del
-setMinus $TMP.list2 $TMP.list1 > $TMP.list2-del
+if [ "$TARGET" ]; then
+  setIntersect.sh $TMP.list1 $TARGET 0 > $TMP.list1-good 
+  setIntersect.sh $TMP.list2 $TARGET 0 > $TMP.list2-good 
+else
+  cp $TMP.list1 $TMP.list1-good
+  cp $TMP.list2 $TMP.list2-good
+fi
+
+N=`setIntersect.sh $TMP.list1-good $TMP.list2-good 0 | wc -l`
+if [ $N -le 2 ]; then
+  echo "Intersection = $N"
+  exit 0
+fi
+
+N=`cat $TMP.list1-good | wc -l`
+if [ $N -le 2 ]; then
+  wc -l $TMP.list1-good
+  exit 0
+fi
+
+N=`cat $TMP.list2-good | wc -l`
+if [ $N -le 2 ]; then
+  wc -l $TMP.list2-good
+  exit 0
+fi
+
+setMinus $TMP.list1 $TMP.list2-good > $TMP.list1-del
+setMinus $TMP.list2 $TMP.list1-good > $TMP.list2-del
 
 makeDistTree -input_tree $T1  -delete $TMP.list1-del  -check_delete  -output_tree $TMP.tree1  -output_feature_tree $TMP.feature_tree1 > $TMP.makeDistTree1
 makeDistTree -input_tree $T2  -delete $TMP.list2-del  -check_delete  -output_tree $TMP.tree2  -output_feature_tree $TMP.feature_tree2 > $TMP.makeDistTree2
