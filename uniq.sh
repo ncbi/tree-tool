4759a87af986d1a062412a5fb0b41ec177b7819c
Slava Brover <vbrover@gmail.com>
2019-02-11 12:38:01 -0500

    bash

--- uniq.sh
+++ uniq.sh
@@ -1,12 +1,12 @@
-#!/bin/csh -f
-
-if ($# != 1) then
+#!/bin/bash
+EXEC=~brovervv/code/cpp
+source $EXEC/bash_common.sh
+if [ $# != 1 ]; then
   echo "#1: File to sort and uniq"
   exit 1
-endif
+fi
 
-set TmpFNam = `mktemp`
-cat $1 | tr '\t' ' ' | sed 's/ *$//1' | sort | uniq > $TmpFNam
-if ($?) exit 1
-mv $TmpFNam $1
-if ($?) exit 1
+TMP=`mktemp`
+cat $1 | tr '\t' ' ' | sed 's/ *$//1' | sed 's/  / /g' | sort | uniq | tr ' ' '\t' > $TMP
+mv $TMP $1
+
