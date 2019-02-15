bea181b47fa1f8338e79d3960ef915f93d9f198a
Slava Brover <vbrover@gmail.com>
2019-02-15 14:58:36 -0500

    QSUB

--- bash_common.sh
+++ bash_common.sh
@@ -3,3 +3,6 @@ set -o errexit
 set -o posix
 set -o pipefail
 export LC_ALL=C
+
+QSUB_="qsub  -r n  -cwd  -V  -P unified  -j y  -v SGE_NOMAIL  -v SGE_SUMMARY=/dev/null  -b y   -o /dev/null  -l h_vmem=36G,mem_free=500M,m_mem_free=500M,reserve_mem=500M"
+export QSUB_5="$QSUB_,h_rt=36000" 
