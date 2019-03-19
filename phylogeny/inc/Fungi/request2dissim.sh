e6a0c8b00417660fb91c56fa5b9009a2e08397bf
Slava Brover <vbrover@gmail.com>
2019-03-19 15:54:23 -0400

    inc/Fungi

--- phylogeny/inc/Fungi/request2dissim.sh
+++ phylogeny/inc/Fungi/request2dissim.sh
@@ -1,18 +1,23 @@
-#!/bin/csh -f
-
-if ($# != 3) then
+#!/bin/bash
+THIS=`dirname $0`
+source bash_common.sh
+if [ $# -ne 3 ]; then
   echo "Compute dissimilarities"
   echo "#1: file with pairs <Object1> <Object2>"
   echo "#2: Output file"
   echo "#3: log (temporary)"
   exit 1
-endif
+fi
+REQ=$1
+OUT=$2
+LOG=$3
 
-set DIR = /home/brovervv/panfs/GenBank/Fungi
+
+DIR=/home/brovervv/panfs/GenBank/Fungi
 # was:                                             200
-~/code/genetics/combine_dissims.sh $1 $2 $DIR/hash  10 0.1 $DIR/dissim_scale $DIR/hmm-univ.stat 1 0.57 $3
-#                                  1  2  3          4  5   6                 7                  8 9    10
-if ($?) exit 1
+~/code/genetics/combine_dissims.sh $REQ $OUT $DIR/hash  10 0.1 $THIS/dissim_scale $THIS/hmm-univ.stat 1 0.57 $LOG
+#                                  1    2    3          4  5   6                  7                   8 9    10
+
 
 
 
