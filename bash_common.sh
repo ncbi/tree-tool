959d6c3c13c8d005c150aa651e0d39bf30cd9584
Slava Brover <vbrover@gmail.com>
2019-04-18 13:10:05 -0400

    QSUB

--- bash_common.sh
+++ bash_common.sh
@@ -4,5 +4,6 @@ set -o posix
 set -o pipefail
 export LC_ALL=C
 
-QSUB_="qsub  -r n  -cwd  -V  -P unified  -j y  -v SGE_NOMAIL  -v SGE_SUMMARY=/dev/null  -b y   -o /dev/null  -l h_vmem=36G,mem_free=500M,m_mem_free=500M,reserve_mem=500M"
-export QSUB_5="$QSUB_,h_rt=36000" 
+QSUB_="qsub  -r n  -cwd  -V  -P unified  -j y  -v SGE_NOMAIL  -v SGE_SUMMARY=/dev/null  -b y   -o /dev/null"
+export QSUB_5="$QSUB_  -l h_vmem=36G,mem_free=500M,m_mem_free=500M,reserve_mem=500M,h_rt=36000" 
+export QSUB_L="$QSUB_  -l h_vmem=36G,mem_free=1G,m_mem_free=1G,reserve_mem=1G,h_rt=72000" 
