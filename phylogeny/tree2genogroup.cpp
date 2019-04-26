5e3868b05dc44b1fb71a892574867d80ac42a8b8
Slava Brover <vbrover@gmail.com>
2019-04-26 14:35:57 -0400

    cosmetic

--- phylogeny/tree2genogroup.cpp
+++ phylogeny/tree2genogroup.cpp
@@ -20,21 +20,21 @@ struct Genogroup
   VectorPtr<Tree::TreeNode> leaves;
     // Distinct
   const Steiner* lca {nullptr};
-  const Leaf* repr {nullptr};
+  const Leaf* leader {nullptr};
   
   Genogroup () = default;
     
   void finish ()
     { ASSERT (! leaves. empty ());
       if (leaves. size () == 1)
-        repr = static_cast <const DTNode*> (leaves [0]) -> asLeaf ();
+        leader = static_cast <const DTNode*> (leaves [0]) -> asLeaf ();
       else
       { Tree::LcaBuffer buf;
         const Tree::TreeNode* lca_ = Tree::getLca (leaves, buf);
         EXEC_ASSERT (lca = static_cast <const DTNode*> (lca_) -> asSteiner ());
-        EXEC_ASSERT (repr = static_cast <const DTNode*> (lca->getFirstDecendant ()) -> asLeaf ());
+        EXEC_ASSERT (leader = static_cast <const DTNode*> (lca->getFirstDecendant ()) -> asLeaf ());
       }
-      ASSERT (repr);
+      ASSERT (leader);
     }
 };
 
@@ -47,9 +47,9 @@ struct ThisApplication : Application
 		{
 		  addPositional ("input_tree", "Tree file");
 		  addPositional ("genogroup_dist", "Max. distance between objects of the same genogroup");
-		  addKey ("genogroup_table", "File with lines: <object> <genogroup>, where <genogroup> is a genogroup representative object");
+		  addKey ("genogroup_table", "File with lines: <object> <genogroup leader>");
 		  addKey ("genogroups", "File with the names of the interior nodes which are genogroup roots");
-		  addKey ("genogroup_under_genogroup", "File with lines: <node1 LCA name> <node2 LCA name>, where nodes belong to different genogroup, but node1 is a child of node2");
+		  addKey ("genogroup_under_genogroup", "File with lines: <node1 LCA name> <node2 LCA name>, where nodes belong to different genogroups, but node1 is a child of node2");
 		}
 
 
@@ -66,7 +66,7 @@ struct ThisApplication : Application
 
     
     DistTree tree (input_tree, string (), string (), string());
-    tree. sort ();  // For Genogroup::repr
+    tree. sort ();  // For Genogroup::leader
     tree. qc ();    
       
     tree. findGenogroups (genogroup_dist);
@@ -95,11 +95,10 @@ struct ThisApplication : Application
 	  if (! genogroup_table. empty ())
 	  {
 	  	OFStream f (genogroup_table);
-		 	for (DiGraph::Node* node_ : tree. nodes)
+		 	for (const auto& it : tree. name2leaf)
 		 	{
-		 		const DTNode* node = static_cast <const DTNode*> (node_);
-		 		if (const Leaf* leaf = node->asLeaf ())
-		 			f << leaf->name << '\t' << genogroups [node_->getDisjointCluster ()]. repr->name << endl;
+		 		const Leaf* leaf = it. second;
+	 		  f << leaf->name << '\t' << genogroups [var_cast (leaf) -> getDisjointCluster ()]. leader->name << endl;
 		 	}
 	  }
 	      
