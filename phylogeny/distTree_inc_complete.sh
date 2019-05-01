83ad3ea2f36797a26c35eda1a679bbf1c746f070
Slava Brover <vbrover@gmail.com>
2019-05-01 15:40:22 -0400

    distTriangle -hybrid

--- phylogeny/distTree_inc_complete.sh
+++ phylogeny/distTree_inc_complete.sh
@@ -6,7 +6,7 @@ if [ $# -ne 3 ]; then
   echo "#1: Incremental distance tree directory"
   echo "#2: List of objects"
   echo "#3: Update database (0/1)"
-  echo "Output: #1/, data.dm"
+  echo "Output: #1/, data.dm (without hybrids)"
   exit 1
 fi
 INC=$1
@@ -32,6 +32,11 @@ if [ $N -gt 0 ]; then
   exit 1
 fi
 
+if [ -s $INC/outlier-alien ]; then
+  echo "$INC/outlier-alien must be empty"
+  exit 1
+fi
+
 
 $THIS/../sort.sh $OBJS
 
@@ -45,31 +50,43 @@ echo ""
 echo "data.dm ..."
 $THIS/../dm/pairs2attr2 $INC/dissim 1 cons 6 -distance > data.dm
 
-# distTriangle
-cat data.dm | sed 's/nan/inf/g' > $INC/data1.dm
-mkdir $INC/clust
-$THIS/../dm/distTriangle $INC/data1 cons  -clustering_dir $INC/clust
-rm $INC/data1.dm
-N=`ls $INC/clust/ | wc -l`
-if [ $N -gt 1 ]; then
-  echo "# Clusters: $N"
-  exit 1
-fi
-$THIS/../dm/dm2objs $INC/clust/1/data1 | sort > $INC/tree.list
-mv $INC/clust/1/data1.dm data.dm
-$THIS/../setMinus $OBJS $INC/tree.list > $INC/outlier-triangle
-rm -r $INC/clust/
-wc -l $INC/outlier-triangle
+$THIS/../dm/dm2objs data | sort > $INC/tree.list
+$THIS/../setMinus $OBJS $INC/tree.list > $INC/outlier-alien
 if [ $DB == 1 ]; then
-  $THIS/../trav $INC/outlier-triangle "$INC/outlier2db.sh %f triangle"
+  $THIS/../trav $INC/outlier-alien "$INC/outlier2db.sh %f alien"
 fi
-rm $INC/outlier-triangle
+
+
+HYBRIDNESS_MIN=`cat $INC/hybridness_min`
+if [ $HYBRIDNESS_MIN != 0 ]; then
+  echo ""
+  echo "distTriangle ..."
+  cat data.dm | sed 's/nan/inf/g' > $INC/data1.dm
+  mkdir $INC/clust
+  $THIS/../dm/distTriangle $INC/data1 cons  -clustering_dir $INC/clust  -hybridness_min $HYBRIDNESS_MIN  -hybrid $INC/hybrid.new
+  rm $INC/data1.dm
+  N=`ls $INC/clust/ | wc -l`
+  if [ $N -gt 1 ]; then
+    echo "# Clusters: $N"
+    exit 1
+  fi
+  mv $INC/clust/1/data1.dm data.dm
+  rm -r $INC/clust/
+  $THIS/../dm/dm2objs data | sort > $INC/tree.list  
+  if [ $DB == 1 ]; then
+    echo ""
+    echo "Hybrid ..."
+  	$THIS/distTree_inc_hybrid.sh $INC 
+    echo "Unhybrid ..."
+    $THIS/distTree_inc_unhybrid.sh $INC 
+  fi
+fi
+
 
 echo ""
 echo "Tree ..."
 HYBRID=""
-HYBRIDNESS_MIN=`cat $INC/hybridness_min`
-if [ "$HYBRIDNESS_MIN" != 0 ]; then
+if [ $HYBRIDNESS_MIN != 0 ]; then
   DISSIM_BOUNDARY=`cat $INC/dissim_boundary`
 	HYBRID="-hybrid_parent_pairs $INC/hybrid_parent_pairs  -delete_hybrids $INC/hybrid.new  -hybridness_min $HYBRIDNESS_MIN  -dissim_boundary $DISSIM_BOUNDARY"
 fi
@@ -80,7 +97,7 @@ if [ $DB == 1 ]; then
   echo ""
   echo "Database ..."
   $INC/objects_in_tree.sh $INC/tree.list 1
-  if [ "$HYBRIDNESS_MIN" != 0 ]; then
+  if [ $HYBRIDNESS_MIN != 0 ]; then
     echo ""
     echo "Hybrid ..."
   	$THIS/distTree_inc_hybrid.sh $INC
