942b9f01af595e0539b8e852b8cc326d2127abfd
Slava Brover <vbrover@gmail.com>
2019-05-03 10:36:12 -0400

    QSUB_L has 100 hours

--- bash_common.sh
+++ bash_common.sh
@@ -6,4 +6,4 @@ export LC_ALL=C
 
 QSUB_="qsub  -r n  -cwd  -V  -P unified  -j y  -v SGE_NOMAIL  -v SGE_SUMMARY=/dev/null  -b y   -o /dev/null"
 export QSUB_5="$QSUB_  -l h_vmem=36G,mem_free=500M,m_mem_free=500M,reserve_mem=500M,h_rt=36000" 
-export QSUB_L="$QSUB_  -l h_vmem=36G,mem_free=1G,m_mem_free=1G,reserve_mem=1G,h_rt=72000" 
+export QSUB_L="$QSUB_  -l h_vmem=36G,mem_free=1G,m_mem_free=1G,reserve_mem=1G,h_rt=360000" 
