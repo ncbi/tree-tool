33f4305866c1e03be8b466fe4f370955305145a3
Slava Brover <vbrover@gmail.com>
2019-05-08 12:38:22 -0400

    cosmetic

--- phylogeny/distTree_inc.sh
+++ phylogeny/distTree_inc.sh
@@ -4,7 +4,7 @@ source $THIS/../bash_common.sh
 if [ $# -ne 2 ]; then
   echo "Build a distance tree incrementally"
   echo "Update: #1/"
-  echo "Output: leaf_errors.{dm,txt}, tree.<DATE>, disagreement_nodes[.txt], disagreement_objects, gain_nodes, qual, genogroups, gug"
+  echo "Output: leaf_errors.{dm,txt}, tree.<DATE>, disagreement_nodes[.txt], disagreement_objects, gain_nodes, qual"
   echo "#1: incremental distance tree directory"
   echo "#2: new.list | ''"
   echo "Time: O(n log^4(n))"
