a705162614ed9add07e3690090e3c78be0defc19
Slava Brover <vbrover@gmail.com>
2019-05-12 13:55:08 -0400

    new

---
+++ rm_suff.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+THIS=`dirname $0`
+source $THIS/bash_common.sh
+if [ $# -ne 2 ]; then
+  echo "Remove a suffix from a file name"
+  echo "#1: File name"
+  echo "#2: File name suffix"
+  exit 1
+fi
+FILE=$1
+SUF=$2
+
+
+DIR=`dirname $FILE`
+OUT=`basename $FILE $SUF`
+mv $FILE $DIR/$OUT
