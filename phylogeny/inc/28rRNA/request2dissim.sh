48d183fce7845c5614ea5800d810e9d95f2961df
Slava Brover <vbrover@gmail.com>
2019-05-12 20:09:02 -0400

    new

---
+++ phylogeny/inc/28rRNA/request2dissim.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source bash_common.sh
+if [ $# -ne 3 ]; then
+  echo "$0"
+  echo "#1: dissimilaruty requests (input)"
+  echo "#2: dissim (output)"
+  echo "#3: log"
+  exit 1
+fi
+REQUEST=$1
+DISSIM=$2
+LOG=$3
+
+dna_pairs2dissim  -log $LOG  -coeff 0.00634  $REQUEST /home/brovervv/panfs/marker/Fungi/28SrRNA/seq 300 $DISSIM
+rm -f $LOG
+
