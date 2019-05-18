b6a5bf06e27720c0519aa724e1b978f61fdb870f
Slava Brover <vbrover@gmail.com>
2019-05-18 11:26:49 -0400

    #1-out.dm is created in the current directory

--- phylogeny/calibrateDissims_univ.sh
+++ phylogeny/calibrateDissims_univ.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 THIS=`dirname $0`
 source $THIS/../bash_common.sh
-if [ $# -ne 6 ]; then
-  echo "Output: #1-out.dm, hmm-univ.stat"
-  echo "#1: Input .dm-file without '.dm'"
+if [ $# -ne 7 ]; then
+  echo "Output: hmm-univ.stat"
+  echo "#1: input .dm-file without '.dm'"
   echo "#2: delete hybrids (0/1)"
-  echo "#3: power"
+  echo "#3: dissimilarity power (> 0)"
   echo "#4: outlierSEs"
-  echo "#5: dissim_coeff: 0 - variance = lin, else linVar"
-  echo "#6: phen/"
+  echo "#5: dissim_coeff: >0 <=> variance linExp"
+  echo "#6: variance power (NAN <=> variance = linExp)"
+  echo "#7: phen/"
   exit 1
 fi
 INPUT=$1
 DELETE_HYBRIDS=$2
-POWER=$3
+DISSIM_POWER=$3
 OUTLIER_SES=$4
 DISSIM_COEFF=$5
-PHEN=$6
+VAR_POWER=$6
+PHEN=$7
 
 
 echo ""
-echo "power = $POWER"
+echo "power = $DISSIM_POWER"
 echo "outlier_SEs = $OUTLIER_SES"
 
 
@@ -28,9 +30,9 @@ TMP=`mktemp`
 #echo $TMP 
 
 
-$THIS/../dm/positiveAverage $INPUT $POWER $OUTLIER_SES  -output_dissim $TMP.pairs > hmm-univ.stat
+$THIS/../dm/positiveAverage $INPUT $DISSIM_POWER $OUTLIER_SES  -output_dissim $TMP.pairs > hmm-univ.stat
 tail -n +5 $TMP.pairs.dm | sed 's/-/ /1' > $TMP.pairs
-$THIS/../dm/pairs2attr2 $TMP.pairs 1 cons 6  -distance > $INPUT-out.dm
+$THIS/../dm/pairs2attr2 $TMP.pairs 1 cons 6  -distance > $TMP.dm
 
 
 echo ""
@@ -44,20 +46,17 @@ fi
 VARIANCE=linExp
 DISSIM_COEFF_OPTION="-dissim_coeff $DISSIM_COEFF"
 if [ $DISSIM_COEFF == 0 ]; then
-  VARIANCE=lin
+  VARIANCE="pow  -variance_power $VAR_POWER"
   DISSIM_COEFF_OPTION=""
 fi
 
-
-$THIS/../dm/dm2objs $INPUT-out > $TMP.list
+$THIS/../dm/dm2objs $TMP > $TMP.list
 ls $PHEN > $TMP.phen
 $THIS/../setMinus $TMP.list $TMP.phen > $TMP.delete
 
-
 echo ""
 echo ""
-$THIS/makeDistTree  -threads 5  -data $INPUT-out  -dissim_attr cons  -variance $VARIANCE  $DISSIM_COEFF_OPTION  -delete $TMP.delete  -optimize  -subgraph_iter_max 20  $HYBRID  -noqual  -output_feature_tree $TMP.feature_tree
-
+$THIS/makeDistTree  -threads 5  -data $TMP  -dissim_attr cons  -variance $VARIANCE  $DISSIM_COEFF_OPTION  -delete $TMP.delete  -optimize  -subgraph_iter_max 20  $HYBRID  -noqual  -output_feature_tree $TMP.feature_tree
 
 echo ""
 echo ""
