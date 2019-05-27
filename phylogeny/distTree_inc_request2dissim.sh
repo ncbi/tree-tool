f35bb8a23325c66fb50a0954594b00093b2450e3
Slava Brover <vbrover@gmail.com>
2019-05-27 18:43:52 -0400

    $1 --> $INC

--- phylogeny/distTree_inc_request2dissim.sh
+++ phylogeny/distTree_inc_request2dissim.sh
@@ -28,7 +28,7 @@ else
 		
 		ls $INC/dr > $INC/dr.list
 		while [ -s $INC/dr.list ]; do
-  		$THIS/../trav -step 1 $INC/dr.list "$QSUB_5 -N j%f %q$1/request2dissim.sh $INC/dr/%f $INC/dr.out/%f $INC/dr.out/%f.log%q > /dev/null"		
+  		$THIS/../trav -step 1 $INC/dr.list "$QSUB_5 -N j%f %q$INC/request2dissim.sh $INC/dr/%f $INC/dr.out/%f $INC/dr.out/%f.log%q > /dev/null"		
   		$THIS/../qstat_wait.sh 2000 1	
  			set +o errexit	
 		  ls $INC/dr.out/*.log  1> $INC/dr.log  2> /dev/null
