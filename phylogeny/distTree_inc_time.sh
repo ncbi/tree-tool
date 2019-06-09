8d0fa9fb023e5934a46948d48d6eb53def4c37ac
Slava Brover <vbrover@gmail.com>
2019-06-08 23:17:49 -0400

    time_optim is divided by the number of iterations

--- phylogeny/distTree_inc_time.sh
+++ phylogeny/distTree_inc_time.sh
@@ -3,9 +3,9 @@ THIS=`dirname $0`
 source $THIS/../bash_common.sh
 if [ $# -ne 3 ]; then
   echo "Print time statistics for an incremental distance tree: # leaves, # dissimilarities, optimization time"
-  echo "#1: Distance tree data"
-  echo "#2: Tree version"
-  echo "#3: Chronometer::enabled (0/1)"
+  echo "#1: incremental distance tree directory"
+  echo "#2: tree version"
+  echo "#3: chronometer::enabled (0/1)"
   exit 1
 fi
 INC=$1
@@ -13,7 +13,13 @@ VER=$2
 CHRON=$3
 
 
+#set -x
+
+
 OUT=$INC/hist/makeDistTree.$VER
+if [ ! -e $OUT ]; then
+  exit
+fi
 #echo $OUT
 
 
@@ -21,6 +27,19 @@ Leaves=(`grep '^# Leaves: ' $OUT`)
 Dissim=(`grep '^# Dissimilarities: ' $OUT`)
 CHRON_init=(`grep '^CHRON: Initial topology: ' $OUT`)
 CHRON_optim=(`grep '^CHRON: Topology optimization: local: ' $OUT`)
+
+set +o errexit
+ITER=(`grep '^Iteration ' $OUT | tail -1 | tr '/' ' '`)
+if [ $? == 0 ]; then
+  ITER=${ITER[1]}
+else
+  ITER=`grep -c '^# Hybrids: ' $OUT`
+  if [ $? -ne 0 ]; then
+    exit 
+  fi
+fi
+set -o errexit
+CHRON_optim=`echo "scale=0; ${CHRON_optim[4]} / $ITER" | bc -l`
 
 
 CHRON_getBestChange=0
@@ -49,4 +68,4 @@ fi
 Radius=(`grep '^Ave. radius: ' $OUT`)
 
 
-echo $VER ${Leaves[2]} ${Dissim[2]} ${CHRON_init[3]} ${CHRON_optim[4]} $CHRON_getBestChange $CHRON_tree2subgraph $CHRON_tree2subgraphDissim $CHRON_subgraph $CHRON_subgraph2tree ${Radius[2]}
+echo $VER ${Leaves[2]} ${Dissim[2]} ${CHRON_init[3]} $CHRON_optim $CHRON_getBestChange $CHRON_tree2subgraph $CHRON_tree2subgraphDissim $CHRON_subgraph $CHRON_subgraph2tree ${Radius[2]}
