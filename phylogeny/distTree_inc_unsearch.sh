990935808c4ffd58f1ca163d32fb215155b94af2
Slava Brover <vbrover@gmail.com>
2019-06-25 21:12:35 -0400

    cosmetic

--- phylogeny/distTree_inc_unsearch.sh
+++ phylogeny/distTree_inc_unsearch.sh
@@ -3,12 +3,14 @@ THIS=`dirname $0`
 source $THIS/../bash_common.sh
 if [ $# -ne 2 ]; then
   echo "Restore #1/search/#2/"
-  echo "#1: Directory containing search/"
-  echo "#2: New object"
+  echo "#1: incremental distance tree directory"
+  echo "#2: new object"
   exit 1
 fi
+INC=$1
+OBJ=$2
 
 
-cp /dev/null $1/new/$2
-rm -r $1/search/$2
+cp /dev/null $INC/new/$OBJ
+rm -r $INC/search/$OBJ/
 
