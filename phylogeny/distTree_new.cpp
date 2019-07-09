c2766aa2dc461c6f73af490d17147816bd7152c8
Slava Brover <vbrover@gmail.com>
2019-07-09 11:31:01 -0400

    -variance_dissim

--- phylogeny/distTree_new.cpp
+++ phylogeny/distTree_new.cpp
@@ -19,12 +19,19 @@ struct ThisApplication : Application
 	ThisApplication ()
 		: Application ("Find location of new objects in a distance tree")
 		{
+		  // Input
 		  addPositional ("data", "Directory with data");
 		  addFlag ("init", "Initialize search");
-		  addKey ("variance", "Dissimilarity variance: " + varianceTypeNames. toString (" | "), varianceTypeNames [varianceType]);
-  	  addKey ("variance_power", "Power for -variance pow; > 0", "NaN");
+		  
+  	  addKey ("variance", "Dissimilarity variance function: " + varianceTypeNames. toString (" | "), varianceTypeNames [varianceType]);
+  	  addKey ("variance_power", "Power for -variance pow; >= 0", "NaN");
+  	  addFlag ("variance_dissim", "Variance is computed off dissimilarities");
+  	  addKey ("variance_min", "Min. dissimilarity variance; to be added to the computed variance. If > 0 then all objects are discernible", "0");
+  	    	  
 		  addKey ("name", "Name of the object");
 		  addKey ("dissim", "File of the format: <obj1> <obj2> <dissimilarity>");
+		  
+		  // Output
 		  addKey ("request", "Output file of the format: <obj1> <obj2>");
 		  addKey ("leaf", "Output file of the format: <obj_new> <obj1>-<obj2> <leaf_len> <arc_len>");
 		}
@@ -35,8 +42,12 @@ struct ThisApplication : Application
   {
 	  const string dataDir       = getArg ("data");
 	  const bool   init          = getFlag ("init");
-	               varianceType  = str2varianceType (getArg ("variance"));  // Global   
-	               variancePower = str2real (getArg ("variance_power"));    // Global
+
+	               varianceType    = str2varianceType (getArg ("variance"));  // Global
+	               variancePower   = str2real (getArg ("variance_power"));    // Global
+	               variance_min    = str2real (getArg ("variance_min"));      // Global
+	//const bool   variance_dissim =           getFlag ("variance_dissim");  
+
 	  const string name          = getArg ("name");
 	  const string dissimFName   = getArg ("dissim");
 	  const string requestFName  = getArg ("request");
