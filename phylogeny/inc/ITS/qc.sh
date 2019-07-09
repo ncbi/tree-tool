39eba18a973a5fe6131e47f6a1418029f878d7a8
Slava Brover <vbrover@gmail.com>
2019-07-09 16:22:13 -0400

    ITS parameters

--- phylogeny/inc/ITS/qc.sh
+++ phylogeny/inc/ITS/qc.sh
@@ -31,6 +31,21 @@ sqsh-ms -S PROTEUS  -U anyone  -P allowed  -D uniColl << EOT | sed 's/|$//1' > $
 EOT
 diff $TMP.seq-fa $TMP.locus
 
+ls $INC/new/ > $TMP.new
+sqsh-ms -S PROTEUS  -D uniColl  -U anyone  -P allowed  << EOT | sed 's/|$//1' | sort > $TMP.locus-new
+  select id
+    from Locus
+    where     taxroot = 4751
+          and gene = '5.8S'
+          and dead = 0
+          and outlier is null
+          and in_tree is null;
+  go -m bcp  
+EOT
+#wc -l $TMP.genome-new 
+#wc -l $TMP.new
+diff $TMP.locus-new $TMP.new
+
 #ls $INC/../seq/ > $TMP.seq
 #diff $TMP.seq-fa $TMP.seq
 
