d91ecfb5ae264c1ab4906bb518ac4b912ef11498
Slava Brover <vbrover@gmail.com>
2019-07-25 16:55:38 -0400

    disclaimer

--- dm/threshold.cpp
+++ dm/threshold.cpp
@@ -1,5 +1,37 @@
 // threshold.cpp
 
+/*===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE                          
+*               National Center for Biotechnology Information
+*                                                                          
+*  This software/database is a "United States Government Work" under the   
+*  terms of the United States Copyright Act.  It was written as part of    
+*  the author's official duties as a United States Government employee and 
+*  thus cannot be copyrighted.  This software/database is freely available 
+*  to the public for use. The National Library of Medicine and the U.S.    
+*  Government have not placed any restriction on its use or reproduction.  
+*                                                                          
+*  Although all reasonable efforts have been taken to ensure the accuracy  
+*  and reliability of the software and data, the NLM and the U.S.          
+*  Government do not and cannot warrant the performance or results that    
+*  may be obtained by using this software or data. The NLM and the U.S.    
+*  Government disclaim all warranties, express or implied, including       
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.                                                                
+*                                                                          
+*  Please cite the author in any work or product based on this material.   
+*
+* ===========================================================================
+*
+* Author: Vyacheslav Brover
+*
+* File Description:
+*   Find a threshold separating the values of a Boolean attribute
+*
+*/
+
+
 #undef NDEBUG
 #include "../common.inc"
 
@@ -140,7 +172,7 @@ size_t findClass_last (const Vector<Bin> &bins,
 struct ThisApplication : Application
 {
   ThisApplication ()
-    : Application ("Find a <score-attr> threshold separating the Boolean attribute <class_attr>")
+    : Application ("Find a <score-attr> threshold separating the values of Boolean attribute <class_attr>")
     {
       addPositional ("file", dmSuff + "-file, sorted by <score_attr> descending");
       addPositional ("score_attr", "Score number attribute");
--- str2hash.cpp
+++ str2hash.cpp
@@ -1,5 +1,37 @@
 // str2hash.cpp
 
+/*===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE                          
+*               National Center for Biotechnology Information
+*                                                                          
+*  This software/database is a "United States Government Work" under the   
+*  terms of the United States Copyright Act.  It was written as part of    
+*  the author's official duties as a United States Government employee and 
+*  thus cannot be copyrighted.  This software/database is freely available 
+*  to the public for use. The National Library of Medicine and the U.S.    
+*  Government have not placed any restriction on its use or reproduction.  
+*                                                                          
+*  Although all reasonable efforts have been taken to ensure the accuracy  
+*  and reliability of the software and data, the NLM and the U.S.          
+*  Government do not and cannot warrant the performance or results that    
+*  may be obtained by using this software or data. The NLM and the U.S.    
+*  Government disclaim all warranties, express or implied, including       
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.                                                                
+*                                                                          
+*  Please cite the author in any work or product based on this material.   
+*
+* ===========================================================================
+*
+* Author: Vyacheslav Brover
+*
+* File Description:
+*   Convert a list of strings to hash codes
+*
+*/
+
+
 #undef NDEBUG
 #include "common.inc"
 
@@ -17,7 +49,7 @@ namespace
 struct ThisApplication : Application
 {
   ThisApplication ()
-  : Application ("Print sorted unique hash codes for a list of strings form stdin", false)
+  : Application ("Print sorted unique 64-bit hash codes for a list of strings form stdin", false)
   {}
 
 
