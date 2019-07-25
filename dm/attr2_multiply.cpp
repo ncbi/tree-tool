e86a2260d469381f6e892cacfebdbff50e3e86f0
Slava Brover <vbrover@gmail.com>
2019-07-25 16:54:12 -0400

    disclaimer

--- dm/attr2_multiply.cpp
+++ dm/attr2_multiply.cpp
@@ -1,5 +1,37 @@
 // attr2_multiply.cpp
 
+/*===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE                          
+*               National Center for Biotechnology Information
+*                                                                          
+*  This software/database is a "United States Government Work" under the   
+*  terms of the United States Copyright Act.  It was written as part of    
+*  the author's official duties as a United States Government employee and 
+*  thus cannot be copyrighted.  This software/database is freely available 
+*  to the public for use. The National Library of Medicine and the U.S.    
+*  Government have not placed any restriction on its use or reproduction.  
+*                                                                          
+*  Although all reasonable efforts have been taken to ensure the accuracy  
+*  and reliability of the software and data, the NLM and the U.S.          
+*  Government do not and cannot warrant the performance or results that    
+*  may be obtained by using this software or data. The NLM and the U.S.    
+*  Government disclaim all warranties, express or implied, including       
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.                                                                
+*                                                                          
+*  Please cite the author in any work or product based on this material.   
+*
+* ===========================================================================
+*
+* Author: Vyacheslav Brover
+*
+* File Description:
+*   Multiply a two-way attribute by a coefficient
+*
+*/
+
+
 #undef NDEBUG
 #include "../common.inc"
 
@@ -23,7 +55,7 @@ const string clusterAttrName = "Cluster";
 struct ThisApplication : Application
 {
   ThisApplication ()
-    : Application ("Multiply a ReaAttr2 and print the resulting data set")
+    : Application ("Multiply a two-way attribute and print the resulting data set")
     {
   	  addPositional ("file", dmSuff + "-file");
   	  addPositional ("attr2Name", "Attribute name of an object-object table in the " + dmSuff + "-file");
