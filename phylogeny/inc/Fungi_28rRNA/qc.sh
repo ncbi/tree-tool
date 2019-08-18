6cf01c5b6da827b09d618ce6775f874d9791c56e
Slava Brover <vbrover@gmail.com>
2019-08-18 17:59:18 -0400

    rename

--- phylogeny/inc/Fungi_28rRNA/qc.sh
+++ phylogeny/inc/Fungi_28rRNA/qc.sh
@@ -2,11 +2,13 @@
 source bash_common.sh
 if [ $# -ne 1 ]; then
   echo "Quality control of distTree_inc_new.sh"
-  echo "#1: Incremental distance tree directory"
+  echo "#1: go"
   echo "Requires: Time: O(n log^3(n))"
   exit 1
 fi
-INC=$1
+
+
+INC=`dirname $0`
 
 
 TMP=`mktemp`
