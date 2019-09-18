8681cb191122b73c6e46be5a3d0ccab765a4ac6d
Slava Brover <vbrover@gmail.com>
2019-09-18 17:58:39 -0400

    better optimization of trees

--- phylogeny/distTree_test.sh
+++ phylogeny/distTree_test.sh
@@ -103,7 +103,7 @@ $THIS/makeDistTree -qc  -threads 3  -data data/Saccharomyces  -variance linExp  
   -hybridness_min 1.2  -hybrid_parent_pairs Saccharomyces.hybrid_parent_pairs  -delete_hybrids Saccharomyces.hybrid  -dissim_boundary 0.675 \
   -delete_criterion_outliers Saccharomyces.criterion_outliers  -criterion_outlier_num_max 1 \
   -delete_deformation_outliers Saccharomyces.deformation_outliers  -deformation_outlier_num_max 1 \
-  > Saccharomyces.distTree
+  -output_tree $TMP.tree > /dev/null
 diff Saccharomyces.hybrid data/Saccharomyces.hybrid
 $THIS/hybrid2list.sh Saccharomyces.hybrid > Saccharomyces.hybrid.list
 rm Saccharomyces.hybrid
@@ -113,11 +113,11 @@ diff Saccharomyces.criterion_outliers data/Saccharomyces.criterion_outliers
 rm Saccharomyces.criterion_outliers
 diff Saccharomyces.deformation_outliers data/Saccharomyces.deformation_outliers
 rm Saccharomyces.deformation_outliers
-if [ $USER == "brovervv" ]; then
-  set +o errexit  # ??
-  $THIS/distTree_compare_criteria.sh Saccharomyces.distTree data/Saccharomyces.distTree
-  set -o errexit  # ??
-fi
+# Saccharomyces.distTree
+$THIS/tree2obj.sh $TMP.tree > $TMP.list
+$THIS/../dm/dm2subset data/Saccharomyces $TMP.list > $TMP.dm
+$THIS/makeDistTree  -threads 3  -data $TMP  -input_tree $TMP.tree  -variance linExp  -optimize  -reinsert  -subgraph_iter_max 10  > Saccharomyces.distTree
+$THIS/distTree_compare_criteria.sh Saccharomyces.distTree data/Saccharomyces.distTree
 rm Saccharomyces.distTree
 
 echo ""
@@ -175,7 +175,7 @@ $THIS/makeDistTree -qc  -threads 3  -data data/Saccharomyces2  -variance linExp 
   -hybridness_min 1.2  -hybrid_parent_pairs Saccharomyces2.hybrid_parent_pairs  -delete_hybrids Saccharomyces2.hybrid  -dissim_boundary 0.675 \
   -delete_criterion_outliers Saccharomyces2.criterion_outliers  -criterion_outlier_num_max 1 \
   -delete_deformation_outliers Saccharomyces2.deformation_outliers  -deformation_outlier_num_max 1 \
-  > Saccharomyces2.distTree
+  -output_tree $TMP.tree > /dev/null
 $THIS/../rm_col.sh Saccharomyces2.hybrid 10
 $THIS/../sort.sh Saccharomyces2.hybrid
 diff Saccharomyces2.hybrid data/Saccharomyces2.hybrid
@@ -189,9 +189,11 @@ diff Saccharomyces2.criterion_outliers data/Saccharomyces2.criterion_outliers
 rm Saccharomyces2.criterion_outliers
 diff Saccharomyces2.deformation_outliers data/Saccharomyces2.deformation_outliers
 rm Saccharomyces2.deformation_outliers
-set +o errexit  # ??
+# Saccharomyces2.distTree
+$THIS/tree2obj.sh $TMP.tree > $TMP.list
+$THIS/../dm/dm2subset data/Saccharomyces2 $TMP.list > $TMP.dm
+$THIS/makeDistTree  -threads 3  -data $TMP  -input_tree $TMP.tree  -variance linExp  -optimize  -reinsert  -subgraph_iter_max 10  > Saccharomyces2.distTree
 $THIS/distTree_compare_criteria.sh Saccharomyces2.distTree data/Saccharomyces2.distTree
-set -o errexit  # ??
 rm Saccharomyces2.distTree
 
 set +o errexit
