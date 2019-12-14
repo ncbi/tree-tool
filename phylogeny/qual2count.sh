813ddb6161609dbe9f23e0677bf7f38b5c3e8d40
Slava Brover <vbrover@gmail.com>
2019-12-13 21:24:11 -0500

    new

---
+++ phylogeny/qual2count.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+THIS=`dirname $0`
+source $THIS/../bash_common.sh
+if [ $# -ne 1 ]; then
+  echo -e "Print pairs: taxon\tgains+losses"
+  echo "cut -f 2 | count: sum - count = misconguence"
+  echo "#1: qual-file made by makeFeatureTree"
+  exit 1
+fi
+QUAL=$1
+
+
+cat $QUAL | sed 's| / .*$||1' | sed -e 's/ [-+]/\t/g' | tr ' ' '_' | awk -F '\t' '{printf "%s\t%d\n", $1, $2 + $3};' 
+
+
