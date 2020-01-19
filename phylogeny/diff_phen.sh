c38279ef3826c460102c9bed89eabb7144f11978
Slava Brover <vbrover@gmail.com>
2020-01-19 17:23:16 -0500

    new

---
+++ phylogeny/diff_phen.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ $# -ne 1 ]; then
+  echo "Exit 1 if there are lineage differences between phen.old/ and phen/"
+  echo "#1: assembly id"
+  exit 1
+fi
+ASM=$1
+
+H=`file2hash $ASM`
+
+diff phen/$H/$ASM phen.old/$H/$ASM | grep '^[><]' | sed 's/^..//1' | sed 's/:.*$//1' | sort | uniq -c | grep -v ' 1 ' 
+if [ $? == 0 ]; then
+  exit 1
+fi
+
+
+
