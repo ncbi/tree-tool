8bf039ab7360b2fb85234f26fcdf0be6451f8919
Slava Brover <vbrover@gmail.com>
2020-01-29 15:53:05 -0500

    new

---
+++ record.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+source ~brovervv/code/cpp/bash_common.sh
+if [ $# -ne 2 ]; then
+  echo "Print a row of a tab-delimnietd table where the top row is a header"
+  echo "#1: table"
+  echo "#2: row number (1-based)"
+  exit 1
+fi
+TAB=$1
+ROW=$2
+
+
+TMP=`mktemp`
+#echo $TMP
+
+
+head -1 $TAB | sed 's/^#//1' | tr '\t' '\n' > $TMP.1
+
+N=$(( $ROW + 1 ))
+head -$N $TAB | tail -1 | tr '\t' '\n' > $TMP.2
+
+paste $TMP.1 $TMP.2
+
+
+rm $TMP*
