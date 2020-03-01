3a2e18a799192be232af2da1b7d59c950c58c73e
Slava Brover <vbrover@gmail.com>
2020-02-29 20:23:58 -0500

    ITER_MAX

--- phylogeny/calibrateDissims.sh
+++ phylogeny/calibrateDissims.sh
@@ -1,40 +1,39 @@
 #!/bin/bash
 THIS=`dirname $0`
 source $THIS/../bash_common.sh
-if [ $# -ne 4 ]; then
+if [ $# -ne 6 ]; then
   echo "#1: input .dm-file without '.dm'"
   echo "#2: dissimilarity attribute in #1"
   echo "#3: parameters of makeDistTree after -variance"
-  echo "#4: phen/"
+  echo "#4: max. optimization iterations (> 0, normally 20)"
+  echo "#5: phen/"
+  echo "#6: phen is large (0/1)"
   exit 1
 fi
 INPUT=$1
-DISSIM=$2
+DISSIM="$2"
 VARIANCE_PAR="$3"
-PHEN=$4
+ITER_MAX=$4
+PHEN=$5
+PHEN_LARGE=$6
 
 
 TMP=`mktemp`
-#echo $TMP 
+echo $TMP 
 
-
-$THIS/../dm/dm2objs $INPUT | sort > $TMP.list
-ls $PHEN > $TMP.phen
-$THIS/../setMinus $TMP.list $TMP.phen > $TMP.delete
-if [ -s $TMP.delete ]; then
-  wc -l $TMP.delete
-  $THIS/../dm/dm2subset $INPUT $TMP.delete -exclude > $TMP.dm
-  INPUT=$TMP.dm
-fi
 
 echo ""
 echo ""
 $THIS/makeDistTree  -threads 5  -data $INPUT  -dissim_attr $DISSIM  -variance $VARIANCE_PAR \
-  -optimize  -subgraph_iter_max 20  -noqual  -output_feature_tree $TMP.feature_tree
+     -optimize  -subgraph_iter_max $ITER_MAX  -noqual  -output_feature_tree $TMP.feature_tree
 
 echo ""
 echo ""
-$THIS/makeFeatureTree  -input_tree $TMP.feature_tree  -features $PHEN  -prefer_gain  -nominal_singleton_is_optional  -qual $TMP.qual
+LARGE=""
+if [ $PHEN_LARGE -eq 1 ]; then
+  LARGE="-large"
+fi
+$THIS/makeFeatureTree  -input_tree $TMP.feature_tree  -features $PHEN  $LARGE  -prefer_gain  -nominal_singleton_is_optional  -qual $TMP.qual
 
 
 rm $TMP*  
