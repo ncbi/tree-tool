d769c7ff3fa5e9c4ee62d16cdfdc24bf1c4c53ad
Slava Brover <vbrover@gmail.com>
2020-03-26 11:13:19 -0400

    change

---
+++ phylogeny/inc/marker/bacteria/outlier2db.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source bash_common.sh
+if [ $# -ne 2 ]; then
+  echo "Add an outlier to database"
+  echo "#1: Locus.id"
+  echo "#2: Locus.outlier"
+  exit 1
+fi
+
+sqsh-ms -S PROTEUS  -D uniColl  << EOT
+  update Locus
+    set outlier = '$2'
+    where id = '$1';
+  go -m bcp 
+EOT
+
+
