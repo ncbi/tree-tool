9e58d4a093cebfb65bca9dafcedb348fd1e48a0d
Slava Brover <vbrover@gmail.com>
2020-03-26 11:17:30 -0400

    change

---
+++ phylogeny/inc/GenBank/bacteria/genogroup2db.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source bash_common.sh
+if [ $# -ne 1 ]; then
+  echo "$0"
+  echo "#1: File genogroup_table"
+  exit 1
+fi
+
+
+loadLISTC $1
+
+sqsh-ms  -S PROTEUS  -D uniColl  -L exit_failcount=1 << EOF | sed 's/|$//1' 
+  EXEC Genogroup2outliers 2 /*PAR*/, 1;
+  go -m bcp
+EOF
+
---
+++ phylogeny/inc/GenBank/bacteria/request_closest.sh
@@ -0,0 +1,1 @@
+Genome_hash_requestClosest.sh $1 ~/panfs/GenBank/bacteria/genome CDS
