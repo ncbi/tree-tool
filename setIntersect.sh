4d64f1481ed97c710e74148b2f74266c0a0b0b70
Slava Brover <vbrover@gmail.com>
2020-05-06 11:13:46 -0400

    cosmetic

--- setIntersect.sh
+++ setIntersect.sh
@@ -2,24 +2,27 @@
 THIS=`dirname $0`
 source $THIS/bash_common.sh
 if [ $# -ne 3 ]; then
-  echo "#1 - List1"
-  echo "#2 - List2"
-  echo "#3 - number(0/1)"
-  echo "Make set-theoretic intersection of List1 and List2"
+  echo "#1: List1"
+  echo "#2: List2"
+  echo "#3: number (0/1)"
+  echo "Print set-theoretic intersection of List1 and List2"
   exit 1
 fi
+L1=$1
+L2=$2
+NUMP=$3
 
 
 NUM=""
-if [ $3 == 1 ]; then
+if [ $NUMP == 1 ]; then
   NUM="-number"
 fi
 
 TMP=`mktemp` 
 #echo $TMP
 
-$THIS/setMinus $NUM $1 $2 > $TMP
-$THIS/setMinus $NUM $1 $TMP
+$THIS/setMinus $NUM $L1 $L2 > $TMP
+$THIS/setMinus $NUM $L1 $TMP
 
 rm $TMP*
 
