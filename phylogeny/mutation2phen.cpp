04e5fef46aa1474686285d5c7e84db048e681124
Slava Brover <vbrover@gmail.com>
2020-05-13 13:21:45 -0400

    new

---
+++ phylogeny/mutation2phen.cpp
@@ -0,0 +1,242 @@
+// mutation2phen.cpp
+
+/*===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE                          
+*               National Center for Biotechnology Information
+*                                                                          
+*  This software/database is a "United States Government Work" under the   
+*  terms of the United States Copyright Act.  It was written as part of    
+*  the author's official duties as a United States Government employee and 
+*  thus cannot be copyrighted.  This software/database is freely available 
+*  to the public for use. The National Library of Medicine and the U.S.    
+*  Government have not placed any restriction on its use or reproduction.  
+*                                                                          
+*  Although all reasonable efforts have been taken to ensure the accuracy  
+*  and reliability of the software and data, the NLM and the U.S.          
+*  Government do not and cannot warrant the performance or results that    
+*  may be obtained by using this software or data. The NLM and the U.S.    
+*  Government disclaim all warranties, express or implied, including       
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.                                                                
+*                                                                          
+*  Please cite the author in any work or product based on this material.   
+*
+* ===========================================================================
+*
+* Author: Vyacheslav Brover
+*
+* File Description:
+*   Convert DNA mutations to 3-values Boolean attributes for makeFeatureTree
+*
+*/
+
+
+#undef NDEBUG
+#include "../common.inc"
+
+#include "../common.hpp"
+using namespace Common_sp;
+#include "version.inc"
+
+
+
+namespace 
+{
+  
+  
+ 
+struct Mutation : Root
+{
+  size_t pos {NO_INDEX};
+    // In reference
+    // < NO_INDEX
+  string ref;
+  string allele;
+  
+  bool ambig {false};
+    // Function of allele
+  
+
+  explicit Mutation (const string& mut)
+    { size_t posStart = NO_INDEX;
+      size_t alleleStart = NO_INDEX;
+      FFOR (size_t, i, mut. size ())
+        if (isDigit (mut [i]))
+        {
+          if (posStart == NO_INDEX)
+            posStart = i;
+        }
+        else
+        {
+          if (posStart != NO_INDEX)
+            if (alleleStart == NO_INDEX)
+              alleleStart = i;
+        }
+      QC_ASSERT (posStart < alleleStart);
+      QC_ASSERT (alleleStart < NO_INDEX);
+      ref    =                mut. substr (0, posStart);
+      pos    = (size_t) stoi (mut. substr (posStart, alleleStart - posStart));
+      allele =                mut. substr (alleleStart);
+      ASSERT (pos < NO_INDEX);
+      if (ref == "INS")
+        ref. clear ();
+      if (allele == "DEL")
+        allele. clear ();
+      QC_ASSERT (! (ref. empty () && allele. empty ()));    
+      static const string acgt ("acgt");
+      for (const char c : ref)
+        QC_ASSERT (charInSet (c, acgt));
+
+      // ambig        
+      for (const char c : allele)
+        if (! charInSet (c, acgt))
+          ambig = true;
+    }
+  Mutation () = default;
+  Mutation (const Mutation& ) = default;
+  void saveText (ostream &os) const override
+    { os << ref << pos << allele; }
+    
+
+  bool operator== (const Mutation& other) const
+    { return    pos    == other. pos
+             && ref    == other. ref
+             && allele == other. allele;
+    }
+  bool operator< (const Mutation& other) const
+    { LESS_PART (*this, other, pos);
+      LESS_PART (*this, other, ref);
+      LESS_PART (*this, other, allele);
+      return false;
+    }
+  struct Hash
+  {
+    size_t operator() (const Mutation &mut) const
+      { static hash<string> strHash;
+        return mut. pos ^ strHash (mut. ref) ^ strHash (mut. allele);
+      }
+  };  
+  size_t stop () const
+    { return pos + ref. size (); }    
+};
+
+  
+  
+void save (const Vector<Mutation> &muts, 
+           const Vector<Vector<Mutation>> &pos2muts,
+           ostream &os) 
+{ 
+  for (const Mutation& mut : muts)
+    if (mut. ambig)
+    {
+      FFOR_START (size_t, i, mut. pos, min (mut. stop (), pos2muts. size ()))
+        for (const Mutation& other : pos2muts [i])
+        {
+          ASSERT (! other. ambig);
+          ASSERT (other. pos == i);
+          if (other. stop () <= mut. stop ())  // other.ref is inside mut.ref
+          {
+            other. saveText (os);
+            os << " 1" << endl;
+          }
+        }
+    }
+    else
+    {
+      mut. saveText (os);
+      os << " 0" << endl;
+    }
+}
+
+  
+
+
+struct ThisApplication : Application
+{
+  ThisApplication ()
+    : Application ("Convert DNA mutations to 3-values Boolean attributes for makeFeatureTree")
+	  {
+	  	addPositional ("in", "Input directory with DNA mutations in the format <ref seq><ref pos><target seq>");
+	  	addPositional ("out", "Output directory for 3-valued Boolean atrtributes for makeFeatrueTree");
+	  }
+
+
+
+  void body () const final
+  {
+    const string inDirName  = getArg ("in");
+    const string outDirName = getArg ("out");
+
+
+    unordered_map<string,Vector<Mutation>> obj2muts;  obj2muts. rehash (100000);  // PAR
+    Vector<Vector<Mutation>> pos2muts;  // !ambig
+    {
+      size_t pos_max = 0;
+      unordered_set<Mutation,Mutation::Hash> allMuts;  allMuts. rehash (10000);  // PAR
+      {
+        FileItemGenerator fig (1000, true, inDirName);
+        string fName;
+    	  while (fig. next (fName))
+        {
+          Vector<Mutation> muts;  muts. reserve (16);  // PAR
+          {
+            LineInput li (inDirName + '/' + fName);
+            while (li. nextLine ())
+            {
+              Mutation mut (li. line);
+              if (! mut. ambig)
+              {
+                allMuts. insert (mut);
+                maximize (pos_max, mut. pos);
+              }
+              muts << move (mut);
+            }
+          }
+          muts. sort ();
+          muts. uniq ();
+          obj2muts [fName] = move (muts);        
+        }
+      }
+      cout << "Object: " << obj2muts. size () << endl;
+      cout << "Mutations: " << allMuts. size () << endl;    
+      cout << "Max. position: " << pos_max + 1 << endl;
+      
+      pos2muts. resize (pos_max + 1);
+      for (const Mutation& mut : allMuts) 
+        pos2muts [mut. pos] << mut;
+    }
+    
+    size_t pos_used = 0;
+    for (const auto& muts : pos2muts)
+      if (! muts. empty ())
+        pos_used++;
+    cout << "Positions: " << pos_used << endl;
+    
+    {
+      Progress prog (obj2muts. size (), 100);  // PAR
+      for (const auto& it : obj2muts)
+      {
+        prog (it. first);
+        OFStream of (outDirName + "/" + it. first);
+        save (it. second, pos2muts, of);
+      }
+    }
+  }  
+};
+
+
+}  // namespace
+
+
+
+
+int main (int argc, 
+          const char* argv[])
+{
+  ThisApplication app;
+  return app. run (argc, argv);  
+}
+
+
+
