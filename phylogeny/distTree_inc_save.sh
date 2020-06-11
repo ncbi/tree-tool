104f0b0fa163008367971e56447e9bc6b38211fd
Slava Brover <vbrover@gmail.com>
2020-06-11 16:50:39 -0400

    makeblastdb version

--- phylogeny/distTree_inc_save.sh
+++ phylogeny/distTree_inc_save.sh
@@ -27,7 +27,7 @@ for F in `ls $INC/`; do
   
   N=`cat $INC/$F | wc -l`
   if [ $N -lt 20000 ]; then  # PAR
-    sed 's|/home/brovervv/code/cpp|CPP_DIR|g' $INC/$F | sed 's/\[ $AT_NCBI == 1 \]/false/g' | sed 's/PROTEUS/""/g' > $OUT/$F
+    sed 's|/home/brovervv/code/cpp|CPP_DIR|g' $INC/$F | sed 's/\[ $AT_NCBI == 1 \]/false/g' | sed 's/PROTEUS/""/g' | sed 's/-blastdb_version 4//g' > $OUT/$F
   fi
 done
 
