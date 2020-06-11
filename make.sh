6f9952a22719c622677cef491b1daa2540bd1f7c
Slava Brover <vbrover@gmail.com>
2020-06-11 14:49:45 -0400

    installation

--- make.sh
+++ make.sh
@@ -2,7 +2,8 @@
 source ./bash_common.sh
 unset AT_NCBI
 
-sed 's|^CPP_DIR =.*$|CPP_DIR = '$PWD'|1' MakeRules > MakeRules.tmp
+DIR=$PWD
+sed 's|^CPP_DIR =.*$|CPP_DIR = '$DIR'|1' MakeRules > MakeRules.tmp
 mv MakeRules.tmp MakeRules
 
 make all
@@ -27,5 +28,5 @@ ls inc/*/*.sh     1>> tmp.list  2> /dev/null
 ls inc/*/*/*.sh   1>> tmp.list  2> /dev/null
 ls inc/*/*/*/*.sh 1>> tmp.list  2> /dev/null
 set -o errexit
-../trav tmp.list -step 1 "sed 's|CPP_DIR|'$PWD'|g' %f > %f.tmp && mv %f.tmp %f && chmod a+x %f"
+../trav tmp.list -step 1 "sed 's|CPP_DIR|'$DIR'|g' %f > %f.tmp && mv %f.tmp %f && chmod a+x %f"
 rm tmp.list
