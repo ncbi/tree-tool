09bc7c9e585491790bbc51ff0beba979e0fcf405
Slava Brover <vbrover@gmail.com>
2020-06-18 13:08:59 -0400

    new

---
+++ database/distTree_inc.sql
@@ -0,0 +1,28 @@
+create table List
+(
+  id numeric(20)  not null  primary key
+);
+
+
+create table ListC
+(
+  id varchar(256)  not null  primary key
+);
+
+
+create table Locus
+(
+  id varchar(12)  not null  constraint Locus_pk primary key
+
+, taxroot int  not null  
+, gene varchar(16)  not null  
+
+, in_tree bit  constraint Locus_in_tree_ck check (in_tree = 1)
+, outlier varchar(16)  
+, constraint Locus_outlier_in_tree_nl check (outlier is null or in_tree is null)
+);
+grant select on Locus to public;
+create index Locus_gene_idx on Locus(gene);
+create index Locus_outlier_idx on Locus(outlier);
+
+
