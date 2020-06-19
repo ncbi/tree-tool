debce24d4ec308b819ebbc680f5bed4d84726e60
Slava Brover <vbrover@gmail.com>
2020-06-19 17:52:05 -0400

    new

---
+++ genetics/hash2table.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+THIS=`dirname $0`
+source $THIS/../bash_common.sh
+if [ $# -ne 2 ]; then
+  echo "#1: Genome.id"
+  echo "#2: hash type (GenomeHash.type)"
+  exit 1
+fi
+GENOME=$1
+TYPE=$2
+
+
+H=`$THIS/../file2hash $GENOME`
+cat genome/$H/$GENOME/$GENOME.hash-$TYPE | sed -e 's/^/'$GENOME'\t'$TYPE'\t/1'
+
+
