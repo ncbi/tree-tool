68ebe4ec6a2e66435f9b29b38bc868ea987292c0
Slava Brover <vbrover@gmail.com>
2020-08-20 12:10:23 -0400

    head -100 is fixed

--- genetics/dna_closest.sh
+++ genetics/dna_closest.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 THIS=`dirname $0`
 source $THIS/../bash_common.sh
-if [ $# -ne 3 ]; then
-  echo "Return: top #3 BLASTN hits in top strand"
+if [ $# -ne 2 ]; then
+  echo "Return: top 100 BLASTN hits in top strand"
   echo "#1; Query DNA sequence"
   echo "#2: Subject DNA database"
-  echo "#3: Number of top hits"
   exit 1
 fi
 QUERY=$1
 DB=$2
-NUM=$3
 
 
 
@@ -22,7 +20,7 @@ blastn  -db $DB  -query $QUERY  -strand plus  -task blastn  -outfmt '6 sseqid ni
 NAME=`head -1 $QUERY | sed 's/^>//1' | cut -f 1 -d ' '`
 set +o errexit
 grep -v $NAME $TMP.blastn > $TMP.grep 
-sort -k 2 -n -r $TMP.grep | cut -f 1 | head -$NUM > $TMP.head
+sort -k 2 -n -r $TMP.grep | cut -f 1 | head -100 > $TMP.head
 set -o errexit
 sort -u $TMP.head 
 
