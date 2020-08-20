42b218a78f87db851d4fecfa35c2bdaf4df40d3a
Slava Brover <vbrover@gmail.com>
2020-08-20 11:57:02 -0400

    new

---
+++ genetics/ncbi7.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+THIS=`dirname $0`
+source $THIS/../bash_common.sh
+if [ $# -ne 1 ]; then
+  echo "Convert NCBI taxonomy lineage to the 7-rank lineage"
+  echo "#1: Input taxonomy feature file"
+  exit 1
+fi
+IN=$1
+
+
+grep '^01-00:\|^04-00:\|^07-00:\|^13-00:\|^18-00:\|^22-00:\|^26-00:' $IN
