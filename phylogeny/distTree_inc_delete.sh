b183ce46b43b313032d1f91489e025a750f707a9
Slava Brover <vbrover@gmail.com>
2021-01-06 17:21:57 -0500

    bug with distTree_inc_new_list.sh

--- phylogeny/distTree_inc_delete.sh
+++ phylogeny/distTree_inc_delete.sh
@@ -42,7 +42,7 @@ mv $INC/dissim.new $INC/dissim
 echo ""
 $INC/objects_in_tree.sh $DEL null
 
-$THIS/distTree_inc_new_list.sh > $INC/new.list
+$THIS/distTree_inc_new_list.sh $INC > $INC/new.list
 $THIS/../setIntersect.sh $DEL $INC/new.list 0 > $INC/new-del.list
 rm $INC/new.list
 $THIS/distTree_inc_new_cmd.sh $INC "rm" $INC/new-del.list
