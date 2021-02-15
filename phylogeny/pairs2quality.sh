f8264ad3e6b5567729f22859d70d2c14cb8f3231
Slava Brover <vbrover@gmail.com>
2021-02-15 17:28:21 -0500

    $EVAL_LIST

--- phylogeny/pairs2quality.sh
+++ phylogeny/pairs2quality.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 THIS=`dirname $0`
 source $THIS/../bash_common.sh
-if [ $# -ne 5 ]; then
+if [ $# -ne 6 ]; then
   echo "Evaluate the quality of a dissimilarity"
   echo "#1: file with dissimilarities, line format: <obj1> <obj2> <dissim>"
-  echo "#2: directory with reference classifications"
-    # #2 is small
+  echo "#2: directory with reference classifications or ''"
+    # #2 is small (one-level)
   echo "#3: parameters after '-variance' in makeDistTree "
-  echo "#4: output tree file"
-  echo "#5: output tree format: ASNT, newick, dm"
+  echo "#4: object list to measure quality for or ''"
+  echo "#5: output tree file"
+  echo "#6: output tree format: ASNT, newick, dm"
   exit 1
 fi
 DISSIM=$1
-PHEN=$2
+PHEN="$2"
 VARIANCE=$3
-TREE=$4
-FORMAT=$5
+EVAL_LIST="$4"
+TREE=$5
+FORMAT=$6
 
 
 TMP=`mktemp`
@@ -27,10 +29,13 @@ $THIS/../dm/pairs2dm $DISSIM 1 symbet 8 -distance -qc > $TMP.dm
 section "Building tree ..."
 $THIS/makeDistTree  -data $TMP  -dissim_attr symbet  -variance $VARIANCE  -optimize  -subgraph_iter_max 5  -output_tree $TMP.tree  -threads 10
 
-section "Calculating miscongruence with $PHEN ..."
-$THIS/tree_quality_phen.sh $TMP.tree "" $PHEN 0 1 ''
-
+section "Creating $TREE ..."
 $THIS/printDistTree $TMP.tree -qc  -order  -format $FORMAT > $TREE
+
+if [ $PHEN ]; then
+  section "Calculating miscongruence of $TREE with $PHEN ..."
+  $THIS/tree_quality_phen.sh $TMP.tree "$EVAL_LIST" $PHEN 0 1 ''
+fi
  
 
 rm -f $TMP*
