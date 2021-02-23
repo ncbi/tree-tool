a0b6c6c9008080e4805938615d7ac10ae63c1bfc
Slava Brover <vbrover@gmail.com>
2021-02-23 17:58:28 -0500

    moved

--- database/diff_phen.sh
+++ database/diff_phen.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 if [ $# -ne 1 ]; then
   echo "Exit 1 if there are lineage differences between phen.old/ and phen/"
-  echo "#1: assembly id"
+  echo "#1: Genome.id"
   exit 1
 fi
 ASM=$1
 
-H=`file2hash $ASM`
+
+H=`$THIS/../file2hash $ASM`
 
 diff phen/$H/$ASM phen.old/$H/$ASM | grep '^[><]' | sed 's/^..//1' | sed 's/:.*$//1' | sort | uniq -c | grep -v ' 1 ' 
 if [ $? == 0 ]; then
