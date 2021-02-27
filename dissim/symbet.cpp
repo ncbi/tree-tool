768de126d3f8bf0d01537d0a2cbefa8d77ef241c
Slava Brover <vbrover@gmail.com>
2021-02-26 19:59:29 -0500

    sequences are made unique

--- dissim/symbet.cpp
+++ dissim/symbet.cpp
@@ -43,6 +43,7 @@ using namespace DM_sp;
 using namespace DM_sp;
 #include "../genetics/seq.hpp"
 using namespace Seq_sp;
+#include "nw/sm_blosum62.c"
 #include "../version.inc"
 
 
@@ -51,24 +52,44 @@ namespace
 {
   
   
-StringVector readSeqs (const string &fName)
+unordered_set<string> readSeqs (const string &fName,
+                                size_t len_min)
 {
-  StringVector seqs;  seqs. reserve (100000);  // PAR
+  unordered_set<string> seqs;  seqs. rehash (100000);  // PAR
   Multifasta fa (fName, true, 16 * 1024 * 1024);  // PAR
   while (fa. next ())
   {
-    const Peptide pep (fa, Peptide::stdAveLen, false);  
+    Peptide pep (fa, Peptide::stdAveLen, false);  
     pep. qc ();
-    seqs << pep. seq;
-      // Convert to "positives" ??
+      // Convert pep.seq to "positives" ??
+    if (pep. seq. size () >= len_min)
+      seqs. insert (move (pep. seq));
   }
   return seqs;
 }
 
 
 
-Vector<size_t> getBests (const StringVector &seqs1,
-                         const StringVector &seqs2,
+double kmer2weight (const string &kmer)
+{
+  ASSERT (! kmer. empty ());
+  double sum = 0.0;
+  for (const char c : kmer)
+  {
+    const char* c_pos = strchr (NCBISM_Blosum62. symbols, c);
+    ASSERT (c_pos);
+    const size_t i = (size_t) (c_pos - NCBISM_Blosum62. symbols);
+    const double w = s_Blosum62PSM [i * 25 + i];  // Different matrix ??
+    ASSERT (w > 0.0);
+    sum += w;
+  }
+  return sum;
+}
+
+
+
+Vector<size_t> getBests (const unordered_set<string> &seqs1,
+                         const unordered_set<string> &seqs2,
                          size_t k)
 // Return: size() = seqs1.size()
 //         values are indexes of seqs2 or no_index
@@ -97,10 +118,10 @@ Vector<size_t> getBests (const StringVector &seqs1,
 
   Vector<size_t> bests;  bests. reserve (seqs1. size ());
   {
-    unordered_map<size_t/*id*/,size_t> id2num;  id2num. rehash (100000);  // PAR
+    unordered_map<size_t/*id*/,double> id2weight;  id2weight. rehash (100000);  // PAR
     for (const string& seq : seqs1)
 	  {
-	    id2num. clear ();
+	    id2weight. clear ();
       const size_t seqSize = seq. size ();
       FOR (size_t, i, seqSize)
         if (i + k <= seqSize)
@@ -112,12 +133,17 @@ Vector<size_t> getBests (const StringVector &seqs1,
             continue;
           if (const Vector<size_t>* ids = findPtr (kmer2ids, kmer. seq))
             for (const size_t id : *ids)
-              id2num [id] ++;
+            {
+              const double weight = kmer2weight (kmer. seq);
+              ASSERT (weight > 0.0);
+              id2weight [id] += weight;
+            }
         }
       size_t id_best = no_index;
-      size_t n_max = 0;
-      for (const auto& it : id2num)
-        if (maximize (n_max, it. second))
+      double weight_max = 0;
+      for (const auto& it : id2weight)
+        // ignore small it.second ??
+        if (maximize (weight_max, it. second))
           id_best = it. first;
       bests << id_best;
 	  }
@@ -139,23 +165,27 @@ struct ThisApplication : Application
   	  addPositional ("fasta1", "Protein FASTA file 1");
   	  addPositional ("fasta2", "Protein FASTA file 2");
   	  addKey ("k", "k-mer size, >= 3", "5");
+  	  addKey ("min_prot_len", "Min. protein length", "0");
   	}
 
 
 
 	void body () const final
 	{
-		const string fName1 = getArg ("fasta1");
-		const string fName2 = getArg ("fasta2");
-		const size_t k      = (size_t) arg2uint ("k");
+		const string fName1  = getArg ("fasta1");
+		const string fName2  = getArg ("fasta2");
+		const size_t k       = (size_t) arg2uint ("k");
+		const size_t len_min = (size_t) arg2uint ("min_prot_len");
 		QC_ASSERT (k >= 3);  // PAR
 		
 		
-		const StringVector seqs1 (readSeqs (fName1));
-		const StringVector seqs2 (readSeqs (fName2));
+		const unordered_set<string> seqs1 (readSeqs (fName1, len_min));
+		const unordered_set<string> seqs2 (readSeqs (fName2, len_min));
 		
 		const Vector<size_t> bests1 (getBests (seqs1, seqs2, k));
 		const Vector<size_t> bests2 (getBests (seqs2, seqs1, k));
+		
+		// For approximate k-mer symbets run Needleman-Wunsch ??!
 		
 		size_t symbet = 0;
 		FFOR (size_t, i, bests1. size ())
