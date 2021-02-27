8f980b27d2787feb9c54fda8b996af6c6db089d5
Slava Brover <vbrover@gmail.com>
2021-02-26 20:00:17 -0500

    input dissimilarities are in one line per object pair

--- dissim/combine_dissims.cpp
+++ dissim/combine_dissims.cpp
@@ -43,50 +43,35 @@ using namespace DM_sp;
 
 
 
-#undef WEIGHT 
-
-
-
 namespace 
 {
 	
 	
 struct Scale
 {
-	Real unit {inf};
-	Real raw_min {0.0};
-	Real raw_max {0.0};
-#ifdef WEIGHT
-	Real weight {NaN};
-#endif
+	Real unit {NaN};
+	Real raw_max {NaN};
+
 	
 	Scale (const string &line,
 	       Real coeff)
 	  {
-	  	static Istringstream iss;
-	  	iss. reset (line);
-	  	iss >> unit >> raw_min >> raw_max 
-        #ifdef WEIGHT
-	  	    >> weight
-	  	  #endif
-	  	  ;
+	  	istringstream iss (line);
+	  	string raw_maxS;
+	  	iss >> unit >> raw_maxS;
 	  	unit *= coeff;
-	  	ASSERT (unit > 0.0);
-	  //ASSERT (unit <= 1.0); 
-	  	ASSERT (raw_min >= 0.0);
-	  	ASSERT (raw_min <= raw_max);
-    #ifdef WEIGHT
-	  	ASSERT (weight > 0.0);
-	  #endif
+	  	raw_max = str2real (raw_maxS);
+	  	QC_ASSERT (unit > 0.0);
+	  	QC_ASSERT (raw_max > 0.0);
+	  	ASSERT (! isNan (dissim_max ()));
 	  }
 	Scale () = default;
+
 	  
 	Real raw2dissim (Real raw) const
 	  { if (isNan (raw))
 	  	  return NaN;
-	  	ASSERT (raw >= 0.0);
-	  	if (raw < raw_min)
-	  	  return NaN;
+	  	QC_ASSERT (raw >= 0.0);
 	  	if (raw > raw_max)
 	  	  return NaN;
 	  	return raw * unit; 
@@ -97,40 +82,16 @@ struct Scale
 	
 	
 	
-struct ObjPair
-{
-	string name1;
-	string name2;
-	Real dissim {NaN};
 	
-	explicit ObjPair (const string &line)
-	  { 
-	  	static Istringstream iss;
-	  	iss. reset (line);
-	  	static string dissimS;
-	  	dissimS. clear ();
-	  	iss >> name1 >> name2 >> dissimS;
-	  	ASSERT (iss. eof ());
-	  	if (name1 > name2)
-	  	  swap (name1, name2);
-	  	ASSERT (name1 < name2);
-	  	dissim = str2real (dissimS);
-	  	IMPLY (! isNan (dissim), dissim >= 0.0);
-	  }
-};
-
-
-
-
 struct ThisApplication : Application
 {
   ThisApplication ()
     : Application ("Combine different dissimilarities for the same pairs of objects")
     {
       version = VERSION;
-  	  addPositional ("dissims", "File with lines: <obj1> <obj2> <dissimilarity>; # lines = # object pairs times # dissimilarities");
-  	  addPositional ("scales", "File with equalizing scales and max. values for each dissimilarity, ordered by scale descending.\n\
-Line format: <unit> <raw_min> <raw_max>");
+  	  addPositional ("dissims", "File with lines: <obj1> <obj2> <dissimilarity 1> <dissimilarity 2> ...");
+  	  addPositional ("scales", "File with equalizing scales and max. values for each dissimilarity, ordered by <unit> * <raw_max>.\n\
+Line format: <unit> <raw_max>");
       addKey ("coeff", "Coefficient to multiply all dissimilarities, > 0", "1");
   	}
 
@@ -141,88 +102,86 @@ Line format: <unit> <raw_min> <raw_max>");
 		const string dissimFName = getArg ("dissims");
 		const string scaleFName  = getArg ("scales");
 		const Real coeff         = str2real (getArg ("coeff"));
-		ASSERT (coeff > 0.0);
+		QC_ASSERT (coeff > 0.0);
 		
 		
 		Vector<Scale> scales;  scales. reserve (16); // PAR
 		{
-			LineInput f (scaleFName);
+			LineInput f (scaleFName);			
 			while (f. nextLine ())
 			{
 	      scales << Scale (f. line, coeff);
 	      const size_t i = scales. size ();
 	      if (i >= 2)
-	      {
-	      	if (  scales [i - 2]. unit 
-	      		  > scales [i - 1]. unit
-	      		 )
-	      	  throw runtime_error ("Scale units must increase");
-	      	if (  scales [i - 2]. dissim_max () 
-	      		  > scales [i - 1]. dissim_max ()
+	      	if (   scales [i - 2]. dissim_max () 
+	      		  >= scales [i - 1]. dissim_max ()
 	      		 )
 	      	  throw runtime_error ("dissim_max must increase");
-	      }
 	    }
 		}
 		QC_ASSERT (scales. size () >= 2);
 
       
-    Vector<ObjPair> objPairs;  objPairs. reserve (1000);  // PAR
+  	LineInput f (dissimFName, 1024 * 100, 1);
+  	string name1;
+  	string name2;
+  	string dissimS;
+   	Istringstream iss;
+  	while (f. nextLine ())
     {
-    	LineInput f (dissimFName);
-    	while (f. nextLine ())
-    		objPairs << move (ObjPair (f. line));
-    }
+	  	iss. reset (f. line);
+	  	iss >> name1 >> name2;
+	  	if (name1 > name2)
+	  	  swap (name1, name2);
+	  	ASSERT (name1 < name2);
 
-    QC_ASSERT (objPairs. size () % scales. size () == 0);
-    const size_t n = objPairs. size () / scales. size ();  // # Pairs
-    
-    const ONumber on (cout, 6, true);
-    Progress prog (n, 1000);  // PAR
-	  FOR (size_t, i, n)
-	  {
-	  	prog ();
-	  #ifdef WEIGHT
-	    Real dissim_sum = 0.0;
-	    Real weight_sum = 0.0;
-	  #else
 	    Real dissim = NaN;
-	  #endif
-  	  FFOR (size_t, j, scales. size ())
-      {
-      	const size_t k = i + j * n;
-        if (objPairs [k]. name1 != objPairs [i]. name1)
-        	throw runtime_error ("Object " + toString (i + 1) + ", scale " + toString (j + 1) 
-        	                     + " has a different name1: " + objPairs [k]. name1 + " vs. " + objPairs [i]. name1
-        	                    );
-        ASSERT (objPairs [k]. name2 == objPairs [i]. name2);
-        const Real raw = objPairs [k]. dissim;  // isNan (objPairs [k]. dissim) ? NaN : max (dissim_max_prev, objPairs [k]. dissim);  // Bad
-  	  #ifdef WEIGHT
-        const Real dissim = scales [j]. raw2dissim (raw);
+	    Real dissim_prev = NaN;	    
+	  	FFOR (size_t, i, scales. size ())
+	  	{
+	  	  dissimS. clear ();
+	  	  iss >> dissimS;
+	  	  QC_ASSERT (! dissimS. empty ());
+  	  	const Real raw = str2real (dissimS);
+  	  	QC_IMPLY (! isNan (raw), raw >= 0.0);
+        dissim = scales [i]. raw2dissim (raw);
+        if (dissim == 0.0 && i) 
+          dissim = NaN;
         if (isNan (dissim))
-        	continue;
-        ASSERT (dissim >= 0.0);
-        dissim_sum += scales [j]. weight * dissim;
-        weight_sum += scales [j]. weight;
-      #else
-        dissim = scales [j]. raw2dissim (raw);
-        if (! isNan (dissim))
+        {
+          if (! isNan (dissim_prev))
+          {
+            dissim = dissim_prev;
+            break;
+          }
+        }
+        else
         {
           ASSERT (dissim >= 0.0);
-          if (! dissim && j)  // Contradiction
-            dissim = NaN;
-          break;
+          if (isNan (dissim_prev))
+            dissim_prev = dissim;
+          else
+          {
+            // Merging dissimilarities of adjacent scales
+            ASSERT (i);
+            const Real prev_max = scales [i - 1]. dissim_max ();
+            ASSERT (dissim_prev <= prev_max);
+            const Real prev_prev_max = (i - 1 ? scales [i - 2]. dissim_max () : 0.0);
+            ASSERT (prev_prev_max < prev_max);
+            if (dissim_prev <= prev_prev_max)
+              dissim = dissim_prev;
+            else
+            {
+              const Prob weight = (dissim_prev - prev_prev_max) / (prev_max - prev_prev_max);
+              dissim = weight * dissim + (1.0 - weight) * dissim_prev;
+            }
+            break;
+          }
         }
-      #endif
       }
-      cout         << objPairs [i]. name1 
-           << '\t' << objPairs [i]. name2
-           << '\t' << 
-		       #ifdef WEIGHT
-             dissim_sum / weight_sum
-           #else
-             dissim 
-           #endif
+      cout         << name1 
+           << '\t' << name2
+           << '\t' << dissim 
            << endl;
     }
 	}
