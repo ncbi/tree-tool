0c0343522500a87ddd1a1553e81e8464759b8700
Slava Brover <vbrover@gmail.com>
2021-03-04 18:23:55 -0500

    new

---
+++ check_tmp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash --noprofile
+THIS=`dirname $0`
+source $THIS/bash_common.sh
+
+AVAILABLE=`df -k /tmp | tail -1 | awk '{print $4};'`
+if [ $AVAILABLE -lt 1000000 ]; then  # PAR
+  error "Space available on /tmp is $AVAILABLE Kb"
+fi
