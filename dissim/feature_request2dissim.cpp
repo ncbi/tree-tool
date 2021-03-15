2f68e53ee27232fc0ff819a89149a142db33afd7
Slava Brover <vbrover@gmail.com>
2021-03-14 20:52:26 -0400

    -dna_weight

--- dissim/feature_request2dissim.cpp
+++ dissim/feature_request2dissim.cpp
@@ -46,6 +46,29 @@ using namespace DM_sp;
 namespace 
 {
   
+
+void vec2dna_prot (const ObjFeatureVector &vec, 
+                   ObjFeatureVector &dnaVec, 
+                   ObjFeatureVector &protVec)
+// Output: dnaVec, protVec
+{
+  ASSERT (dnaVec. empty ());
+  ASSERT (protVec. empty ());
+  dnaVec.  reserve (vec. size());
+  protVec. reserve (vec. size());
+  for (const ObjFeature& of : vec)
+    if (contains (of. name, '-'))  // Protein Mutation's have geneName
+      protVec << of;
+    else
+      dnaVec << of;
+  dnaVec. sort ();
+  ASSERT (dnaVec. isUniq ());
+  protVec. sort ();
+  ASSERT (protVec. isUniq ());  
+}
+
+
+  
   
 struct ThisApplication : Application
 {
@@ -56,8 +79,9 @@ struct ThisApplication : Application
   	  addPositional ("req", "File with pairs of objects");
   	  addPositional ("dir", "Directory with <object> files containing feature-optional(0/1) pairs");
   	  addKey ("optional_weight", "For Hamming distance: Weight of optional-nonoptional match (0..1)", "NaN");
-  	  addKey ("mutation_rate", "For SNP dissimilarity: File where each line has format: <feature_name> <mutation rate 0->1> <mutation rate 1->0>");
+  	  addKey ("mutation_rate", "For SNP dissimilarity: file where each line has format: <feature_name> <mutation rate 0->1> <mutation rate 1->0>");
   	//addFlag ("freq", "<mutation_rate> file has allele frequencies");
+  	  addKey ("dna_weight", "Weight of DNA mutations (0..1). Protein mutations are weighted as 1. Features are DNA or protein mutations, where DNA is not named and proteins are named", "NaN");
   	}
 
 
@@ -66,15 +90,21 @@ struct ThisApplication : Application
 	{
 		const string reqFName        =           getArg ("req");
 		const string dir             =           getArg ("dir");
-		const Real   optional_weight = str2real (getArg ("optional_weight"));
+		const Prob   optional_weight = str2real (getArg ("optional_weight"));
     const string mutationsFName  =           getArg ("mutation_rate");
-  //const bool freq              =           getFlag ("freq");
+  //const bool   freq            =           getFlag ("freq");
+    const Prob   dnaWeight       = str2real (getArg ("dna_weight"));
 		
 		if (! isNan (optional_weight))
 		{
-  		QC_ASSERT (optional_weight >= 0.0);
-  		QC_ASSERT (optional_weight <= 1.0);
+  		QC_ASSERT (isProb (optional_weight));
   		QC_ASSERT (mutationsFName. empty ());
+    }    
+    
+    if (! isNan (dnaWeight))
+    { 
+      QC_ASSERT (isProb (dnaWeight));    
+      QC_ASSERT (mutationsFName. empty ());
     }
 		
 		
@@ -109,7 +139,21 @@ struct ThisApplication : Application
       const ObjFeatureVector vec1 (dir + "/" + obj1);
       const ObjFeatureVector vec2 (dir + "/" + obj2);
     #endif
-      cout << obj1 << '\t' << obj2 << '\t' << features2dissim (vec1, vec2, optional_weight, *feature2rate) << endl;
+      Real dissim = NaN;
+      if (isNan (dnaWeight))
+        dissim = features2dissim (vec1, vec2, optional_weight, *feature2rate);
+      else
+      {
+        ObjFeatureVector dnaVec1, protVec1;
+        ObjFeatureVector dnaVec2, protVec2;
+        vec2dna_prot (vec1, dnaVec1, protVec1);
+        vec2dna_prot (vec2, dnaVec2, protVec2);
+        const Real dnaDissim  = features2hamming (dnaVec1,  dnaVec2,  optional_weight);
+        const Real protDissim = features2hamming (protVec1, protVec2, optional_weight);
+        dissim = dnaWeight * dnaDissim + protDissim;  
+      }
+      ASSERT (dissim >= 0.0);
+      cout << obj1 << '\t' << obj2 << '\t' << dissim << endl;
     }
 	}
 };
