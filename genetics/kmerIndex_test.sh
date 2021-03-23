68f57f9ca850a40de483091aabead1e384e35b85
Slava Brover <vbrover@gmail.com>
2021-03-23 10:32:59 -0400

    section

--- genetics/kmerIndex_test.sh
+++ genetics/kmerIndex_test.sh
@@ -12,16 +12,23 @@ TMP=`mktemp`
 echo $TMP
 
 
+section "make"
 $THIS/kmerIndex_make $TMP.kmi 14 -qc
+
+section "add"
 $THIS/kmerIndex_add  $TMP.kmi $THIS/data/5_8S.fa  -qc
+
+section "stat"
 $THIS/kmerIndex_stat $TMP.kmi -qc
 
+section "$TMP.seq/"
 $THIS/fa2list.sh $THIS/data/5_8S.fa | sort -R > $TMP.list-all
 head $TMP.list-all | sort > $TMP.list
 $THIS/extractFastaDna $THIS/data/5_8S.fa $TMP.list -qc > $TMP.fa
 mkdir $TMP.seq
 $THIS/splitFasta $TMP.fa $TMP.seq -qc 
 
+section "find"
 mkdir $TMP.out
 $THIS/../trav $TMP.seq "$THIS/kmerIndex_find $TMP.kmi %d/%f 100 -qc -self > $TMP.out/%f"
 $THIS/../trav $TMP.out '[ `cat %d/%f | wc -l` == 100 ]'
