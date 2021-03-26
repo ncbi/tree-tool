55c5b4795b7b065825463bdfa9dbc86c69e83837
Slava Brover <vbrover@gmail.com>
2021-03-26 11:11:42 -0400

    cosmetic

--- phylogeny/tree2outlier.sh
+++ phylogeny/tree2outlier.sh
@@ -3,9 +3,11 @@ THIS=`dirname $0`
 source $THIS/../bash_common.sh
 if [ $# -ne 1 ]; then
   echo "Print objects sorted by criterion descending"
-  echo "#1: Distance tree"
+  echo "#1: distance tree"
   exit 1
 fi
+TREE=$1
 
-grep 'leaf_error=' $1 | sed 's/:.* leaf_error=/#/1' | sed 's/^ *//1' | sed 's/ \+[^ ]*$//1' | tr '#' ' ' | sort -k 2 -r -g
+
+grep 'leaf_error=' $TREE | sed 's/:.* leaf_error=/#/1' | sed 's/^ *//1' | sed 's/ \+[^ ]*$//1' | tr '#' ' ' | sort -k 2 -r -g
 
