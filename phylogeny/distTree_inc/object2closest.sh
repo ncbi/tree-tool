1bd73acdd7ec3b624c0ad7002bf26fff9a491cbf
Slava Brover <vbrover@gmail.com>
2021-04-11 20:37:17 -0400

    parameters bug

--- phylogeny/distTree_inc/object2closest.sh
+++ phylogeny/distTree_inc/object2closest.sh
@@ -1,12 +1,13 @@
 #!/bin/bash --noprofile
 source bash_common.sh
-if [ $# -ne 1 ]; then
-  echo "Print 100 approximately closest objects to #1"
-  echo "#1: object id"
-  echo "Requires: Time: O(log^4(n))"
+if [ $# -ne 2 ]; then
+  echo "Find closest objects"
+  echo "#1: object"
+  echo "#2: directory or ''"
   exit 1
 fi
 OBJ=$1
+DIR="$2"
 
 
 error "$0 is not implemented"
--- phylogeny/distTree_inc/pairs2dissim.sh
+++ phylogeny/distTree_inc/pairs2dissim.sh
@@ -1,15 +1,20 @@
 #!/bin/bash --noprofile
 source bash_common.sh
-if [ $# -ne 3 ]; then
-  echo "Compute dissimilarities"
-  echo "#1: file with pairs: <Object1> <Object2>"
-  echo "#2: output file with triples:  <Object1> <Object2> <dissimilarity>"
-  echo "#3: log (temporary)"
+if [ $# -ne 4 ]; then
+  echo "Compute dissimilarities for pairs of objects"
+  echo "#1: input dissimilarity requests (pairs of objects)"
+  echo "#2: new object file or directory, or ''. Object name is basename"
+  echo "#3: output dissimilarities added to the pairs of objects"
+  echo "#4: error log"
   exit 1
 fi
-REQ=$1
-OUT=$2
-LOG=$3
+REQUEST=$1
+FILE_NEW="$2"
+DISSIM=$3
+LOG=$4
 
 
 error "$0 is not implemented"
+
+
+rm -f $LOG
