d116387d43d9ab9e654187c4fccb2cc0457d9d60
Slava Brover <vbrover@gmail.com>
2021-04-12 20:27:54 -0400

    new

---
+++ dissim/seq2dissim.cpp
@@ -0,0 +1,229 @@
+// seq2dissim.cpp
+
+/*===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE                          
+*               National Center for Biotechnology Information
+*                                                                          
+*  This software/database is a "United States Government Work" under the   
+*  terms of the United States Copyright Act.  It was written as part of    
+*  the author's official duties as a United States Government employee and 
+*  thus cannot be copyrighted.  This software/database is freely available 
+*  to the public for use. The National Library of Medicine and the U.S.    
+*  Government have not placed any restriction on its use or reproduction.  
+*                                                                          
+*  Although all reasonable efforts have been taken to ensure the accuracy  
+*  and reliability of the software and data, the NLM and the U.S.          
+*  Government do not and cannot warrant the performance or results that    
+*  may be obtained by using this software or data. The NLM and the U.S.    
+*  Government disclaim all warranties, express or implied, including       
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.                                                                
+*                                                                          
+*  Please cite the author in any work or product based on this material.   
+*
+* ===========================================================================
+*
+* Author: Vyacheslav Brover
+*
+* File Description:
+*   Align and compute dissimilarity for a pair of sequences.
+*
+*/
+
+
+#undef NDEBUG
+#include "../common.inc"
+
+#include "../common.hpp"
+using namespace Common_sp;
+#include "../genetics/seq.hpp"
+using namespace Seq_sp;
+#include "align.hpp"
+#include "../version.inc"
+
+
+
+namespace 
+{
+	
+
+Seq* readSeq (const string &fName,
+              bool aa)
+{
+  LineInput in (fName);
+  EXEC_ASSERT (in. nextLine ());
+  Seq* seq = nullptr;
+  if (aa)
+    seq = new Peptide (in, 10 * 1024, false);  // PAR
+  else
+    seq = new Dna (in, 1024 * 1024, false);  // PAR
+  QC_ASSERT (seq);
+  seq->qc ();
+  
+  return seq;
+}
+
+
+
+struct ThisApplication : Application
+{
+  
+  ThisApplication ()
+    : Application ("Align and compute dissimilarity for a pair of sequences.\nPrint: ref_match: <target start>-<target stop> (human coordinates)")
+    {
+      version = VERSION;
+  	  addPositional ("target", "FASTA file 1 with a sequence (target)");
+  	  addPositional ("reference", "FASTA file 1 with a sequence (reference)");
+  	  addKey ("prot_name", "Protein name. Empty string means that sequences are DNA");
+  	  addFlag ("global", "Global alignment, otherwise semiglobal");
+  	  addKey ("match_len_min", "Min. match length. Valid for semiglobal alignment", "60");
+  	  addKey ("mutation", "file for mutations");
+	  	addFlag ("ambig_start", "Replace deletions at the start of <FASTA1> by the ambiguity character");
+           	  //ambig_end ??
+      addFlag ("blosum62", "Use substitution matrix BLASUM62, otehrwise PAM30. valid for protein sequences");
+    }
+
+
+	
+	void body () const final
+  {
+	  const string targetFName = getArg ("target");
+	  const string refFName    = getArg ("reference");
+	  const string protName    = getArg ("prot_name");
+	  const bool global        = getFlag ("global");
+	        size_t match_len_min = str2<size_t> (getArg ("match_len_min"));
+	  const string mutFName    = getArg ("mutation");
+    const bool   ambig_start = getFlag ("ambig_start");
+    const bool   blosum62    = getFlag ("blosum62");
+    
+
+    if (global)
+    	match_len_min = 0;
+    else
+    	if (match_len_min == 0)
+    		throw runtime_error ("match_len_min cannot be 0 for a semiglobal alignment");
+
+    const bool aa = ! protName. empty ();
+    
+    if (! aa && blosum62)
+      throw runtime_error ("-blasum62 requires protein sequences");
+      
+
+    unique_ptr<const Seq> targetSeq (readSeq (targetFName, aa));
+    unique_ptr<const Seq> refSeq    (readSeq (refFName,    aa));
+    ASSERT (targetSeq. get ());
+    ASSERT (refSeq. get ());
+
+    unique_ptr<Align_sp::Align> align;
+    if (aa)
+      align. reset (new Align_sp::Align (* targetSeq->asPeptide (), * refSeq->asPeptide (), ! global, match_len_min, blosum62));
+    else
+      align. reset (new Align_sp::Align (* targetSeq->asDna (), * refSeq->asDna (), ! global, match_len_min, 0 /*band*/));
+    ASSERT (align. get ());
+		
+		if (verbose ())
+		  align->saveText (cout);
+		
+		cout << "Identity = " << align->matches << '/' << align->tr. size () 
+		     << " (" << ((double) align->matches / (double) align->tr. size () * 100) << "%)" << endl;
+		cout << "Min. edit distance = " << align->getMinEditDistance () << endl;
+		cout << endl;
+		
+		align->setAlignment (targetSeq->seq, refSeq->seq);
+
+ 		size_t targetStart = 0;
+		size_t targetStop = targetSeq->seq. size ();
+    {		
+  		while (align->sparse2 [targetStart] == '-')
+  	  {
+  	    ASSERT (align->sparse1 [targetStart] != '-');
+  		  targetStart++;
+  		}
+  		
+  		size_t i = align->sparse2. size ();
+  		while (align->sparse2 [i - 1] == '-')
+  	  {
+  	    ASSERT (align->sparse1 [i] != '-');
+  	    ASSERT (i);
+  	    i--;
+  	    ASSERT (targetStop);
+  		  targetStop--;
+  		}
+    }
+		if (targetStart >= targetStop)
+		  throw runtime_error ("No alignment");
+		cout << "ref_match: " << targetStart + 1 << ' ' << targetStop << endl;
+  		
+
+		if (! mutFName. empty ())
+		{
+		  OFStream f (mutFName);
+		  size_t refPos = 0;
+  		size_t mismatchStart = no_index;  
+  		size_t refStart = no_index;
+  		FFOR (size_t, i, align->sparse1. size ())
+  	  {
+  		  if (align->sparse1 [i] == align->sparse2 [i])
+  		  {
+  		    if (mismatchStart != no_index)
+  		    {
+  		      ASSERT (mismatchStart < i);
+  		      const size_t len = i - mismatchStart;
+  		      string ref    (align->sparse2. substr (mismatchStart, len));
+  		      string allele (align->sparse1. substr (mismatchStart, len));
+  		      replaceStr (ref,      "-", "");
+  		      replaceStr (allele,   "-", "");
+  		      ASSERT (ref != allele);
+  		      ASSERT (refStart <= refSeq->seq. size ());
+  		      const bool outside =     ref. empty ()
+                  		           && (   refStart == 0 
+                  		               || refStart == refSeq->seq. size ()
+                  		              );
+            if (! outside)
+            {
+              if (ambig_start && refStart == 0)
+              {
+                ASSERT (! ref. empty ());
+                allele = string (ref. size (), aa ? 'X' : 'n');
+              }
+    		      const Mutation mut (protName, refStart, ref, allele);
+    		      mut. qc ();
+    		      f << mut << endl;
+    		    }
+  		      mismatchStart = no_index;
+  		      refStart = no_index;
+  		    }
+  		  }
+  		  else
+  		    if (mismatchStart == no_index)
+  		    {
+  		      mismatchStart = i;
+  		      refStart = refPos;
+  		    }
+  		  if (align->sparse2 [i] != '-')
+  		    refPos++;
+  		}
+  		ASSERT (refPos == refSeq->seq. size ());
+    }
+
+
+		align->printAlignment (60);  // PAR
+  }
+};
+
+
+}  // namespace
+
+
+
+
+int main (int argc, 
+          const char* argv[])
+{
+  ThisApplication app;
+  return app. run (argc, argv);
+}
+
+
+
