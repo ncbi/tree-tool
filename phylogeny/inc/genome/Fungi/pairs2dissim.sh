17acbcb7458703025f2b1e0c8354dcb4ee4c4a9e
Slava Brover <vbrover@gmail.com>
2021-04-15 18:03:21 -0400

    inc/ files

---
+++ phylogeny/inc/genome/Fungi/pairs2dissim.sh
@@ -0,0 +1,23 @@
+#!/bin/bash --noprofile
+source CPP_DIR/bash_common.sh
+if [ $# -ne 4 ]; then
+  echo "Compute dissimilarities for pairs of objects"
+  echo "#1: input dissimilarity requests (pairs of objects)"
+  echo "#2: new object file or directory, or ''. Object name is basename"
+  echo "#3: output dissimilarities added to the pairs of objects"
+  echo "#4: error log"
+  exit 1
+fi
+REQ=$1
+FILE_NEW="$2"
+OUT=$3
+LOG=$4
+
+
+INC=`dirname $0`
+# PAR
+CPP_DIR/dissim/combine_dissims.sh $REQ $INC/../genome "$FILE_NEW" $OUT 0 10 0.1 $INC/dissim_scale $INC/hmm-univ.stat 1  0.57 $LOG
+#                                         1    2              3           4    5 6  7   8                  9                 10 11   12
+
+
+
---
+++ phylogeny/inc/genome/Protists/object2closest.sh
@@ -0,0 +1,29 @@
+#!/bin/bash --noprofile
+source CPP_DIR/bash_common.sh
+if [ $# -ne 2 ]; then
+  echo "Find closest genomes"
+  echo "#1: Genome.id"
+  echo "#2: directory or ''"
+  exit 1
+fi
+GENOME=$1
+DIR="$2"
+
+
+INC=`dirname $0`
+SERVER=`cat $INC/server`
+DATABASE=`cat $INC/database`
+BULK_REMOTE=`cat $INC/bulk_remote`
+
+if [ -z $DIR ]; then
+  DIR=$INC/../genome
+  if [ -e $INC/large ]; then
+    H=`CPP_DIR/file2hash $GENOME`
+    DIR=$DIR/$H
+  fi
+fi
+
+CPP_DIR/phylogeny/database/Genome_hash_requestClosest.sh $SERVER $DATABASE $INC/bulk $BULK_REMOTE $GENOME $DIR PRT
+
+
+
