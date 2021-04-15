b9f03e1ca4ec5c1aeda51863cd48635a31c70fe9
Slava Brover <vbrover@gmail.com>
2021-04-15 18:08:21 -0400

    LARGE, database can be ''

--- phylogeny/distTree_inc_init_stnd.sh
+++ phylogeny/distTree_inc_init_stnd.sh
@@ -5,10 +5,10 @@ if [ $# -ne 6 ]; then
   echo "Initialize an incremental distance tree directory with standard parameters for a specific biological project in #1/"
   echo "#1: output directory"
   echo "#2: biological project (incremental distance tree directory)"
-  echo "#3: SQL server name"
-  echo "#4: database name on the SQL server"
-  echo "#5: complete path to a directory for bulk insert into the database"
-  echo "#6: path in Universal Naming Convention to the bulk directory #5"
+  echo "#3: SQL server name or ''"
+  echo "#4: database name on the SQL server or ''"
+  echo "#5: complete path to a directory for bulk insert into the database or ''"
+  echo "#6: path in Universal Naming Convention to the bulk directory #5 or ''"
   exit 1
 fi
 INC=$1
@@ -23,11 +23,16 @@ if [ ! -e $FROM/variance ]; then
   error "$FROM must be an incremental distance tree directory"
 fi
 
-( sqsh-ms  -S $SERVER  -D $DATABASE  -m bcp  -C "select @@version" > /dev/null ) || error "Database is not available"
+if [ $SERVER ]; then
+  ( sqsh-ms  -S $SERVER  -D $DATABASE  -m bcp  -C "select @@version" &> /dev/null ) || error "Database is not available"
+fi
 
-
-$THIS/distTree_inc_init.sh $INC 1 "" 0 NAN NAN "" 0 0 "$SERVER" "$DATABASE" "$BULK_LOCAL" "$BULK_REMOTE"
-#                          1    2 3  4 5   6   7  8 9 10        11          12            13
+LARGE=0
+if [ -e $FROM/large ]; then
+  LARGE=1
+fi
+$THIS/distTree_inc_init.sh $INC 1 "" 0 NAN NAN "" $LARGE 0 "$SERVER" "$DATABASE" "$BULK_LOCAL" "$BULK_REMOTE"
+#                          1    2 3  4 5   6   7  8      9 10        11          12            13
 cp $FROM/* $INC/
 
 
