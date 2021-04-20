8062a96b7aed50d18b3c105b507d4cbac52f2d83
Slava Brover <vbrover@gmail.com>
2021-04-20 16:57:53 -0400

    output common hashes

--- phylogeny/database/genomes2hashes.sh
+++ phylogeny/database/genomes2hashes.sh
@@ -1,18 +1,20 @@
 #!/bin/bash --noprofile
 THIS=`dirname $0`
 source $THIS/../../bash_common.sh
-if [ $# != 4 ]; then
+if [ $# != 5 ]; then
   echo "Print the number of common hashes for 2 genomes"
   echo "#1: genome/"
   echo "#2: Genome.id"
   echo "#3: Genome.id"
   echo "#4: CDS|PRT|HMM"
+  echo "#5: out file with common hashes or ''"
   exit 1
 fi
 DIR=$1
 G1=$2
 G2=$3
 TYPE=$4
+OUT="$5"
 
 
 H1=`$THIS/../../file2hash $G1`
@@ -23,4 +25,8 @@ N1=`cat $F1 | wc -l`
 N2=`cat $F2 | wc -l`
 COMMON=`$THIS/../../setIntersect.sh $F1 $F2 1 | wc -l`
 echo "$G1 $G2 $N1 $N2 $COMMON"
+
+if [ $OUT ]; then
+  $THIS/../../setIntersect.sh $F1 $F2 1 > $OUT
+fi
 
