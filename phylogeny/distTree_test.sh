090bfb95d4cd55d2fba40de524db7a26dcfe3cc1
Slava Brover <vbrover@gmail.com>
2021-05-12 16:39:40 -0400

    data/ --> $DATA/

--- phylogeny/distTree_test.sh
+++ phylogeny/distTree_test.sh
@@ -14,123 +14,126 @@ if [ ${AT_NCBI:-0} == 1 ]; then
 fi
 
 
+DATA=$THIS/data
+
+
 #if false; then  
 section "mdsTree: Enterobacteriaceae"
-rm -rf data/Enterobacteriaceae.dir/
-$THIS/../dm/mdsTree.sh data/Enterobacteriaceae Conservation 2  &> $TMP.out
-$THIS/makeDistTree  -qc -input_tree data/Enterobacteriaceae.dir/  -data data/Enterobacteriaceae  -dissim_attr Conservation  -variance linExp  -optimize  -output_tree Enterobacteriaceae.tree > Enterobacteriaceae.distTree
-$THIS/distTree_compare_criteria.sh Enterobacteriaceae.distTree data/Enterobacteriaceae.distTree
+rm -rf $DATA/Enterobacteriaceae.dir/
+$THIS/../dm/mdsTree.sh $DATA/Enterobacteriaceae Conservation 2  &> $TMP.out
+$THIS/makeDistTree  -qc -input_tree $DATA/Enterobacteriaceae.dir/  -data $DATA/Enterobacteriaceae  -dissim_attr "Conservation"  -variance linExp  -optimize  -output_tree Enterobacteriaceae.tree > Enterobacteriaceae.distTree
+$THIS/distTree_compare_criteria.sh Enterobacteriaceae.distTree $DATA/Enterobacteriaceae.distTree
 rm Enterobacteriaceae.distTree
-rm -r data/Enterobacteriaceae.dir/
+rm -r $DATA/Enterobacteriaceae.dir/
 
 section "To Newick"
-$THIS/printDistTree  -qc  -data data/Enterobacteriaceae  -dissim_attr Conservation  -variance linExp  Enterobacteriaceae.tree  -order  -decimals 4  -ext_name > Enterobacteriaceae.nw
-diff Enterobacteriaceae.nw data/Enterobacteriaceae.nw
+$THIS/printDistTree  -qc  -data $DATA/Enterobacteriaceae  -dissim_attr "Conservation"  -variance linExp  Enterobacteriaceae.tree  -order  -decimals 4  -ext_name > Enterobacteriaceae.nw
+diff Enterobacteriaceae.nw $DATA/Enterobacteriaceae.nw
 rm Enterobacteriaceae.nw
 
 rm Enterobacteriaceae.tree
 
 section "From Newick"
-$THIS/newick2tree -qc data/Enterobacteriaceae.nw > Enterobacteriaceae.tree
+$THIS/newick2tree -qc $DATA/Enterobacteriaceae.nw > Enterobacteriaceae.tree
 $THIS/printDistTree -qc Enterobacteriaceae.tree  -decimals 4  -ext_name > Enterobacteriaceae.nw
-diff Enterobacteriaceae.nw data/Enterobacteriaceae.nw
+diff Enterobacteriaceae.nw $DATA/Enterobacteriaceae.nw
 rm Enterobacteriaceae.nw
 rm Enterobacteriaceae.tree
 
 section "Perfect tree"
-$THIS/makeDistTree  -qc  -data data/tree4  -variance linExp  -optimize  -output_tree tree4 | grep -v '^CHRON: ' > tree4.makeDistTree
-$THIS/distTree_compare_criteria.sh tree4.makeDistTree data/tree4.makeDistTree
+$THIS/makeDistTree  -qc  -data $DATA/tree4  -variance linExp  -optimize  -output_tree tree4 | grep -v '^CHRON: ' > tree4.makeDistTree
+$THIS/distTree_compare_criteria.sh tree4.makeDistTree $DATA/tree4.makeDistTree
 rm tree4.makeDistTree
 $THIS/statDistTree tree4 > tree4.stat
 rm tree4
-diff tree4.stat data/tree4.stat
+diff tree4.stat $DATA/tree4.stat
 rm tree4.stat
 echo "Verbose ..."
-$THIS/makeDistTree -qc  -data data/tree4  -variance linExp  -optimize  -verbose 2 &> $TMP.out
+$THIS/makeDistTree -qc  -data $DATA/tree4  -variance linExp  -optimize  -verbose 2 &> $TMP.out
 
 section "Random tree"
-$THIS/makeDistTree  -qc   -data data/randomTree  -variance lin  -output_tree random-output.tree > $TMP.out
-$THIS/makeDistTree  -qc  -input_tree random-output.tree    -data data/randomTree  -variance lin  -optimize | grep -v '^CHRON: ' > randomTree.makeDistTree
-$THIS/distTree_compare_criteria.sh randomTree.makeDistTree data/randomTree.makeDistTree
+$THIS/makeDistTree  -qc   -data $DATA/randomTree  -variance lin  -output_tree random-output.tree > $TMP.out
+$THIS/makeDistTree  -qc  -input_tree random-output.tree    -data $DATA/randomTree  -variance lin  -optimize | grep -v '^CHRON: ' > randomTree.makeDistTree
+$THIS/distTree_compare_criteria.sh randomTree.makeDistTree $DATA/randomTree.makeDistTree
 echo "Verbose ..."
-$THIS/makeDistTree  -qc  -input_tree random-output.tree    -data data/randomTree  -variance lin  -verbose 2 &> $TMP.out
+$THIS/makeDistTree  -qc  -input_tree random-output.tree    -data $DATA/randomTree  -variance lin  -verbose 2 &> $TMP.out
 rm randomTree.makeDistTree
 rm random-output.tree
 
 section "Salmonella"
 # Check time ??
-$THIS/makeDistTree  -qc  -data data/Salmonella  -variance linExp  -optimize  -subgraph_iter_max 10 \
+$THIS/makeDistTree  -qc  -data $DATA/Salmonella  -variance linExp  -optimize  -subgraph_iter_max 10 \
   -delete_criterion_outliers Salmonella.criterion_outliers \
   -delete_deformation_outliers Salmonella.deformation_outliers \
   -delete_hybrids Salmonella.hybrids \
   | grep -v '^CHRON: ' > Salmonella.distTree
-diff Salmonella.criterion_outliers data/Salmonella.criterion_outliers
+diff Salmonella.criterion_outliers $DATA/Salmonella.criterion_outliers
 rm Salmonella.criterion_outliers
 $THIS/../sort.sh Salmonella.deformation_outliers
 cut -f 1      Salmonella.deformation_outliers > $TMP.Salmonella.deformation_outliers
-cut -f 1 data/Salmonella.deformation_outliers > $TMP.data.Salmonella.deformation_outliers
+cut -f 1 $DATA/Salmonella.deformation_outliers > $TMP.data.Salmonella.deformation_outliers
 diff $TMP.Salmonella.deformation_outliers $TMP.data.Salmonella.deformation_outliers
 rm Salmonella.deformation_outliers
-diff Salmonella.hybrids data/Salmonella.hybrids
+diff Salmonella.hybrids $DATA/Salmonella.hybrids
 rm Salmonella.hybrids
-$THIS/distTree_compare_criteria.sh Salmonella.distTree data/Salmonella.distTree
+$THIS/distTree_compare_criteria.sh Salmonella.distTree $DATA/Salmonella.distTree
 rm Salmonella.distTree
 
 section "testDistTree"
-$THIS/makeDistTree  -data data/Salmonella  -variance linExp  -optimize  -output_tree $TMP.tree > $TMP.out
-$THIS/testDistTree -qc  data/Salmonella  -input_tree  $TMP.tree  -variance linExp 
+$THIS/makeDistTree  -data $DATA/Salmonella  -variance linExp  -optimize  -output_tree $TMP.tree > $TMP.out
+$THIS/testDistTree -qc  $DATA/Salmonella  -input_tree  $TMP.tree  -variance linExp 
 
 section "Salmonella: delete"
-$THIS/makeDistTree  -qc  -data data/Salmonella  -variance linExp  -delete data/delete.list  -check_delete > $TMP.out
+$THIS/makeDistTree  -qc  -data $DATA/Salmonella  -variance linExp  -delete $DATA/delete.list  -check_delete > $TMP.out
 
-if [ -d data/inc.ITS ]; then
+if [ -d $DATA/inc.ITS ]; then
   section "ITS threads"
-  $THIS/makeDistTree  -threads 10  -data data/inc.ITS/  -variance linExp  -variance_dissim  -optimize  -skip_len  -reinsert  -subgraph_iter_max 1  -noqual > ITS.distTree
-  $THIS/distTree_compare_criteria.sh ITS.distTree data/ITS.distTree
+  $THIS/makeDistTree  -threads 10  -data $DATA/inc.ITS/  -variance linExp  -variance_dissim  -optimize  -skip_len  -reinsert  -subgraph_iter_max 1  -noqual > ITS.distTree
+  $THIS/distTree_compare_criteria.sh ITS.distTree $DATA/ITS.distTree
   rm ITS.distTree
 fi
 
 section "Saccharomyces hybrids"
-$THIS/makeDistTree -qc  -threads 10  -data data/Saccharomyces  -variance linExp  -optimize  -subgraph_iter_max 2  \
+$THIS/makeDistTree -qc  -threads 10  -data $DATA/Saccharomyces  -variance linExp  -optimize  -subgraph_iter_max 2  \
   -hybridness_min 1.2  -hybrid_parent_pairs Saccharomyces.hybrid_parent_pairs  -delete_hybrids Saccharomyces.hybrid  -dissim_boundary 0.675 \
   -delete_criterion_outliers Saccharomyces.criterion_outliers  -criterion_outlier_num_max 1 \
   -delete_deformation_outliers Saccharomyces.deformation_outliers  -deformation_outlier_num_max 1 \
   -output_tree $TMP.tree > $TMP.out
-diff Saccharomyces.hybrid data/Saccharomyces.hybrid
+diff Saccharomyces.hybrid $DATA/Saccharomyces.hybrid
 $THIS/hybrid2list.sh Saccharomyces.hybrid > Saccharomyces.hybrid.list
 rm Saccharomyces.hybrid
-diff Saccharomyces.hybrid_parent_pairs data/Saccharomyces.hybrid_parent_pairs
+diff Saccharomyces.hybrid_parent_pairs $DATA/Saccharomyces.hybrid_parent_pairs
 rm Saccharomyces.hybrid_parent_pairs
-diff Saccharomyces.criterion_outliers data/Saccharomyces.criterion_outliers
+diff Saccharomyces.criterion_outliers $DATA/Saccharomyces.criterion_outliers
 rm Saccharomyces.criterion_outliers
 cut -f 1      Saccharomyces.deformation_outliers > $TMP.Saccharomyces.deformation_outliers
-cut -f 1 data/Saccharomyces.deformation_outliers > $TMP.data.Saccharomyces.deformation_outliers
+cut -f 1 $DATA/Saccharomyces.deformation_outliers > $TMP.data.Saccharomyces.deformation_outliers
 diff $TMP.Saccharomyces.deformation_outliers $TMP.data.Saccharomyces.deformation_outliers
 rm Saccharomyces.deformation_outliers
 # Saccharomyces.distTree
 $THIS/tree2obj.sh $TMP.tree > $TMP.list
-$THIS/../dm/dm2subset data/Saccharomyces $TMP.list > $TMP.dm
+$THIS/../dm/dm2subset $DATA/Saccharomyces $TMP.list > $TMP.dm
 $THIS/makeDistTree  -threads 10  -data $TMP  -input_tree $TMP.tree  -variance linExp  -optimize  -reinsert  -subgraph_iter_max 10  > Saccharomyces.distTree
-$THIS/distTree_compare_criteria.sh Saccharomyces.distTree data/Saccharomyces.distTree
+$THIS/distTree_compare_criteria.sh Saccharomyces.distTree $DATA/Saccharomyces.distTree
 rm Saccharomyces.distTree
 
 section "-variance_min"
 # 0.0005 = average arc length / 100
-$THIS/makeDistTree  -qc  -data data/Salmonella  -variance linExp  -variance_min 0.0005  -optimize  -subgraph_iter_max 10 \
+$THIS/makeDistTree  -qc  -data $DATA/Salmonella  -variance linExp  -variance_min 0.0005  -optimize  -subgraph_iter_max 10 \
   -delete_criterion_outliers Salmonella-var_min.criterion_outliers \
   -delete_deformation_outliers Salmonella-var_min.deformation_outliers \
   -delete_hybrids Salmonella-var_min.hybrids \
   | grep -v '^CHRON: ' > Salmonella-var_min.distTree
-diff Salmonella-var_min.criterion_outliers data/Salmonella-var_min.criterion_outliers
+diff Salmonella-var_min.criterion_outliers $DATA/Salmonella-var_min.criterion_outliers
 rm Salmonella-var_min.criterion_outliers
 $THIS/../sort.sh Salmonella-var_min.deformation_outliers 
 cut -f 1      Salmonella-var_min.deformation_outliers > $TMP.Salmonella-var_min.deformation_outliers
-cut -f 1 data/Salmonella-var_min.deformation_outliers > $TMP.data.Salmonella-var_min.deformation_outliers
+cut -f 1 $DATA/Salmonella-var_min.deformation_outliers > $TMP.data.Salmonella-var_min.deformation_outliers
 diff $TMP.Salmonella-var_min.deformation_outliers $TMP.data.Salmonella-var_min.deformation_outliers
 rm Salmonella-var_min.deformation_outliers
-diff Salmonella-var_min.hybrids data/Salmonella-var_min.hybrids
+diff Salmonella-var_min.hybrids $DATA/Salmonella-var_min.hybrids
 rm Salmonella-var_min.hybrids
-$THIS/distTree_compare_criteria.sh Salmonella-var_min.distTree data/Salmonella-var_min.distTree
+$THIS/distTree_compare_criteria.sh Salmonella-var_min.distTree $DATA/Salmonella-var_min.distTree
 rm Salmonella-var_min.distTree
 
 
@@ -141,52 +144,52 @@ else
   super_section "Two dissimilarity types"
 
   section "Salmonella2"
-  $THIS/makeDistTree  -qc  -data data/Salmonella2  -variance linExp  -optimize  -subgraph_iter_max 10  \
+  $THIS/makeDistTree  -qc  -data $DATA/Salmonella2  -variance linExp  -optimize  -subgraph_iter_max 10  \
     -output_dissim_coeff Salmonella2.dissim_coeff \
     -delete_criterion_outliers Salmonella2.criterion_outliers \
     -delete_deformation_outliers Salmonella2.deformation_outliers \
     -delete_hybrids Salmonella2.hybrids \
     | grep -v '^CHRON: ' > Salmonella2.distTree
-  diff Salmonella2.dissim_coeff data/Salmonella2.dissim_coeff
+  diff Salmonella2.dissim_coeff $DATA/Salmonella2.dissim_coeff
   rm Salmonella2.dissim_coeff
-  diff Salmonella2.criterion_outliers data/Salmonella2.criterion_outliers
+  diff Salmonella2.criterion_outliers $DATA/Salmonella2.criterion_outliers
   rm Salmonella2.criterion_outliers
   $THIS/../sort.sh Salmonella2.deformation_outliers
   cut -f 1      Salmonella2.deformation_outliers > $TMP.Salmonella2.deformation_outliers
-  cut -f 1 data/Salmonella2.deformation_outliers > $TMP.data.Salmonella2.deformation_outliers
+  cut -f 1 $DATA/Salmonella2.deformation_outliers > $TMP.data.Salmonella2.deformation_outliers
   diff $TMP.Salmonella2.deformation_outliers $TMP.data.Salmonella2.deformation_outliers
   rm Salmonella2.deformation_outliers
-  diff Salmonella2.hybrids data/Salmonella2.hybrids
+  diff Salmonella2.hybrids $DATA/Salmonella2.hybrids
   rm Salmonella2.hybrids
-  $THIS/distTree_compare_criteria.sh Salmonella2.distTree data/Salmonella2.distTree
+  $THIS/distTree_compare_criteria.sh Salmonella2.distTree $DATA/Salmonella2.distTree
   rm Salmonella2.distTree
 
   section "Saccharomyces hybrids"
-  $THIS/makeDistTree -qc  -threads 10  -data data/Saccharomyces2  -variance linExp  -optimize  -subgraph_iter_max 2  \
+  $THIS/makeDistTree -qc  -threads 10  -data $DATA/Saccharomyces2  -variance linExp  -optimize  -subgraph_iter_max 2  \
     -hybridness_min 1.2  -hybrid_parent_pairs Saccharomyces2.hybrid_parent_pairs  -delete_hybrids Saccharomyces2.hybrid  -dissim_boundary 0.675 \
     -delete_criterion_outliers Saccharomyces2.criterion_outliers  -criterion_outlier_num_max 1 \
     -delete_deformation_outliers Saccharomyces2.deformation_outliers  -deformation_outlier_num_max 1 \
     -output_tree $TMP.tree > $TMP.out
   $THIS/../rm_col.sh Saccharomyces2.hybrid 10
   $THIS/../sort.sh Saccharomyces2.hybrid
-  diff Saccharomyces2.hybrid data/Saccharomyces2.hybrid
+  diff Saccharomyces2.hybrid $DATA/Saccharomyces2.hybrid
   $THIS/hybrid2list.sh Saccharomyces2.hybrid > Saccharomyces2.hybrid.list
   rm Saccharomyces2.hybrid
   $THIS/../rm_col.sh Saccharomyces2.hybrid_parent_pairs 13
   $THIS/../sort.sh Saccharomyces2.hybrid_parent_pairs
-  diff Saccharomyces2.hybrid_parent_pairs data/Saccharomyces2.hybrid_parent_pairs
+  diff Saccharomyces2.hybrid_parent_pairs $DATA/Saccharomyces2.hybrid_parent_pairs
   rm Saccharomyces2.hybrid_parent_pairs
-  diff Saccharomyces2.criterion_outliers data/Saccharomyces2.criterion_outliers
+  diff Saccharomyces2.criterion_outliers $DATA/Saccharomyces2.criterion_outliers
   rm Saccharomyces2.criterion_outliers
   cut -f 1      Saccharomyces2.deformation_outliers > $TMP.Saccharomyces2.deformation_outliers
-  cut -f 1 data/Saccharomyces2.deformation_outliers > $TMP.data.Saccharomyces2.deformation_outliers
+  cut -f 1 $DATA/Saccharomyces2.deformation_outliers > $TMP.data.Saccharomyces2.deformation_outliers
   diff $TMP.Saccharomyces2.deformation_outliers $TMP.data.Saccharomyces2.deformation_outliers
   rm Saccharomyces2.deformation_outliers 
   # Saccharomyces2.distTree
   $THIS/tree2obj.sh $TMP.tree > $TMP.list
-  $THIS/../dm/dm2subset data/Saccharomyces2 $TMP.list > $TMP.dm
+  $THIS/../dm/dm2subset $DATA/Saccharomyces2 $TMP.list > $TMP.dm
   $THIS/makeDistTree  -threads 10  -data $TMP  -input_tree $TMP.tree  -variance linExp  -optimize  -reinsert  -subgraph_iter_max 10  > Saccharomyces2.distTree
-  $THIS/distTree_compare_criteria.sh Saccharomyces2.distTree data/Saccharomyces2.distTree
+  $THIS/distTree_compare_criteria.sh Saccharomyces2.distTree $DATA/Saccharomyces2.distTree
   rm Saccharomyces2.distTree
 
   set +o errexit
@@ -196,17 +199,17 @@ else
     diff Saccharomyces.hybrid.list Saccharomyces2.hybrid.list
   fi
   rm Saccharomyces.hybrid.list Saccharomyces2.hybrid.list
-  diff data/Saccharomyces2.criterion_outliers data/Saccharomyces.criterion_outliers
-  cut -f 1 data/Saccharomyces2.deformation_outliers > $TMP.Saccharomyces2.deformation_outliers
-  cut -f 1 data/Saccharomyces.deformation_outliers  > $TMP.Saccharomyces.deformation_outliers
+  diff $DATA/Saccharomyces2.criterion_outliers $DATA/Saccharomyces.criterion_outliers
+  cut -f 1 $DATA/Saccharomyces2.deformation_outliers > $TMP.Saccharomyces2.deformation_outliers
+  cut -f 1 $DATA/Saccharomyces.deformation_outliers  > $TMP.Saccharomyces.deformation_outliers
   diff $TMP.Saccharomyces2.deformation_outliers $TMP.Saccharomyces.deformation_outliers
 
 
   super_section "Many dissimilarity types"
 
   section "Virus9"
-  $THIS/makeDistTree  -qc  -data data/Virus9  -optimize  -subgraph_iter_max 100  -variance linExp  -variance_dissim  -output_dissim_coeff Virus9.coeff  -output_data Virus9-out  -output_tree Virus9.tree  1> $TMP.1 2> $TMP.out
-  diff Virus9.coeff data/Virus9.coeff
+  $THIS/makeDistTree  -qc  -data $DATA/Virus9  -optimize  -subgraph_iter_max 100  -variance linExp  -variance_dissim  -output_dissim_coeff Virus9.coeff  -output_data Virus9-out  -output_tree Virus9.tree  1> $TMP.1 2> $TMP.out
+  diff Virus9.coeff $DATA/Virus9.coeff
   rm Virus9.coeff
   A=`grep -w '^Error between dissimilarities' -A 1 $TMP.1 | tail -1`
   #
@@ -224,7 +227,7 @@ else
 
 
   section "Virus110"
-  $THIS/makeDistTree  -data data/Virus110  -variance_min 0.005  -variance linExp  -variance_dissim  -optimize  -subgraph_iter_max 100  -output_data Virus110-out  -output_tree Virus110.tree 1> $TMP.1 
+  $THIS/makeDistTree  -data $DATA/Virus110  -variance_min 0.005  -variance linExp  -variance_dissim  -optimize  -subgraph_iter_max 100  -output_data Virus110-out  -output_tree Virus110.tree 1> $TMP.1 
   A=`grep -w '^Error between dissimilarities' -A 1 $TMP.1 | tail -1`
   #
   $THIS/makeDistTree  -qc  -input_tree Virus110.tree  -data Virus110-out  -dissim_attr dissim  -weight_attr weight  -optimize  -output_tree Virus110-out1.tree 1> $TMP.2 2> $TMP.out
