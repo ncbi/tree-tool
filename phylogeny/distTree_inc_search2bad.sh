da53da69bfeb7372d565dfd436c113b491a8464e
Slava Brover <vbrover@gmail.com>
2021-05-12 16:39:20 -0400

    nan or -nan

--- phylogeny/distTree_inc_search2bad.sh
+++ phylogeny/distTree_inc_search2bad.sh
@@ -4,7 +4,7 @@ source $THIS/../bash_common.sh
 if [ $# -ne 2 ]; then
   echo "Remove a new contaminated object in a distance tree data structure indentified by inconsistent dissimilarities"
   echo "#1: incremental distance tree directory"
-  echo "#2: New object"
+  echo "#2: new object"
   exit 1
 fi
 INC=$1
@@ -18,7 +18,7 @@ if [ ! -e $DIR/dissim ]; then
   error "$DIR/dissim does not exist"
 fi
 
-awk '$3 == "nan"' $DIR/dissim > $DIR/dissim.bad
+awk '$3 == "nan" || $3 == "-nan"' $DIR/dissim > $DIR/dissim.bad
 
 if [ -s $DIR/dissim.bad ]; then
   cat $DIR/dissim.bad >> $INC/dissim.bad
