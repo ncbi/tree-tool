fd689b1966a4a8d9e35106f2cb7b4467863cc698
Slava Brover <vbrover@gmail.com>
2021-05-30 20:44:15 -0400

    cosmetic

--- phylogeny/featureTree.sh
+++ phylogeny/featureTree.sh
@@ -23,7 +23,7 @@ section "Building distance tree"
 $THIS/makeDistTree  $QC  -data $TMP  -dissim_attr dissim  -variance linExp  -optimize  -output_feature_tree ${TMP}-init.tree 
 
 section "Building feature tree without time"
-$THIS/makeFeatureTree  $QC  -input_tree ${TMP}-init.tree  -features $FEAT  -optim_iter_max 100  -output_core ${TMP}-maxParsimony.core  -prefer_gain  -output_tree ${TMP}-maxParsimony.tree  
+$THIS/makeFeatureTree  $QC  -input_tree ${TMP}-init.tree  -features $FEAT  -optim_iter_max 100  -prefer_gain  -output_core ${TMP}-maxParsimony.core  -output_tree ${TMP}-maxParsimony.tree  
 
 # MLE
 section "Building feature tree with time"
