75b984551a607bc824b20c9f2422a3b8acb59c57
Slava Brover <vbrover@gmail.com>
2021-06-02 18:10:20 -0400

    -variance_dissim

--- phylogeny/calibrateDissims_univ.sh
+++ phylogeny/calibrateDissims_univ.sh
@@ -1,7 +1,7 @@
 #!/bin/bash --noprofile
 THIS=`dirname $0`
 source $THIS/../bash_common.sh
-if [ $# -ne 8 ]; then
+if [ $# -ne 9 ]; then
   echo "Output: hmm-univ.stat, positiveAverage.out, data.dm, tree"
   echo "#1: input .dm-file without '.dm' created by univ_separate.sh"
   echo "#2: delete hybrids (0/1)"
@@ -9,8 +9,9 @@ if [ $# -ne 8 ]; then
   echo "#4: outlierSEs"
   echo "#5: dissim_coeff: >=0 (>0 <=> variance = linExp)"
   echo "#6: variance_power (NAN <=> variance = linExp)"
-  echo "#7: phen/"
-  echo "#8: phen/ is large (0/1)"
+  echo "#7: variance_dissim (0/1)"
+  echo "#8: phen/"
+  echo "#9: phen/ is large (0/1)"
   exit 1
 fi
 INPUT=$1
@@ -19,8 +20,9 @@ DISSIM_POWER=$3
 OUTLIER_SES=$4
 DISSIM_COEFF=$5
 VAR_POWER=$6
-PHEN=$7
-LARGE=$8
+VARIANCE_DISSIM=$7
+PHEN=$8
+LARGE=$9
 
 
 TMP=`mktemp`
@@ -30,7 +32,7 @@ echo $TMP
 section "Estimating hmm-univ.stat"
 $THIS/../dm/positiveAverage $INPUT $DISSIM_POWER $OUTLIER_SES hmm-univ.stat  -ignoreZero  -iter_max 30  -output_dissim $TMP.pairs > positiveAverage.out
 tail -n +5 $TMP.pairs.dm | sed 's/-/ /1' > $TMP.pairs
-$THIS/../dm/pairs2dm $TMP.pairs 1 cons 6  -distance > data.dm
+$THIS/../dm/pairs2dm $TMP.pairs 1 "cons" 6  -distance > data.dm
 
 
 section "Building tree"
@@ -39,6 +41,9 @@ DISSIM_COEFF_OPTION="-dissim_coeff $DISSIM_COEFF"
 if [ $DISSIM_COEFF == 0 ]; then
   VARIANCE="pow  -variance_power $VAR_POWER"
   DISSIM_COEFF_OPTION=""
+fi
+if [ $VARIANCE_DISSIM == 1 ]; then
+  VARIANCE="$VARIANCE -variance_dissim"
 fi
 
 HYBRID=""
