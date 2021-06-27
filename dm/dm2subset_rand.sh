f92c4972afe5b85f283ec6a21ec7857b3d546bcb
Slava Brover <vbrover@gmail.com>
2021-06-27 15:39:22 -0400

    -sigpipe

--- dm/dm2subset_rand.sh
+++ dm/dm2subset_rand.sh
@@ -2,21 +2,23 @@
 THIS=`dirname $0`
 source $THIS/../bash_common.sh
 if [ $# -ne 3 ]; then
-  echo "Seed sample of a .dm-file "
+  echo "Print a random sample of a .dm-file "
   echo "#1: Input .dm file without .dm"
   echo "#2: Sample seed"
   echo "#3: Sample size"
   exit 1
 fi
 INPUT=$1
-BASE_SEED=$2
-BASE_SIZE=$3
+SEED=$2
+SIZE=$3
 
 
-BASE=$INPUT-$BASE_SEED
-$THIS/dm2objs $INPUT | sort > $INPUT.list
-$THIS/../setRandOrd $INPUT.list  -seed $BASE_SEED | head -$BASE_SIZE | sort > $BASE.list
-$THIS/dm2subset $INPUT $BASE.list > $BASE.dm
-rm $BASE.list
+TMP=`mktemp`
 
 
+$THIS/dm2objs $INPUT > $TMP.list
+$THIS/../setRandOrd $TMP.list  -seed $SEED  -sigpipe | head -$SIZE | sort > $TMP.subset
+$THIS/dm2subset $INPUT $TMP.subset
+
+
+rm $TMP*
