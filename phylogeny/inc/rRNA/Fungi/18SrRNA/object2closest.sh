c6fe3a756169fe6fec5bdcd577d8e087715bee21
Slava Brover <vbrover@gmail.com>
2021-07-12 12:48:32 -0400

    head --> first command in a pipe

--- phylogeny/inc/rRNA/Fungi/18SrRNA/object2closest.sh
+++ phylogeny/inc/rRNA/Fungi/18SrRNA/object2closest.sh
@@ -9,8 +9,12 @@ NEW_OBJ=$1
 #set -o xtrace 
 
 
+TMP=`mktemp`
+
+
 INC=`dirname $0`
-blastn  -db $INC/seq.fa  -query $INC/../seq/$NEW_OBJ  -strand plus  -evalue 1e-20  -task blastn  -outfmt '6 sseqid nident' | sort -k 2 -n -r | cut -f 1 | head -100 | sort -u
+blastn  -db $INC/seq.fa  -query $INC/../seq/$NEW_OBJ  -strand plus  -evalue 1e-20  -task blastn  -outfmt '6 sseqid nident' | sort -k 2 -n -r | cut -f 1 > $TMP
+head -100 $TMP | sort -u
 
 
-
+rm $TMP*
