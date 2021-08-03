8ef60855cb568a4304500d22d75f1c4c50b3d95b
Slava Brover <vbrover@gmail.com>
2021-08-03 16:28:27 -0400

    new

---
+++ dissim/blast2cons.sh
@@ -0,0 +1,29 @@
+#!/bin/bash --noprofile
+THIS=`dirname $0`
+source $THIS/../bash_common.sh
+if [ $# -ne 2 ]; then
+  echo "Print the conservation dissimilarity of 2 DNA sequences"
+  echo "#1: DNA 1 (FASTA)"
+  echo "#2: DNA 2 (FASTA)"
+  exit 1
+fi
+DNA1=$1
+DNA2=$2
+
+
+TMP=`mktemp`
+#echo $TMP 
+
+
+N1=`$THIS/../genetics/fasta2len $DNA1 | cut -f 2`
+N2=`$THIS/../genetics/fasta2len $DNA2 | cut -f 2`
+
+cp $DNA1 $TMP
+makeblastdb  -in $TMP  -dbtype nucl  -blastdb_version 4  -logfile /dev/null
+blastn  -db $TMP  -query $DNA2   -evalue 1e-100  -dust no  -task megablast  \
+  -max_target_seqs 100000  -xdrop_gap 150  -xdrop_gap_final 150  -penalty -1  -gapopen 3  -gapextend 1  -dbsize 10000000  -searchsp 10000000 \
+  -outfmt '6 qseqid sseqid qstart qend sstart send nident length' \
+  | $THIS/blast2cons 0 $N2 $N1
+
+
+rm $TMP*
---
+++ genetics/plasmid_seq.sh
@@ -0,0 +1,55 @@
+#!/bin/bash --noprofile
+THIS=`dirname $0`
+source $THIS/../bash_common.sh
+if [ $# -ne 4 ]; then
+  echo "Create files for #1 in the current directory"
+  echo "Input: ./#1"
+  echo "Invokes: prodigal"
+  echo "#1: plasmid accession"
+ #echo "#2: Pfam HMM library"
+  echo "#2: iniveral HMM library"
+  echo "#3: list of ribosomal HMMs"
+  echo "#4: log file"
+  echo "Time: 40 sec."
+  exit 1
+fi
+PL=$1
+#PFAM=$2
+UNIV=$2
+RIB=$3
+LOG=$4
+
+
+if [ ! -e $PL ]; then
+  echo "No $PL sequence" >> $LOG
+  exit 1
+fi
+if [ ! -s $PL ]; then
+  echo "Empty genome" >> $LOG
+  exit 1
+fi
+
+$THIS/dna2stat $PL  -log $LOG > $PL.stat
+
+prodigal  -o /dev/null  -i $PL  -d $PL.cds  -c  -p meta  &>> $LOG
+
+if [ -s $PL.cds ]; then
+ #gunzip $PL.prot.gz
+  # $PL.prot
+  $THIS/orf2prot $PL.cds  -log $LOG > $PL.prot
+  # Find mobilome (transposases) ??
+  
+  $THIS/prots2hmm_univ.sh $PL $UNIV 1 $LOG
+  grep -w -f $RIB $PL.univ > $PL.ribosomal || true
+  
+ #$THIS/prots2hmm_hash.sh $PL.prot $PFAM 0 $PL.HMM $PL.hash-HMM $LOG
+ #cut -f 2 $PL.HMM | tr ',' '\n' | sort -u > $PL.pfam
+ #gzip $PL.HMM
+  
+  gzip $PL.prot
+fi
+
+rm $PL.cds
+
+
+
