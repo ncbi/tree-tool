6dcfd18a8aaea1cd87ead73cdb8e90d96f4bc706
Slava Brover <vbrover@gmail.com>
2022-01-27 17:46:11 -0500

    cosmetic

--- tsv/tsv_sort.sh
+++ tsv/tsv_sort.sh
@@ -2,13 +2,13 @@
 THIS=`dirname $0`
 source $THIS/../bash_common.sh
 if [ $# -ne 2 ]; then
-  echo "Sort s tsv-file"
+  echo "Sort a tsv-file"
   echo "#1: tsv-file name"
   echo "#2: sort parameters"
   exit 1
 fi
 F=$1
-P=$2
+P="$2"
 
 
 head -1 $F
