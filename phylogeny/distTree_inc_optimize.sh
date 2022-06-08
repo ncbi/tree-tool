cf5b73090b704864f12e9f7a25646188981f77f0
Slava Brover <vbrover@gmail.com>
2022-06-08 16:50:52 -0400

    -subgraph_iter_max 5

--- phylogeny/distTree_inc_optimize.sh
+++ phylogeny/distTree_inc_optimize.sh
@@ -1,18 +1,16 @@
 #!/bin/bash --noprofile
 THIS=`dirname $0`
 source $THIS/../bash_common.sh
-if [ $# -ne 4 ]; then
+if [ $# -ne 3 ]; then
   echo "Optimize a distance tree and evaluate"
   echo "#1: incremental distance tree directory (no updating)"
   echo "#2: new contents of #1/variance (non-empty string)"
-  echo "#3: number of optimization iterations"
-  echo "#4: output tree"
+  echo "#3: output tree"
   exit 1
 fi
 INC=$1
-PAR="$2"
-ITER_MAX=$3
-OUT_TREE=$4
+VARIANCE="$2"
+OUT_TREE=$3
 
 
 if [ ! -e $INC/phen ]; then
@@ -26,9 +24,7 @@ fi
 THREADS="-threads $N"
 
 section "Tree"
-# Time: O(n log^4(n))
-$THIS/makeDistTree  $THREADS  -data $INC/  -variance $PAR  -optimize  -skip_len  -subgraph_iter_max $ITER_MAX  -output_tree $OUT_TREE
-  # -reinsert  
+$THIS/makeDistTree  $THREADS  -data $INC/  -variance $VARIANCE  -optimize  -skip_len  -subgraph_iter_max 5  -output_tree $OUT_TREE
 
 super_section "Quality"
 LARGE=0
