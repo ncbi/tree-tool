4f1149e4e062f1a06a9350d48d56ff3c0e41e3dc
Slava Brover <vbrover@gmail.com>
2022-06-28 15:46:10 -0400

    new

---
+++ phylogeny/pairs2pivot.sh
@@ -0,0 +1,42 @@
+#!/bin/bash --noprofile
+THIS=`dirname $0`
+source $THIS/../bash_common.sh
+if [ $# -ne 2 ]; then
+  echo "Print a pivod table where rows and columns are ordered by distance"
+  echo "#1: file with numeric attribute, line format: <row_name> <col_name> <num_attr>"
+  echo "#2: output .dm-file"
+  exit 1
+fi
+DATA=$1
+OUT=$2
+
+
+TMP=`mktemp`
+#echo $TMP > /dev/stderr
+
+
+function make_list
+{
+  local IN=$1
+  $THIS/../dm/obj_attr2dm $IN "Real" -decimals 6 -qc > $TMP.dm
+  $THIS/../dm/dm2dist $TMP "dist" -qc > $TMP.dist.dm
+  $THIS/makeDistTree  -data $TMP.dist  -dissim_attr "dist"  -variance "linExp"  -optimize  -subgraph_iter_max 5  -output_tree $TMP.tree > /dev/stderr
+  $THIS/tree2obj_raw.sh $TMP.tree
+}
+
+
+section "Ordering rows" 
+make_list $DATA > $TMP.rows
+
+section "Ordering columns" 
+awk '{printf "%s\t%s\t%f\n", $2, $1, $3};' $DATA > $TMP.rev
+make_list $TMP.rev > $TMP.cols
+
+section "Result"
+$THIS/../dm/obj_attr2dm $DATA "Real" -decimals 6 -qc > $TMP.dm
+$THIS/../dm/printDataset $TMP  -objs $TMP.rows  -attrs $TMP.cols > $OUT
+
+
+rm -f $TMP*
+
+
