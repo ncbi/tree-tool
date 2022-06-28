c933a512c941663047cd739b2882d9bc5aa07f7b
Slava Brover <vbrover@gmail.com>
2022-06-28 15:41:32 -0400

    new

---
+++ phylogeny/tree2obj_raw.sh
@@ -0,0 +1,14 @@
+#!/bin/bash --noprofile
+THIS=`dirname $0`
+source $THIS/../bash_common.sh
+if [ $# -ne 1 ]; then
+  echo "Print objects of a distance tree, sorted"
+  echo "#1: Distance tree"
+  exit 1
+fi
+TREE=$1
+
+
+grep -v '^ *0x' $TREE | sed 's/^ *//1' | sed 's/: .*$//1' 
+
+
