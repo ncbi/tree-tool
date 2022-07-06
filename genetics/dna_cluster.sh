5774314f7743972418fb1193a2a0a2a784ce9434
Slava Brover <vbrover@gmail.com>
2022-07-06 15:28:04 -0400

    instruction

--- genetics/dna_cluster.sh
+++ genetics/dna_cluster.sh
@@ -3,12 +3,12 @@ THIS=`dirname $0`
 source $THIS/../bash_common.sh
 if [ $# -ne 6 ]; then
   echo "Cluster DNA sequences, if #1 = #2 then print representatives else print #1-#2 pairs"
-  echo "#1; query directory with DNA sequence"
+  echo "#1; query   directory with DNA sequence"
   echo "#2; subject directory with DNA sequence"
   echo "#3: min. fraction of identity"
   echo "#4: min. fraction of coverage"
   echo "#5: output file with all pairs of sequences of the same clusters"
-  echo "#6: output file with cluster representatives"
+  echo "#6: output file with clusters: <item> <cluster representative>"
   exit 1
 fi
 QDIR=$1
@@ -16,7 +16,7 @@ SDIR=$2
 IDENT=$3
 COV=$4
 PAIR=$5
-REPR=$6
+CLUST=$6
 
 
 TMP=`mktemp`
@@ -35,8 +35,8 @@ $THIS/../trav -step 1 $QDIR "blastn  -query %d/%f  -db $TMP  -dust no  -evalue 1
 #                                                                                                                                          1      2      3    4    5      6      7      8    9      10
 awk '$5 >= $6 * '$IDENT' && ($8 - $7 + 1) >= $3 * '$COV' && ($10 > $9 && ($10 - $9 + 1) >= $4 * '$COV' || $10 < $9 && ($9 - $10 + 1) >= $4 * '$COV')' $TMP.blastn | cut -f 1,2 >> $PAIR
 
-$THIS/../connectPairs $PAIR $TMP.out  -pairs 
-cut -f 2 $TMP.out | sort -u > $REPR
+$THIS/../connectPairs $PAIR $CLUST  -pairs 
+#cut -f 2 $TMP.out | sort -u > $CLUST
 
 
 rm $TMP*
