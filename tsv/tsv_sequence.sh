fed9eab57184f09e85360cbcde78c0e9b7cc0941
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2022-09-21 11:15:39 -0400

    instruction

--- tsv/tsv_sequence.sh
+++ tsv/tsv_sequence.sh
@@ -4,7 +4,7 @@ source $THIS/../bash_common.sh
 if [ $# -ne 2 ]; then
   echo "Add sequential numbers to the end of a tsv-file"
   echo "#1: tsv-file name"
-  echo "32: sequence column name"
+  echo "#2: sequence column name"
   exit 1
 fi
 F=$1
