a5c7afe15086c5f73d3184f1a113bc01c85a33af
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2022-10-14 11:14:52 -0400

    fasta2dissim -distance

--- phylogeny/fasta2tree.sh
+++ phylogeny/fasta2tree.sh
@@ -1,48 +1,53 @@
 #!/bin/bash --noprofile
 THIS=`dirname $0`
 source $THIS/../bash_common.sh
-if [ $# -ne 5 ]; then
+if [ $# -ne 6 ]; then
   echo "Create a phylogenetic tree from a FASTA files"
   echo "#1: FASTA"
   echo "#2: #1 is proteins (1/0)"
   echo "#3: DNA strand is known (0/1)"
   echo "#4: global alignment (0/1)"
-  echo "#5: output tree"
+  echo "#5: alignment distance"
+  echo "#6: output tree"
   exit 1
 fi
 FASTA=$1
 PROT=$2
 KNOWN_STRAND=$3
 GLOB=$4
-TREE=$5
+DIST=$5
+TREE=$6
 
 
 TMP=`mktemp`
+#echo $TMP > /dev/stderr
+#set -x
 
 
 section "Computing dissimilarities"
 
-PAR=""
+PAR="-distance $DIST"
 if [ $GLOB == 1 ]; then
-  PAR="-global"
+  PAR="$PAR  -global"
 fi
 if [ $PROT == 0 -a $KNOWN_STRAND == 0 ]; then
-  PAR="$PAR -unknown_strand"
+  PAR="$PAR  -unknown_strand"
 fi
 
-# PAR
-VAR="linExp"
 if [ $PROT == 1 ]; then
-  $THIS/../dissim/fasta2dissim  -threads 30  $FASTA  $PAR  -aa  -blosum62  -power 0.5  -dataset $TMP  
+  POWER=""
+  if [ $DIST != "diff" ]; then
+    POWER="-power 0.5"
+  fi
+  $THIS/../dissim/fasta2dissim  -threads 30  $FASTA  $PAR  -aa  -blosum62  $POWER  -dataset $TMP  
 else
   $THIS/../dissim/fasta2dissim  -threads 30  $FASTA  $PAR  -dataset $TMP  
-  VAR="pow  -variance_power 2"
 fi
 
 
 section "Building tree"
 # PAR
-$THIS/makeDistTree  -data $TMP  -dissim_attr "dissim"  -variance $VAR  -optimize  -subgraph_iter_max 5  -output_tree $TREE  -threads 10
+$THIS/makeDistTree  -data $TMP  -dissim_attr "dissim"  -variance pow  -variance_power 5  -optimize  -subgraph_iter_max 5  -output_tree $TREE  -threads 10
 
 
 rm $TMP*
