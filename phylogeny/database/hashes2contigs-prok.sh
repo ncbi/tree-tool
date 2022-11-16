c881c6a230369c845a8bbc160ae06bef2f9564d0
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2022-11-16 17:05:43 -0500

    renamed

--- phylogeny/database/hashes2contigs-prok.sh
+++ phylogeny/database/hashes2contigs-prok.sh
@@ -2,7 +2,7 @@
 THIS=`dirname $0`
 source $THIS/../../bash_common.sh
 if [ $# -ne 5 ]; then
-  echo "Find contigs with identical protein hash codes in different GenBank bacterial assemblies"
+  echo "Find contigs with identical protein hash codes in two GenBank bacterial assemblies"
   echo "#1: genome/ (large directory)"
   echo "#2: Assembly 1 multi-FASTA"
   echo "#3: Assembly 2 multi-FASTA"
@@ -28,10 +28,13 @@ HASH1=`$THIS/../../file2hash $NAME1`
 HASH2=`$THIS/../../file2hash $NAME2`
 
 $THIS/../../setIntersect.sh $GENOME_DIR/$HASH1/$NAME1/$NAME1.hash-PRT $GENOME_DIR/$HASH2/$NAME2/$NAME2.hash-PRT 1 > $TMP.hash_common
+wc -l $TMP.hash_common
 
+section "Prodigal"
 prodigal  -o /dev/null  -i $ASM1  -d $TMP.cds1 &> /dev/null
 prodigal  -o /dev/null  -i $ASM2  -d $TMP.cds2 &> /dev/null
 
+section "fasta2hash"
 $THIS/../../genetics/fasta2hash  $TMP.cds1  $TMP.out  -gene_finder "prodigal"  -cds  -translate  -target_hashes $TMP.hash_common  -target_seq $TMP.contig1 &> /dev/null
 $THIS/../../genetics/fasta2hash  $TMP.cds2  $TMP.out  -gene_finder "prodigal"  -cds  -translate  -target_hashes $TMP.hash_common  -target_seq $TMP.contig2 &> /dev/null
 
