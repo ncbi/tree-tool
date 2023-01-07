8f93b28615ae8c74ef2461cf5aa2aa92f7629f81
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2023-01-07 13:23:04 -0500

    instruction

--- phylogeny/fasta2tree.sh
+++ phylogeny/fasta2tree.sh
@@ -4,10 +4,10 @@ source $THIS/../bash_common.sh
 if [ $# -ne 6 ]; then
   echo "Create a phylogenetic tree from a FASTA files"
   echo "#1: FASTA"
-  echo "#2: #1 is proteins (1/0)"
+  echo "#2: #1 is proteins (0/1)"
   echo "#3: DNA strand is known (0/1)"
   echo "#4: global alignment (0/1)"
-  echo "#5: alignment distance"
+  echo "#5: `fasta2dissim -help | grep ^-distance -A 1 | tail -1 | sed 's/^ *//1'`"
   echo "#6: output tree"
   exit 1
 fi
