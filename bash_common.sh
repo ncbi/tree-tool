236d35f41dd2d8c71376b1ee678ba2975e6ec62a
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2023-03-06 16:28:19 -0500

    function comment

--- bash_common.sh
+++ bash_common.sh
@@ -13,6 +13,7 @@ export RED='\033[1;31m'
 export GREEN='\033[1;32m'
 export YELLOW='\033[1;33m'
 export BLUE='\033[1;34m'
+export MAGENTA='\033[35m'
 
 
 function error
@@ -61,3 +62,8 @@ function success
 }
 
 
+function comment
+{
+	local MSG="$1"
+  echo -e "${MAGENTA}${MSG}${NOCOLOR}" > /dev/stderr
+}
