22f688f45383434203b9899f74083643a7ad9f2c
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2023-03-27 19:17:07 -0400

    output is #1.tsv

--- tsv/dir2tsv.sh
+++ tsv/dir2tsv.sh
@@ -2,7 +2,7 @@
 THIS=`dirname $0`
 source $THIS/../bash_common.sh
 if [ $# -ne 2 ]; then
-  echo "Combine all .tsv-files of a directory into one .tsv-file"
+  echo "Combine all .tsv-files of a directory into one .tsv-file, save as #1.tsv"
   echo "#1: directory"
   echo "#2: column name for file names"
   exit 1
@@ -16,5 +16,9 @@ if [ -z "$F" ]; then
   error "Directory $D is empty"
 fi
 
-head -1 $D/$F | sed 's/$/\t'$C'/1'
-$THIS/../trav $D "sed 's/$/\t'%f'/1' %d/%f" | grep -v '^#'
+if [ -e $D.tsv ]; then
+  error "File $D.tsv exists"
+fi
+
+head -1 $D/$F | sed 's/$/\t'$C'/1'                       >  $D.tsv
+$THIS/../trav $D "tail -n +2 %d/%f | sed 's/$/\t'%f'/1'" >> $D.tsv
