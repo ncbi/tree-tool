15085ac5503a356fe80baf33544a38ad796977c0
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2023-04-14 16:01:46 -0400

    instruction

--- phylogeny/makeFeatureTree.cpp
+++ phylogeny/makeFeatureTree.cpp
@@ -75,7 +75,7 @@ struct ThisApplication : Application
   	  addKey ("output_tree", "Output file with the tree");
   	  addKey ("newick", "Output file with the tree in the Newick format");
   	  addFlag ("min_newick_name", "Minimal leaf names in Newick");
-  	  addKey ("qual", "Print the summary gain/loss statistics measured by feature consistency, save gain/loss statistics per feature in the indicated file, line format: <feature> +<gaines> -<losses> / <genomes> (<optional genomes>)");
+  	  addKey ("qual", "Print the summary gain/loss statistics measured by feature consistency, save gain/loss statistics per feature in the indicated file, tab-delimited line format: <feature> <gaines> <losses> <genomes> <optional genomes>");
   	  addFlag ("qual_nonredundant", "Non-redundify features by removing identical ones for the summary gain/loss statistics");
   	  addKey ("gain_nodes", "File name to save nodes where features are gained; within a node features are ordered alphabetically");
   	  addKey ("loss_nodes", "File name to save nodes where features are lost; within a node features are ordered alphabetically");
