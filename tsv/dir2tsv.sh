3fb97913949aadeb13aa4340790d16a1cb92203f
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2023-04-20 15:17:56 -0400

    Add initial '#'; print the result

--- tsv/dir2tsv.sh
+++ tsv/dir2tsv.sh
@@ -2,7 +2,7 @@
 THIS=`dirname $0`
 source $THIS/../bash_common.sh
 if [ $# -ne 2 ]; then
-  echo "Combine all .tsv-files of a directory into one .tsv-file, save as #1.tsv"
+  echo "Combine all .tsv-files of a directory into one .tsv-file"
   echo "#1: directory"
   echo "#2: column name for file names"
   exit 1
@@ -15,10 +15,10 @@ F=`ls $D | head -1 || true`
 if [ -z "$F" ]; then
   error "Directory $D is empty"
 fi
-
-if [ -e $D.tsv ]; then
-  error "File $D.tsv exists"
+if [ ! -s $D/$F ]; then
+  error "$D/$F is empty"
 fi
 
-head -1 $D/$F | sed 's/$/\t'$C'/1'                       >  $D.tsv
-$THIS/../trav $D "tail -n +2 %d/%f | sed 's/$/\t'%f'/1'" >> $D.tsv
+head -1 $D/$F | grep -s '^#' > /dev/null || printf '#'
+head -1 $D/$F | sed 's/$/\t'$C'/1'                       
+$THIS/../trav $D "tail -n +2 %d/%f | sed 's/$/\t'%f'/1'" 
