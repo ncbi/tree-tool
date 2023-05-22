f485c3baab84b722fc4e1354b65715a987e4f6ff
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2023-05-21 20:22:32 -0400

    PRT|CDS|HMM

--- phylogeny/database/genome2annot_count.sh
+++ phylogeny/database/genome2annot_count.sh
@@ -1,13 +1,15 @@
 #!/bin/bash --noprofile
 THIS=`dirname $0`
 source $THIS/../../bash_common.sh
-if [ $# != 1 ]; then
+if [ $# != 2 ]; then
   echo "#1: Genome.id"
+  echo "#2: CDS|PRT|HMM"
   exit 1
 fi
 G=$1
+SUF=$2
 
 
 H=`$THIS/../../file2hash $G`
-N=`cat genome/$H/$G/$G.hash-CDS | wc -l`
+N=`cat genome/$H/$G/$G.hash-$SUF | wc -l`
 echo $G $N
