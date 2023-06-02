f3e676220577fbf159b622201fdbb17f6ee4e9fc
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2023-06-02 16:45:53 -0400

    hybrid.new may be empty

--- phylogeny/distTree_inc_hybrid.sh
+++ phylogeny/distTree_inc_hybrid.sh
@@ -12,29 +12,26 @@ INC=$1
 VER=`cat $INC/version`
 
 
+wc -l $INC/hybrid.new
+
 if [ -e $INC/hybrid_parent_pairs ]; then
   mv $INC/hybrid_parent_pairs $INC/hist/hybrid_parent_pairs.$VER
 fi
 
 
-if [ ! -e $INC/hybrid.new ]; then
-  exit 0
+if [ -s $INC/hybrid.new ]; then
+  $THIS/hybrid2list.sh $INC/hybrid.new > $INC/hybrid-indiscern.raw
+  $THIS/distTree_inc_expand_indiscern.sh $INC $INC/hybrid-indiscern.raw 0 > $INC/hybrid-indiscern
+  rm $INC/hybrid-indiscern.raw
+
+  $INC/objects_in_tree.sh $INC/hybrid-indiscern "null"
+  $THIS/../trav $INC/hybrid-indiscern "$INC/outlier2db.sh %f auto_hybrid"  
+  $THIS/distTree_inc_new_cmd.sh $INC "rm -f" $INC/hybrid-indiscern
+
+  mv $INC/hybrid-indiscern $INC/hist/hybrid-indiscern.$VER
+  mv $INC/hybrid.new $INC/hist/hybrid.$VER
+else  
+  rm -f $INC/hybrid.new
+  touch $INC/hist/hybrid-indiscern.$VER
 fi
-wc -l $INC/hybrid.new
 
-if [ ! -s $INC/hybrid.new ]; then
-  rm $INC/hybrid.new
-  exit 0
-fi
-
-# $INC/hybrid-indiscern
-$THIS/hybrid2list.sh $INC/hybrid.new > $INC/hybrid-indiscern.raw
-$THIS/distTree_inc_expand_indiscern.sh $INC $INC/hybrid-indiscern.raw 0 > $INC/hybrid-indiscern
-rm $INC/hybrid-indiscern.raw
-
-$INC/objects_in_tree.sh $INC/hybrid-indiscern "null"
-$THIS/../trav $INC/hybrid-indiscern "$INC/outlier2db.sh %f auto_hybrid"  
-$THIS/distTree_inc_new_cmd.sh $INC "rm -f" $INC/hybrid-indiscern
-
-mv $INC/hybrid-indiscern $INC/hist/hybrid-indiscern.$VER
-mv $INC/hybrid.new $INC/hist/hybrid.$VER
