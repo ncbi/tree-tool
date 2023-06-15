087e2fdb19bd869d982c593b3ab9f7520a20e945
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2023-06-15 14:10:05 -0400

    new

---
+++ check_file.sh
@@ -0,0 +1,13 @@
+#!/bin/bash --noprofile
+THIS=`dirname $0`
+source $THIS/bash_common.sh
+if [ $# -ne 1 ]; then
+  echo "#1: file path to check"
+  exit 1
+fi
+F=$1
+
+
+if [ ! -e $F ]; then  
+  error "File \"$F\" does not exist"
+fi
