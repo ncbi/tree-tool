4a840f543649f9a5f40548774e28eaa1cbdc1d8d
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2023-06-16 21:27:24 -0400

    $TAXROOT

--- phylogeny/database/Genome_hash_requestClosest.sh
+++ phylogeny/database/Genome_hash_requestClosest.sh
@@ -1,15 +1,16 @@
 #!/bin/bash --noprofile
 THIS=`dirname $0`
 source $THIS/../../bash_common.sh
-if [ $# != 7 ]; then
+if [ $# != 8 ]; then
   echo "Print Genome.id's approximately closest to #1, where Genome.in_tree = 1"
   echo "#1: SQL Server name"
-  echo "#2: Database with tables Genome, GenomeHash, FreqHash"
-  echo "#3: Directory for bulk insert"
+  echo "#2: database with tables Genome, GenomeHash, FreqHash"
+  echo "#3: directory for bulk insert"
   echo "#4: path in Universal Naming Convention to the bulk insert directory"
   echo "#5: Genome.id"
-  echo "#6: directory with #1/#1.hash-{[CDS,]PRT,HMM}"
-  echo "#7: Hashtype.id for lowest-level dissimilarity"
+  echo "#6: Taxroot.id"
+  echo "#7: directory with #1/#1.hash-{[CDS,]PRT,HMM}"
+  echo "#8: Hashtype.id for lowest-level dissimilarity"
   exit 1
 fi
 SERVER=$1
@@ -17,8 +18,9 @@ DATABASE=$2
 BULK=$3
 BULK_REMOTE=$4
 GENOME=$5
-DIR=$6
-TYPE=$7
+TAXROOT=$6
+DIR=$7
+TYPE=$8
 
 
 TMP=`mktemp`  
@@ -29,18 +31,18 @@ TMP=`mktemp`
 NAME=`basename $TMP`
 BULK_FILE=$BULK/$NAME
 
-FILE=$DIR/$GENOME/$GENOME
+FILE=$DIR/$GENOME
 
 cp $FILE.hash-$TYPE $BULK_FILE
 unix2dos -o $BULK_FILE &> /dev/null
-$THIS/Genome_hash_requestClosest_.sh $SERVER $DATABASE $BULK_REMOTE $GENOME $TYPE $NAME > $TMP
+$THIS/Genome_hash_requestClosest_.sh $SERVER $DATABASE $BULK_REMOTE $GENOME $TAXROOT $TYPE $NAME > $TMP
 
 if [ "$TYPE" != "PRT" ]; then
   N=`cat $TMP | wc -l`
   if [ $N -lt 90 ]; then  # PAR
     cp $FILE.hash-PRT $BULK_FILE
     unix2dos -o $BULK_FILE &> /dev/null
-    $THIS/Genome_hash_requestClosest_.sh $SERVER $DATABASE $BULK_REMOTE $GENOME 'PRT' $NAME >> $TMP
+    $THIS/Genome_hash_requestClosest_.sh $SERVER $DATABASE $BULK_REMOTE $GENOME $TAXROOT 'PRT' $NAME >> $TMP
 		sort -u $TMP > $TMP.1
 		mv $TMP.1 $TMP
   fi
@@ -50,7 +52,7 @@ N=`cat $TMP | wc -l`
 if [ $N -lt 90 ]; then  # PAR
   cp $FILE.hash-HMM $BULK_FILE
   unix2dos -o $BULK_FILE &> /dev/null
-  $THIS/Genome_hash_requestClosest_.sh $SERVER $DATABASE $BULK_REMOTE $GENOME 'HMM' $NAME >> $TMP
+  $THIS/Genome_hash_requestClosest_.sh $SERVER $DATABASE $BULK_REMOTE $GENOME $TAXROOT 'HMM' $NAME >> $TMP
 fi
 
 sort -u $TMP 
--- phylogeny/database/Genome_hash_requestClosest_.sh
+++ phylogeny/database/Genome_hash_requestClosest_.sh
@@ -1,28 +1,30 @@
 #!/bin/bash --noprofile
 THIS=`dirname $0`
 source $THIS/../../bash_common.sh
-if [ $# != 6 ]; then
+if [ $# != 7 ]; then
   echo "Print Genome.id's approximately closest to #1, where Genome.in_tree = 1"
   echo "#1: SQL Server name"
   echo "#2: Database with tables Genome, GenomeHash, FreqHash"
   echo "#3: path in Universal Naming Convention to the bulk directory"
   echo "#4: Genome.id in #2"
-  echo "#5: Hash type"
-  echo "#6: file with hashes in the bulk directrory"
+  echo "#5: Taxroot.id"
+  echo "#6: Hash type"
+  echo "#7: file with hashes in the bulk directrory"
   exit 1
 fi
 SERVER=$1
 DATABASE=$2
 BULK_REMOTE=$3
 GENOME=$4
-TYPE=$5
-FILE=$6
+TAXROOT=$5
+TYPE=$6
+FILE=$7
 
 
 #set -x
 
 
-sqsh-ms  -S $SERVER  -D $DATABASE << EOT | sed 's/|$//1' 
+sqsh-ms  -S $SERVER  -D $DATABASE << EOT | sed 's/|$//1' | grep -vx $GENOME
   -- Time: O(h log (h n) + sort(h k)), where h - # hashes per genome, k - # genomes per hash
   --       k = O(1) artificially
   -- Time to populate GenomeHash: O(sort(h n))+
@@ -37,17 +39,12 @@ sqsh-ms  -S $SERVER  -D $DATABASE << EOT | sed 's/|$//1'
       , fieldterminator = '|'
       )
 
-    declare @tax_root int
-    select @tax_root = tax_root
-      from Genome (nolock)
-      where id = $GENOME
-
     select #H.[hash]  
       into #A
       from           #H
            left join FreqHash (nolock) on     FreqHash.[hash] = #H.[hash]
                                           and FreqHash.[type] = '$TYPE'
-                                          and FreqHash.tax_root = @tax_root
+                                          and FreqHash.tax_root = $TAXROOT
       where FreqHash.[hash] is null
 
     select GenomeHash.genome, count(*) c
@@ -61,8 +58,8 @@ sqsh-ms  -S $SERVER  -D $DATABASE << EOT | sed 's/|$//1'
       from      #G
            join Genome (nolock) on Genome.id = #G.genome
       where     Genome.in_tree = 1
-            and #G.genome != $GENOME
-            and Genome.tax_root = @tax_root
+          --and #G.genome != $GENOME
+            and Genome.tax_root = $TAXROOT
       order by #G.c desc, #G.genome /*tie resolution*/
   end
   go -m bcp
