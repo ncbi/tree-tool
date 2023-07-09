5d6c86dc7d36e46b917188e2055082b2bdcc7c45
vbrover <vbrover@gmail.com>
2023-07-08 22:06:57 -0400

    Form

--- ncurses.cpp
+++ ncurses.cpp
@@ -40,7 +40,7 @@ using namespace Common_sp;
 
 
 
-namespace Common_sp
+namespace NCurses_sp
 {
   
 
@@ -75,13 +75,6 @@ NCurses::NCurses (bool hideCursor)
 
 
 
-NCurses::~NCurses ()
-{ 
-  endwin (); 
-}
-
-
-
 void NCurses::resize ()
 { 
   int row_max_, col_max_;
@@ -94,55 +87,248 @@ void NCurses::resize ()
 
 	
 	
-	
-// NCAttr
 
+// AttrColor
 
-NCAttr::NCAttr (attr_t attr_arg,
-                bool active_arg)
-: attr (attr_arg)
-, active (active_arg)
-{ 
-  if (active)
-    attron (attr); 
-}
-
-
-
-NCAttr::~NCAttr ()
-{ 
-  if (active)
-    attroff (attr); 
-}
-
-
-
-
-// NCAttrColor
-
-NCAttrColor::NCAttrColor (NCurses::Color color,
-                          bool active_arg)
-: NCAttr ((attr_t) COLOR_PAIR (color + 1), active_arg)
+AttrColor::AttrColor (NCurses::Color color,
+                      bool active_arg)
+: Attr ((attr_t) COLOR_PAIR (color + 1), active_arg)
 {}
 
 
 
 
-// NCBackground 
+// Window
 
-NCBackground ::NCBackground (chtype background)
-: background_old (getbkgd (stdscr))
-{ 
-  bkgdset (background); 
+Window::Window (size_t global_x_arg,
+	              size_t global_y_arg,
+	              size_t width_arg,
+	              size_t height_arg)
+: global_x (global_x_arg)
+, global_y (global_y_arg)
+, width (width_arg)
+, height (height_arg)
+, win (newwin ((int) height, (int) width, (int) global_y, (int) global_x))
+{
+	ASSERT (height);
+	ASSERT (width);
+	ASSERT (win);
+	box (win, 0, 0);  // default characters for the vertical and horizontal lines	  
 }
 
 
 
-NCBackground ::~NCBackground ()
-{ 
-  bkgdset (background_old); 
+
+// Field
+
+Field::Field (Window &win_arg,
+ 			        size_t x_arg,
+		          size_t y_arg,
+			        size_t width_arg,
+			        bool upper_arg,
+			        const StringVector &val_arg)
+: win (win_arg)
+, x (x_arg)
+, y (y_arg)
+, width (width_arg)
+, upper (upper_arg) 
+, val (val_arg)
+{
+  ASSERT (x + width <= win. width);
+  ASSERT (y < win. height);
+	ASSERT (width > 1);
+
+ 	if (upper)
+		for (string& s : val)
+			if (s. size () == 1)
+		  	strUpper (s);
 }
 
+
+
+void Field::print () const
+{ 
+	string s;
+	FOR_START (size_t, i, val_start, val_start + width)
+	  if (i < val. size ())
+	    s += val [i];
+	  else
+	  	s += ' ';
+	win. print (x, y, s); 
+}
+
+
+
+Field::Exit Field::run ()
+{
+	Exit ex = fieldCancel;
+ 	const int prev_visibility = curs_set (1);
+ 	bool done = false;
+  string utf8;
+  while (! done)
+  {
+  	if (val. empty ())
+  		{ ASSERT (! val_start); }
+  	else
+  	{
+  	  ASSERT (val_start <= val. size ());
+  	  if (val_start == val. size ())
+  	  {
+  	  	ASSERT (val_start);
+  	  	val_start--;
+  	  }
+  	}
+  	ASSERT (pos < width);
+		
+    if (utf8. empty ())
+    {
+	  	print ();
+			win. cursor (x + pos, y);
+		}
+		
+		const size_t real_pos = val_start + pos;
+
+    int key = getch ();  
+    if (between (key, 128, 256))  // UTF-8
+    {
+    	utf8 += char (key);    
+  		const size_t len = utf8_len (utf8 [0]);
+  		ASSERT (len >= 2);
+  		if (utf8. size () == len)
+  		{
+  		//val = val. substr (0, real_pos) + utf8 + val. substr (real_pos);
+  		  auto it = val. begin ();
+  		  advance (it, real_pos);
+  			val. insert (it, utf8);
+  			utf8. clear ();    			
+  		}
+    }
+    else 
+    {
+    	ASSERT (utf8. empty ());
+    	if (between<int> (key, ' ', 127))  
+	    {
+	    	if (upper)
+	    		key = toUpper ((char) key);
+  		  auto it = val. begin ();
+  		  advance (it, real_pos);
+	    //val = val. substr (0, real_pos) + (char) key + val. substr (real_pos);
+	    	val. insert (it, string (1, (char) key));
+	    	if (pos + 1 < width)
+	    	  pos++;
+	    	else
+	    		val_start++;
+	    }
+	    else
+		    switch (key)
+		    {
+		    	case 27:  // ESC
+		    		done = true;
+			    	break;
+		    	case 10:
+		    	case KEY_ENTER:	
+		    		done = true;
+		    		ex = fieldDone;
+		    		break;
+		    	case KEY_UP:   
+		    	case KEY_BTAB:
+		    		done = true;
+		    		ex = fieldPrev;
+		    		break;
+		    	case KEY_DOWN:  
+		    	case '\t':
+		    		done = true;
+		    		ex = fieldNext;
+		    		break;
+		    	case KEY_LEFT:
+		    		if (pos)
+		    			pos--;
+		    		else if (val_start)
+		    			val_start--;
+		    		else
+		    			beep ();
+		    		break;
+		    	case KEY_RIGHT:
+		    		if (real_pos < val. size ())
+		    		{
+		    			if (pos + 1 < width)
+		    			  pos++;
+		    			else
+		    				val_start++;
+		    		}
+		    		else
+		    			beep ();
+		    		break;
+		    	case KEY_DC:
+		    		if (real_pos < val. size ())
+		    			val. eraseAt (real_pos);
+		    		else
+		    			beep ();
+		    		break;
+		    	case 127:
+		    	case KEY_BACKSPACE:
+		    		if (real_pos)
+		    		{
+		    			val. eraseAt (real_pos - 1);
+		    			if (pos)
+		    			  pos--;
+		    			else
+		    			{
+		    				ASSERT (val_start);
+		    				val_start--;
+		    			}
+		    		}
+		    		else
+		    			beep ();
+		    		break;	    		
+		    	default: 
+		    		beep ();
+		    }
+		}
+  }
+ 	curs_set (prev_visibility);
+
+  return ex;
+}
+
+
+
+
+// Form
+
+bool Form::run ()
+{
+	ASSERT (! fields. empty ());
+	
+	for (const Field* f : fields)
+	{
+		ASSERT (f);
+		f->print ();
+	}
+	
+	size_t i = 0;
+	for (;;)
+	{
+		const Field* f = fields [i];
+		switch (var_cast (f) -> run ())
+		{
+			case Field::fieldDone:   return true;
+			case Field::fieldCancel: return false;
+			case Field::fieldNext: 
+				i++; 
+				if (i == fields. size ())
+					i = 0;
+				break;
+			case Field::fieldPrev: 
+				if (i)
+					i--;
+				else
+					i = fields. size () - 1;
+				break;
+		}
+	}
+	NEVER_CALL;
+}
 
 
 
