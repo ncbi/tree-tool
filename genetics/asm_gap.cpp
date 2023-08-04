4ac530e3b7da8be3602d2083a6b7215df2ec8adc
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2023-08-04 16:50:30 -0400

    report

--- genetics/asm_gap.cpp
+++ genetics/asm_gap.cpp
@@ -143,7 +143,7 @@ struct ThisApplication : Application
 		
     // Statistics
 		ASSERT (! contigs. empty ());
-		cout << "Contigs" << '\t' << contigs. size () << endl;
+		report (cout, "Contigs", contigs. size ());
 		
 		contigs. sort ();
 
@@ -151,7 +151,7 @@ struct ThisApplication : Application
 		for (const size_t len : contigs)
 		  total += len;
 		const size_t half = total / 2;
-		cout << "Total" << '\t' << total << endl;
+		report (cout, "Total", total);
 
     total = 0;
 		FOR_REV (size_t, i, contigs. size ())
@@ -160,8 +160,8 @@ struct ThisApplication : Application
 		  total += len;
 		  if (total >= half)
 		  {
-		    cout << "L50" << '\t' <<  (contigs. size () - i) << endl;
-		    cout << "N50" << '\t' <<  len << endl;
+		    report (cout, "L50", contigs. size () - i);
+		    report (cout, "N50", len);
 		    break;
 		  }
 		}
