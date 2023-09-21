0bbb815aafd38c632b3785f268e3a8af5c54190a
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2023-09-21 12:36:00 -0400

    hmm-univ.list is used

--- phylogeny/database/genome2univs.sh
+++ phylogeny/database/genome2univs.sh
@@ -2,6 +2,7 @@
 THIS=`dirname $0`
 source $THIS/../../bash_common.sh
 if [ $# -ne 1 ]; then
+  echo "Input: hmm-univ.list"
   echo "#1: Genome.id"
   exit 1
 fi
@@ -10,9 +11,20 @@ G=$1
 
 H=`$THIS/../../file2hash $G`
 
-set +o errexit
-N=`grep -c ">" genome/$H/$G/$G.prot-univ`
-set -o errexit
+if false; then
+  set +o errexit
+  N=`grep -c ">" genome/$H/$G/$G.prot-univ`
+  set -o errexit
+fi
 
+
+TMP=`mktemp`
+
+
+grep '^>' genome/$H/$G/$G.prot-univ | sed 's/^>//1' | sed 's/ .*$//1' | sort -u > $TMP || true
+N=`join  -1 1  -2 1  $TMP  hmm-univ.list | wc -l`
 echo "$G $N"
+
+
+rm $TMP*
 
