f5ce01563a3e76ad6a2ede1492bf9b1c20906267
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2023-11-14 16:55:40 -0500

    new

---
+++ parent_dir.sh
@@ -0,0 +1,13 @@
+#!/bin/bash --noprofile
+THIS=`dirname $0`
+source $THIS/bash_common.sh
+if [ $# -ne 1 ]; then
+  echo "Print parent directory"
+  exit 1
+fi
+F=$1
+
+
+D=`dirname $F`
+P=`realpath $D`
+basename $P
