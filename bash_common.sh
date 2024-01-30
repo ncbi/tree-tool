2693514835c7edeed8a8a4de3be2f878ec347417
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2024-01-29 20:11:43 -0500

    new

--- bash_common.sh
+++ bash_common.sh
@@ -7,10 +7,6 @@ set -o pipefail
 export LC_ALL=C
 
 
-QSUB_="qsub  -r n  -cwd  -V  -P unified  -j y  -v SGE_NOMAIL  -v SGE_SUMMARY=/dev/null  -b y   -o /dev/null"
-export QSUB_5="$QSUB_  -l h_vmem=36G,mem_free=500M,m_mem_free=500M,h_rt=36000"   # ,reserve_mem=500M
-export QSUB_L="$QSUB_  -l h_vmem=36G,mem_free=1G,m_mem_free=1G,h_rt=360000"   # ,reserve_mem=1G
-
 export NOCOLOR='\033[0m'
 export RED='\033[1;31m'
 export GREEN='\033[1;32m'
---
+++ qsub_env.sh
@@ -0,0 +1,6 @@
+#! /bin/echo Please-source
+
+QSUB_="qsub  -r n  -cwd  -V  -P unified  -j y  -v SGE_NOMAIL  -v SGE_SUMMARY=/dev/null  -b y   -o /dev/null"
+export QSUB_5="$QSUB_  -l osver=8,h_vmem=36G,mem_free=500M,m_mem_free=500M,h_rt=36000"   
+export QSUB_L="$QSUB_  -l osver=8,h_vmem=36G,mem_free=1G,m_mem_free=1G,h_rt=1000000"   
+
