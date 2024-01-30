35db8bf04074b1a95bdc8092a9fef1ca2db94bff
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2024-01-29 20:14:05 -0500

    $PANFS is removed

--- phylogeny/database/redo_univ.sh
+++ phylogeny/database/redo_univ.sh
@@ -1,14 +1,20 @@
 #!/bin/bash --noprofile
-EXEC=$PANFS/code
-source $EXEC/cpp/bash_common.sh
+THIS=`dirname $0`
+EXEC=$THIS/../..
+source $EXEC/bash_common.sh
+if [ $# -ne 4 ]; then
+  echo "Output: genome/#hash/#1"
+  echo "#1: assembly"
+  exit 1
+fi
 ASM=$1
 
 
 LOG=$PWD/log/$ASM
 
-HASH=`$EXEC/cpp/file2hash $ASM`
+HASH=`$EXEC/file2hash $ASM`
 cd genome/$HASH/$ASM/
 
 gunzip $ASM.prot.gz
-$EXEC/cpp/genetics/prots2hmm_univ.sh $ASM ../../../hmm.lib 0 $LOG 
+$EXEC/genetics/prots2hmm_univ.sh $ASM ../../../hmm.lib 0 $LOG 
 gzip $ASM.prot
