6f8b7fe469ce1b3456fbfe97567b922b83664020
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2024-02-12 18:09:46 -0500

    #2: 1 - file, 0 - directory

--- check_file.sh
+++ check_file.sh
@@ -1,13 +1,21 @@
 #!/bin/bash --noprofile
 THIS=`dirname $0`
 source $THIS/bash_common.sh
-if [ $# -ne 1 ]; then
-  echo "#1: file path to check"
+if [ $# -ne 2 ]; then
+  echo "#1: file or directory path to check"
+  echo "#2: 1 - file, 0 - directory"
   exit 1
 fi
 F=$1
+D=$2
 
 
-if [ ! -e $F ]; then  
-  error "File \"$F\" does not exist"
+if [  $D -eq 0 ]; then
+  if [ ! -d $F ]; then  
+    error "Directory \"$F\" does not exist"
+  fi
+else
+  if [ ! -f $F ]; then  
+    error "File \"$F\" does not exist"
+  fi
 fi
