f696f73ce9e598d51ca3e38bf179477500e3c5e4
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2024-02-14 16:55:44 -0500

    new

---
+++ phylogeny/distTree_inc_offline.sh
@@ -0,0 +1,41 @@
+#!/bin/bash --noprofile
+THIS=`dirname $0`
+source $THIS/../bash_common.sh
+if [ $# -ne 2 ]; then
+  echo "Copy $1/ to #2/ and make #2/ empty and offline"
+  echo "#1: input incremental distance tree directory"
+  echo "#2: output incremental distance tree directory"
+  exit 1
+fi
+FROM=$1
+TO=$2
+
+
+$THIS/../check_file.sh $FROM 0
+
+if [ -e $TO ]; then
+  error "TO exists"
+fi
+cp -r $FROM $TO
+
+echo 1 > $TO/version
+
+cp /dev/null $TO/tree
+cp /dev/null $TO/dissim
+cp /dev/null $TO/dissim.bad
+cp /dev/null $TO/indiscern
+cp /dev/null $TO/runlog
+rm $TO/hist/*
+rm -f $TO/tree.released
+
+# Offline
+cp /dev/null $TO/server
+rm $TO/bulk
+rm $TO/bulk_remote
+rm $TO/database
+rm $TO/objects_in_tree.sh
+rm $TO/outlier2db.sh
+rm $TO/genogroup2db.sh
+
+
+
