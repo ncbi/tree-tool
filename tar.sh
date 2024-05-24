b11871f1e5e3489a8cb4aaf2223c7d99682c0d02
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2024-05-24 10:37:27 -0400

    #2: dereference

--- tar.sh
+++ tar.sh
@@ -1,14 +1,21 @@
 #!/bin/bash --noprofile
 THIS=`dirname $0`
 source $THIS/bash_common.sh
-if [ $# -ne 1 ]; then
+if [ $# -ne 2 ]; then
   echo "tar #1/ into #.tar.gz"
   echo "#1: directory"
+  echo "#2: dereference (0/1)"
   exit 1
 fi
 D=$1
+DEREF=$2
 
 
-tar -cvf $D.tar $D
-gzip $D.tar
+DEREF_PAR=""
+if [ $DEREF == 1 ]; then
+  DEREF_PAR="--dereference"
+fi
+
+tar -czf  $D.tar.gz $D  $DEREF_PAR
+
 
