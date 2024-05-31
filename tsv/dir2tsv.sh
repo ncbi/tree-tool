99fbf2aafbcc56d2cf1e625619f01b42b8dececb
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2024-05-31 16:54:19 -0400

    header

--- tsv/dir2tsv.sh
+++ tsv/dir2tsv.sh
@@ -1,14 +1,16 @@
 #!/bin/bash --noprofile
 THIS=`dirname $0`
 source $THIS/../bash_common.sh
-if [ $# -ne 2 ]; then
+if [ $# -ne 3 ]; then
   echo "Combine all .tsv-files of a directory into one .tsv-file"
-  echo "#1: directory"
-  echo "#2: column name for file names"
+  echo "#1: directory with tab-delimited files"
+  echo "#2: file with common header line if it is missing in files | ''"
+  echo "#3: column name for file names"
   exit 1
 fi
 D=$1
-C="$2"
+H=$2
+C="$3"
 
 
 F=`ls $D | head -1 || true`
@@ -19,6 +21,21 @@ if [ ! -s $D/$F ]; then
   error "$D/$F is empty"
 fi
 
-head -1 $D/$F | grep -s '^#' > /dev/null || printf '#'
-head -1 $D/$F | sed 's/$/\t'$C'/1'                       
-$THIS/../trav $D "tail -n +2 %d/%f | sed 's/$/\t'%f'/1'" 
+
+TMP=`mktemp`
+
+
+N=1
+if [ $H ]; then
+  cp $H $TMP
+else
+  head -1 $D/$F > $TMP
+  N=2
+fi
+head -1 $TMP | grep -s '^#' > /dev/null || printf '#'
+head -1 $TMP | sed 's/$/\t'$C'/1'                       
+
+$THIS/../trav $D "tail -n +$N %d/%f | sed 's/$/\t'%f'/1'" 
+
+
+rm $TMP
