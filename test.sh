62a4ef2111f3eaa96606a25d70ab510260f5d7e4
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2024-07-15 16:24:29 -0400

    time ...

--- test.sh
+++ test.sh
@@ -7,16 +7,20 @@ super_section "graph"
 $THIS/graph_test go -qc
 
 super_section "dm"
+# Time: 31 sec.
 $THIS/dm/dm_test.sh 1
 
 super_section "kmerIndex"
+# Time: 18 sec.
 $THIS/genetics/kmerIndex_test.sh go
 
 super_section "distTree"
-$THIS/phylogeny/distTree_test.sh go
+# Time: 75 (63) min.
+time $THIS/phylogeny/distTree_test.sh go
 
 super_section "featureTree"
-$THIS/phylogeny/featureTree_test.sh go
+# Time: 10 min.
+time $THIS/phylogeny/featureTree_test.sh go
 
 super_section "xml"
 $THIS/xml/xml_test.sh 0
