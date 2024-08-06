30f391894356b41b5e6e4ce145f6c6fc11fcb555
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2024-08-06 11:01:16 -0400

    for DNA or proteins

--- genetics/fa2start.sh
+++ genetics/fa2start.sh
@@ -1,10 +1,17 @@
 #!/bin/bash --noprofile
 THIS=`dirname $0`
 source $THIS/../bash_common.sh
-if [ $# -ne 1 ]; then
-  echo "Print non-standard start codons (not 'M')"
-  echo "#1: Peptide FASTA-file"
+if [ $# -ne 2 ]; then
+  echo "Print start codons"
+  echo "#1: FASTA-file"
+  echo "#2: DNA - 0; peptide - 1"
   exit 1
 fi
+F=$1
+PEP=$2
 
-cat $1 | tr '\n' ' ' | tr '>' '\n' | grep -v '^$' | sed 's/^\([0-9]\+\) \(.\).*$/\1 \2/1' | grep -v ' M'
+DOTS="..."
+if [ $PEP == 1 ]; then
+  DOTS="."
+fi
+sed 's/ .*$//1' $F | tr '\n' ' ' | tr '>' '\n' | grep -v '^$' | sed 's/^\([^ ]\+\) \('$DOTS'\).*$/\1\t\2/1' 
