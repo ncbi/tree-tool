08a1863186b0fdfe319ba24ffd58029a60d7f10a
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2024-08-10 13:34:39 -0400

    inc-files

---
+++ phylogeny/inc/genome/Stramenopiles-RefSeq/qc_object.sh
@@ -0,0 +1,12 @@
+#!/bin/bash --noprofile
+THIS=`dirname $0`
+source CPP_DIR/bash_common.sh
+if [ $# -ne 1 ]; then
+  echo "QC an object"
+  echo "#1: file or directory with object data"
+  exit 1
+fi
+FD=$1
+
+
+warning "$0 is not implemented"
