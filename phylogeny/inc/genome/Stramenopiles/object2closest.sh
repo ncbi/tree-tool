2eb6f85f88a700043727f6158f303592a77e237c
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2024-08-10 13:34:08 -0400

    inc-files

---
+++ phylogeny/inc/genome/Stramenopiles/object2closest.sh
@@ -0,0 +1,32 @@
+#!/bin/bash --noprofile
+source CPP_DIR/bash_common.sh
+if [ $# -ne 3 ]; then
+  echo "Find closest genomes among #3"
+  echo "#1: Genome.id"
+  echo "#2: new object directory or ''"
+  echo "#3: subset of Genome.id's"
+  exit 1
+fi
+GENOME=$1
+DIR="$2"
+SUBSET=$3
+
+
+INC=`dirname $0`
+SERVER=`cat $INC/server`
+DATABASE=`cat $INC/database`
+BULK_REMOTE=`cat $INC/bulk_remote`
+TAX=`cat $INC/../tax_id`
+
+if [ -z $DIR ]; then
+  DIR=$INC/../genome
+  if [ -e $INC/large ]; then
+    H=`CPP_DIR/file2hash $GENOME`
+    DIR=$DIR/$H/$GENOME
+  fi
+fi
+
+CPP_DIR/phylogeny/database/Genome_hash_requestClosest.sh $SERVER $DATABASE $INC/bulk $BULK_REMOTE $GENOME $TAX $DIR "PRT" $SUBSET
+
+
+
---
+++ phylogeny/inc/genome/Stramenopiles/outlier2db.sh
@@ -0,0 +1,23 @@
+#!/bin/bash --noprofile
+source CPP_DIR/bash_common.sh
+if [ $# -ne 2 ]; then
+  echo "Add an outlier to database"
+  echo "#1: Genome.id"
+  echo "#2: Genome.outlier"
+  exit 1
+fi
+ASM=$1
+OUTLIER="$2"
+
+
+INC=`dirname $0`
+SERVER=`cat $INC/server`
+DATABASE=`cat $INC/database`
+
+sqsh-ms -S $SERVER  -D $DATABASE  << EOT
+  update Genome
+    set outlier = '$OUTLIER'
+    where id = $ASM
+  go -m bcp 
+EOT
+
