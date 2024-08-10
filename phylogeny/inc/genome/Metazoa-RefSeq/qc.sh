3381fa70793c0ebe7762930e42a2f2fc33a45dbf
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2024-08-10 13:35:54 -0400

    inc-files

--- phylogeny/inc/genome/Metazoa-RefSeq/qc.sh
+++ phylogeny/inc/genome/Metazoa-RefSeq/qc.sh
@@ -11,10 +11,11 @@ VERB=$1
 INC=`dirname $0`
 SERVER=`cat $INC/server`
 DATABASE=`cat $INC/database`
+TAX=`cat $INC/../tax_id`
 
 
 if [ -e $INC/good ]; then
-  sort -c -u $INC/good
+  sort -cu $INC/good
 fi
 
 
@@ -29,7 +30,7 @@ CPP_DIR/phylogeny/tree2obj.sh $INC/tree > $TMP.tree
 sqsh-ms -S $SERVER  -D $DATABASE  << EOT | sed 's/|$//1' | sort > $TMP.genome-tree
   select acc_ver
     from RGenome
-    where     tax_root = 33208
+    where     tax_root = $TAX
           and in_tree = 1
   go -m bcp  
 EOT
@@ -42,7 +43,7 @@ CPP_DIR/phylogeny/distTree_inc_new_list.sh $INC > $TMP.new
 sqsh-ms -S $SERVER  -D $DATABASE  << EOT | sed 's/|$//1' | sort > $TMP.genome-new
   select acc_ver
     from RGenome
-    where     tax_root = 33208
+    where     tax_root = $TAX
           and dead = 0
           and prots is not null
           and outlier is null
