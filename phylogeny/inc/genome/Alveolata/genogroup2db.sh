469af432fd71a86977afb11b4f5ce976a022a082
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2024-08-10 13:38:15 -0400

    Alveolata/inc files

---
+++ phylogeny/inc/genome/Alveolata/genogroup2db.sh
@@ -0,0 +1,23 @@
+#!/bin/bash --noprofile
+source CPP_DIR/bash_common.sh
+if [ $# -ne 1 ]; then
+  echo "$0"
+  echo "#1: File genogroup_table"
+  exit 1
+fi
+IN=$1
+
+
+INC=`dirname $0`
+SERVER=`cat $INC/server`
+DATABASE=`cat $INC/database`
+BULK_REMOTE=`cat $INC/bulk_remote`
+TAX=`cat $INC/../tax_id`
+
+CPP_DIR/bulk.sh $SERVER $INC/bulk $BULK_REMOTE $IN $DATABASE..ListC
+
+sqsh-ms  -S $SERVER  -D $DATABASE  -L exit_failcount=1 << EOF | sed 's/|$//1' 
+  EXEC Genogroup2outliers $TAX, 0
+  go -m bcp
+EOF
+
---
+++ phylogeny/inc/genome/Alveolata/pairs2dissim.sh
@@ -0,0 +1,21 @@
+#!/bin/bash --noprofile
+source CPP_DIR/bash_common.sh
+if [ $# -ne 4 ]; then
+  echo "Compute dissimilarities for pairs of objects"
+  echo "#1: input dissimilarity requests (pairs of objects)"
+  echo "#2: new object file or directory, or ''. Object name is basename"
+  echo "#3: output dissimilarities added to the pairs of objects"
+  echo "#4: error log"
+  exit 1
+fi
+REQ=$1
+FILE_NEW="$2"
+OUT=$3
+LOG=$4
+
+
+INC=`dirname $0`
+GENOME=$INC/../genome
+# PAR
+CPP_DIR/phylogeny/database/combine_dissims.sh $REQ $GENOME "$FILE_NEW" $OUT 10 0.1 $INC/dissim_scale $INC/hmm-univ.stat 1 0.59 1  $LOG
+#                                                 1    2       3           4    5  6   7                 8                  9 10   11 12 
