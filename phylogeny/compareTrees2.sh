973b1da9e58690668b8df3648805aaed8005336c
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2024-08-13 12:59:30 -0400

    cosmetic

--- phylogeny/compareTrees2.sh
+++ phylogeny/compareTrees2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash --noprofile
-THIS=`dirname $0`
+THIS=$( dirname $0 )
 source $THIS/../bash_common.sh
 if [ $# -ne 2 ]; then
   echo "Robinson-Foulds comparison"
@@ -11,14 +11,14 @@ T1=$1
 T2=$2
 
 
-TMP=`mktemp`
+TMP=$( mktemp )
 #echo $TMP
 #set -x
 
 
 $THIS/compareTrees $T1 $T2 > $TMP.out
 
-M=(`grep -w 'match' $TMP.out | tr '\t' ' '  | sed 's/ .*$//1' | sort | uniq -c`)
+M=( $( grep -w 'match' $TMP.out | tr '\t' ' '  | sed 's/ .*$//1' | sort | uniq -c ) )
 #   5177 match+
 #   2092 match-
 M0=${M[0]}
@@ -26,7 +26,7 @@ M1=${M[2]}
 
 #head -20 $TMP.out
 
-P=`echo "scale=2; $M1 * 100 / ($M0 + $M1)" | bc -l`
+P=$( echo "scale=2; $M1 * 100 / ($M0 + $M1)" | bc -l )
 echo "$1	$2	$M1 / ($M0 + $M1) = $P %"
 
 
