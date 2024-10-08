e09c46416849de1ea6ac163295a0041e3878a185
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2024-10-07 21:16:18 -0400

    #2: number of hashes

--- dir_hash_init.sh
+++ dir_hash_init.sh
@@ -1,13 +1,15 @@
 #!/bin/bash --noprofile
-THIS=`dirname $0`
+THIS=`$( dirname $0 )
 source $THIS/bash_common.sh
-if [ $# -ne 1 ]; then
+if [ $# -ne 2 ]; then
   echo "Create hashes in a directory for trav '%h'"
   echo "#1: directory"
+  echo "#2: number of hashes (e.g., 1000)"
   exit 1
 fi
 DIR=$1
+H=$2
 
 
-$THIS/trav 1000 -zero -start 0 "mkdir $DIR/%n" -threads 10  -step 1
+$THIS/trav $H  -zero  -start 0  "mkdir $DIR/%n"  -threads 10  -step 1
 
