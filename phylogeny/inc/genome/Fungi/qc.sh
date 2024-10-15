6cb6554542aeb3837f79ff9497eca4b3203c3ca2
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2024-10-15 15:55:48 -0400

    use $TAX

--- phylogeny/inc/genome/Fungi/qc.sh
+++ phylogeny/inc/genome/Fungi/qc.sh
@@ -8,9 +8,10 @@ fi
 VERB=$1
 
 
-INC=`dirname $0`
-SERVER=`cat $INC/server`
-DATABASE=`cat $INC/database`
+INC=$( dirname $0 )
+SERVER=$( cat $INC/server )
+DATABASE=$( cat $INC/database )
+TAX=$( cat $INC/../tax_id )
 
 
 if [ -e $INC/good ]; then
@@ -18,7 +19,7 @@ if [ -e $INC/good ]; then
 fi
 
 
-TMP=`mktemp`
+TMP=$( mktemp )
 if [ $VERB == 1 ]; then
   echo $TMP
   set -x
@@ -29,7 +30,7 @@ CPP_DIR/phylogeny/tree2obj.sh $INC/tree > $TMP.tree
 sqsh-ms -S $SERVER  -D $DATABASE  << EOT | sed 's/|$//1' | sort > $TMP.genome-tree
   select id
     from Genome
-    where     tax_root = 4751
+    where     tax_root = $TAX
           and in_tree = 1
   go -m bcp  
 EOT
@@ -42,7 +43,7 @@ CPP_DIR/phylogeny/distTree_inc_new_list.sh $INC > $TMP.new
 sqsh-ms -S $SERVER  -D $DATABASE  << EOT | sed 's/|$//1' | sort > $TMP.genome-new
   select id
     from Genome
-    where     tax_root = 4751
+    where     tax_root = $TAX
           and dead = 0
           and prots is not null
           and outlier is null
