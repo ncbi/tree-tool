90d077661e9cac2a823ea89370e2f26b17346e2d
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2024-10-29 17:26:32 -0400

    new

---
+++ shellcheck.sh
@@ -0,0 +1,11 @@
+#!/bin/bash --noprofile
+THIS=$( dirname $0 )
+source $THIS/bash_common.sh
+if [ $# -ne 1 ]; then
+  echo "Run shellcheck"
+  echo "#1: bash script"
+  exit 1
+fi
+S=$1
+
+shellcheck -e SC2002,SC2010,SC2012,SC2086,SC2207 -x  $S
