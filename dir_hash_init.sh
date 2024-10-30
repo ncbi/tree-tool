9c018d76cc53da2c761e4fccd9d9d03d06d12ae0
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2024-10-30 17:39:12 -0400

    mkdir -p

--- dir_hash_init.sh
+++ dir_hash_init.sh
@@ -3,7 +3,7 @@ THIS=$( dirname $0 )
 source $THIS/bash_common.sh
 if [ $# -ne 2 ]; then
   echo "Create hashes in a directory for trav '%h'"
-  echo "#1: directory"
+  echo "#1: directory to be created"
   echo "#2: number of hashes (e.g., 1000)"
   exit 1
 fi
@@ -11,5 +11,6 @@ DIR=$1
 H=$2
 
 
+mkdir -p $DIR
 $THIS/trav $H  -zero  -start 0  "mkdir $DIR/%n"  -threads 10  -step 1
 
