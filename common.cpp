78fa4ec7358a860f46efba601da515afd3c9c129
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2024-11-03 10:57:24 -0500

    FUNC; logic_error

--- common.cpp
+++ common.cpp
@@ -514,7 +514,7 @@ size_t powInt (size_t a,
 		if (b)
 			return 0;
 		else
-			throw runtime_error ("powInt: 0^0");
+			throw runtime_error (FUNC "powInt: 0^0");
 }
 
 
@@ -1271,7 +1271,7 @@ void removeDirectory (const string &dirName)
     {
       case Filetype::link: 
         if (unlink (name. c_str ()))
-          throw logic_error ("cannot unlink " + strQuote (name));
+          throw runtime_error (FUNC "cannot unlink " + strQuote (name));
         break;
       case Filetype::dir:
         removeDirectory (name);
@@ -1280,11 +1280,11 @@ void removeDirectory (const string &dirName)
         removeFile (name);
         break;
       default:
-        throw logic_error ("Cannot remove directory item " + strQuote (name) + " of type " + strQuote (filetype2name (t)));
+        throw runtime_error (FUNC "Cannot remove directory item " + strQuote (name) + " of type " + strQuote (filetype2name (t)));
     }
   }
   if (rmdir (dirName. c_str ()))
-    throw logic_error ("Cannot remove directory " + strQuote (dirName));
+    throw runtime_error (FUNC "Cannot remove directory " + strQuote (dirName));
 }
 
 
@@ -1490,7 +1490,7 @@ bool getChar (istream &is,
     return false;
   }
   if (! (i >= 0 && i <= 255))
-    throw runtime_error ("Cannot read character: " + to_string (i));
+    throw runtime_error (FUNC "Cannot read character: " + to_string (i));
   c = static_cast<char> (i);
 
   return true;
@@ -1902,7 +1902,7 @@ void Named::qc () const
   Root::qc ();
     
   if (! goodName (name))
-    throw runtime_error ("Bad name: " + strQuote (name));
+    throw runtime_error (FUNC "Bad name: " + strQuote (name));
 }
 
 
@@ -2164,7 +2164,7 @@ Input::Input (istream &is_arg,
 { 
   QC_ASSERT (is);
   if (! is->good ())
-    throw runtime_error ("Bad input stream");
+    throw runtime_error (FUNC "Bad input stream");
 }
  
 
@@ -2420,7 +2420,7 @@ void Token::qc () const
   		                 QC_ASSERT (Common_sp::isDelimiter (name [0]));
   		                 break;
   		case eDateTime:  break;
-  		default: throw runtime_error ("Token: Unknown type");
+  		default: throw logic_error ("Token: Unknown type");
   	}
   }
 }
@@ -2452,7 +2452,7 @@ void Token::saveText (ostream &os) const
 		                 break;
 		case eDelimiter: os << name;          
 		                 break;
- 		default: throw runtime_error ("Token: Unknown type");
+ 		default: throw logic_error ("Token: Unknown type");
 	}
 }
 
@@ -2898,9 +2898,9 @@ bool PairFile::next ()
   iss >> name1 >> name2;
   
   if (name2. empty ())
-  	throw runtime_error ("No pair: " + strQuote (name1) + " - " + strQuote (name2));
+  	throw runtime_error (FUNC "No pair: " + strQuote (name1) + " - " + strQuote (name2));
   if (! sameAllowed && name1 == name2)
-  	throw runtime_error ("Same name: " + name1);
+  	throw runtime_error (FUNC "Same name: " + name1);
   	
   if (orderNames && name1 > name2)
   	swap (name1, name2);
@@ -3046,10 +3046,10 @@ string Json::getString () const
 { 
   const auto* this_ = this;
   if (! this_ || asJsonNull ())
-    throw runtime_error ("undefined");
+    throw logic_error ("undefined");
   if (const JsonString* j = asJsonString ())
     return j->s;
-  throw runtime_error ("Not a JsonString");
+  throw logic_error ("Not a JsonString");
 }
 
 
@@ -3058,10 +3058,10 @@ long long Json::getInt () const
 { 
   const auto* this_ = this;
   if (! this_ || asJsonNull ())
-    throw runtime_error ("undefined");
+    throw logic_error ("undefined");
   if (const JsonInt* j = asJsonInt ())
     return j->n;
-  throw runtime_error ("Not a JsonInt");
+  throw logic_error ("Not a JsonInt");
 }
 
 
@@ -3070,12 +3070,12 @@ double Json::getDouble () const
 { 
   const auto* this_ = this;
   if (! this_)
-    throw runtime_error ("undefined");
+    throw logic_error ("undefined");
   if (asJsonNull ())
     return numeric_limits<double>::quiet_NaN ();
   if (const JsonDouble* j = asJsonDouble ())
     return j->n;
-  throw runtime_error ("Not a JsonDouble");
+  throw logic_error ("Not a JsonDouble");
 }
 
 
@@ -3084,10 +3084,10 @@ bool Json::getBoolean () const
 { 
   const auto* this_ = this;
   if (! this_ || asJsonNull ())
-    throw runtime_error ("undefined");
+    throw logic_error ("undefined");
   if (const JsonBoolean* j = asJsonBoolean ())
     return j->b;
-  throw runtime_error ("Not a JsonBoolean");
+  throw logic_error ("Not a JsonBoolean");
 }
 
 
@@ -3096,12 +3096,12 @@ const Json* Json::at (const string& name_arg) const
 { 
   const auto* this_ = this;
   if (! this_)
-    throw runtime_error ("undefined");
+    throw logic_error ("undefined");
   if (asJsonNull ())
     return nullptr;
   if (const JsonMap* j = asJsonMap ())
     return findPtr (j->data, name_arg);
-  throw runtime_error ("Not a JsonMap");
+  throw logic_error ("Not a JsonMap");
 }
 
 
@@ -3110,17 +3110,17 @@ const Json* Json::at (size_t index) const
 { 
   const auto* this_ = this;
   if (! this_)
-    throw runtime_error ("undefined");
+    throw logic_error ("undefined");
   if (asJsonNull ())
     return nullptr;
   if (const JsonArray* j = asJsonArray ())
   {
     if (index >= j->data. size ())
-      throw runtime_error ("Index out of range");
+      throw runtime_error (FUNC "Index out of range");
     else
       return j->data [index];
   }
-  throw runtime_error ("Not a JsonArray");
+  throw logic_error ("Not a JsonArray");
 }        
 
 
@@ -3129,12 +3129,12 @@ size_t Json::getSize () const
 { 
   const auto* this_ = this;
   if (! this_)
-    throw runtime_error ("undefined");
+    throw logic_error ("undefined");
   if (asJsonNull ())
     return 0;
   if (const JsonArray* j = asJsonArray ())
     return j->data. size ();
-  throw runtime_error ("Not a JsonArray");
+  throw logic_error ("Not a JsonArray");
 }        
 
 
@@ -3627,7 +3627,7 @@ void Application::addKey (const string &name,
   ASSERT (! name. empty ());
   ASSERT (! contains (name2arg, name));
   if (acronym && contains (char2arg, acronym))
-  	throw logic_error ("Duplicate option " + strQuote (string (1, acronym)));
+  	throw logic_error ("Duplicate key " + strQuote (string (1, acronym)));
   keyArgs << Key (*this, name, argDescription, defaultValue, acronym, var);
   name2arg [name] = & keyArgs. back ();
   if (acronym)
@@ -3643,7 +3643,7 @@ void Application::addFlag (const string &name,
   ASSERT (! name. empty ());
   ASSERT (! contains (name2arg, name));
   if (acronym && contains (char2arg, acronym))
-  	throw logic_error ("Duplicate option " + strQuote (string (1, acronym)));
+  	throw logic_error ("Duplicate flag " + strQuote (string (1, acronym)));
   keyArgs << Key (*this, name, argDescription, acronym);
   name2arg [name] = & keyArgs. back ();
   if (acronym)
@@ -4089,7 +4089,7 @@ int Application::run (int argc,
 	  			
 	  	seed_global = str2<ulong> (getArg ("seed"));
 	  	if (! seed_global)
-	  		throw runtime_error ("seed cannot be 0");
+	  		throw runtime_error ("Seed cannot be 0");
 	  
 	  	jsonFName = getArg ("json");
 	  	ASSERT (! jRoot);
