bab0d7fb9881b63abe97dab5613ca14780f1b78f
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2024-11-19 17:39:30 -0500

    cosmetic

--- tsv/tsv2triple.cpp
+++ tsv/tsv2triple.cpp
@@ -46,7 +46,7 @@ namespace
 {
   
   
-struct ThisApplication : Application
+struct ThisApplication final : Application
 {
   ThisApplication ()
     : Application ("Convert a tsv-file into an object-attribute-value table")
