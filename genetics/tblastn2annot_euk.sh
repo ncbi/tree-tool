33ae5917d17417bcb42fcd09add91753b42fa43b
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2024-11-21 10:12:40 -0500

    update

--- genetics/tblastn2annot_euk.sh
+++ genetics/tblastn2annot_euk.sh
@@ -22,6 +22,13 @@ tblastn  -query $REF  -db $TMP  -db_gencode 1  -evalue 1000  -word_size 2  -outf
   # -task tblastn-fast  -word_size 5  -evalue 1e-4: weak HSPs are needed!
   # -num_threads requires -db
   # -seg no  -comp_based_stats 0 
+
+# From Terence M.:
+# blastp_wnode -asn-cache ${GP_cache_dir} -backlog 1 -best_hit_overhang 0.1 -best_hit_score_edge 0.1 -comp_based_stats 0 -delay 0 -evalue 0.0001 -max-jobs 1 -seg no -service ${GP_qservice} -threshold 21 -word_size 6 -workers 4
+# iamond -asn-cache ${GP_cache_dir} -blastp-args --sam-query-len --comp-based-stats 0 --evalue 0.0001 --very-sensitive --masking 0 --unal 0 -diamond-executable ${GP_HOME}/third-party/diamond/diamond -lds2 ${input.lds2.target} -ofmt seq-align-set -output-dir ${output} -output-manifest ${output}/align.mft -output-prefix hits -query-fmt seq-ids -query-manifest ${input.query_ids} -subject-fmt seq-ids -subject-manifest ${input.subject_ids} -work-area ${tmp}
+
+# Nick: -word_size 3  -evalue 100
+
 $THIS/tblastn2annot_euk  $TMP.tblastn  -qc  -log $TMP.log > $PROT
 
 
