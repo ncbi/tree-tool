b1d36c536f128f99e0e953ecad30795d23c7767b
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2024-12-02 16:12:52 -0500

    log

--- genetics/tblastn2marker_euk.sh
+++ genetics/tblastn2marker_euk.sh
@@ -1,21 +1,24 @@
 #!/bin/bash --noprofile
 THIS=$( dirname $0 )
 source $THIS/../bash_common.sh
-if [ $# -ne 3 ]; then
+if [ $# -ne 4 ]; then
   echo "Find proteins with introns by tblastn"
   echo "#1: DNA multi-FASTA file"
   echo "#2: reference proteins"
   echo "#3: output protein file"
+  echo "#4: log file"
   echo "Time: 110 min./3.8G"
   exit 1
 fi
 DNA=$1
 REF=$2
 PROT=$3
+LOG=$4
 
 
 TMP=$( mktemp )
-#comment $TMP  
+comment $TMP &> $LOG
+date >> $LOG
 #set -x  
 
 MATRIX="BLOSUM62"
@@ -32,10 +35,10 @@ tblastn  -query $REF  -db $TMP  -db_gencode 1  -seg no  -comp_based_stats 0  -wo
   #   diamond  --comp-based-stats 0  --evalue 0.0001  --very-sensitive  --masking 0  --unal 0  seq-align-set  \
   #            -asn-cache ${GP_cache_dir}  -blastp-args  --sam-query-len  -diamond-executable ${GP_HOME}/third-party/diamond/diamond  -lds2 ${input.lds2.target}  -ofmt  -output-dir ${output}  -output-manifest ${output}/align.mft -output-prefix hits -query-fmt seq-ids -query-manifest ${input.query_ids} -subject-fmt seq-ids -subject-manifest ${input.subject_ids} -work-area ${tmp}
 
-$THIS/tblastn2marker_euk  $TMP.tblastn  -matrix $MATRIX  -qc  -log $TMP.log > $PROT
+$THIS/tblastn2marker_euk  $TMP.tblastn  -matrix $MATRIX  -qc  -log $LOG > $PROT
 # Quality: 
   # grep '^>' $PROT | sed 's/^.*score=//1' | count
-  # fasta2len $PROT | cut -f 2 | count
+  # fasta2len $PROT -noprogress | cut -f 2 | count
 
 
 rm $TMP*  
