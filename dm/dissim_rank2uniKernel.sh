da092c9486200d1756f39e989ba438ac0d8b91af
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2024-12-14 20:44:39 -0500

    bug

--- dm/dissim_rank2uniKernel.sh
+++ dm/dissim_rank2uniKernel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash --noprofile
 THIS=$( dirname $0 )
-source $THIS/../cpp/bash_common.sh
+source $THIS/../bash_common.sh
 if [ $# -ne 1 ]; then
   echo "Dependence of dissimilarity on taxonomy rank"
   echo "#1: dissimilarity file: obj1 obj2 dissim rank"
