93a96b18b052bae94ae69ec31c759aeb8948a146
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2024-12-14 20:45:09 -0500

    bug

--- dm/nominal2effective_size.sh
+++ dm/nominal2effective_size.sh
@@ -1,6 +1,6 @@
 #!/bin/bash --noprofile
-THIS=`dirname $0`
-source $THIS/bash_common.sh
+THIS=$( dirname $0 )
+source $THIS/../bash_common.sh
 if [ $# -ne 1 ]; then
   echo "Print effective size"
   echo "#1: file with columns: object nominal_value"
@@ -9,9 +9,8 @@ fi
 IN=$1
 
 
-S=`cut -f 2 $IN | sort | uniq -c | awk '{s+=$1} END {printf "%f", s}'`
-S2=`echo "$S "'*'" $S" | bc -l`
-#echo $S2
-VAR=`cut -f 2 $IN | sort | uniq -c | awk '{s+=$1*$1/'$S2'} END {printf "%f", s}'`
-EFF=`echo 1 / $VAR | bc -l`
-echo $EFF
+S=$( cut -f 2 $IN | sort | uniq -c | awk '{s+=$1} END {printf "%f", s}' )
+S2=$( echo "$S "'*'" $S" | bc -l )
+VAR=$( cut -f 2 $IN | sort | uniq -c | awk '{s+=$1*$1/'$S2'} END {printf "%f", s}' )
+echo "1 / $VAR" | bc -l 
+
