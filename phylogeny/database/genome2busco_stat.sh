ad0eef048d85d8f99bef81a17fdfc405c953bd7c
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2025-01-16 20:55:02 -0500

    move

--- phylogeny/database/genome2busco_stat.sh
+++ phylogeny/database/genome2busco_stat.sh
@@ -1,18 +1,21 @@
 #!/bin/bash --noprofile
 THIS=$( realpath $( dirname $0 ) )
-source $THIS/../bash_common.sh
+source $THIS/../../bash_common.sh
 if [ $# -ne 1 ]; then
-  echo "Print: file #complete_genes #duplicate_genes #total_genes duplicate_genes_share"
-  echo "#1: BUSCO .tsv-file"
+  echo "Input: genome/<hash>/#1/#1.busco10.tsv" 
+  echo "Print: <Genome.id> <#complete_genes> <#duplicate_genes> <#total_genes> <duplicate_genes_share>"
+  echo "#1: Genome.id"
   exit 1
 fi
-IN=$1
+G=$1
 
 
 TMP=$( mktemp )
 
 
-grep -v '^#' $IN | cut -f 1,2 > $TMP
+H=$( $THIS/../../file2hash $G )
+
+grep -v '^#' genome/$H/$G/$G.busco10.tsv | cut -f 1,2 > $TMP
 C=$( awk -F '\t' '$2 == "Complete"'   $TMP | cut -f 1 | sort -u | wc -l )
 D=$( awk -F '\t' '$2 == "Duplicated"' $TMP | cut -f 1 | sort -u | wc -l )
 T=$(( C + D ))
@@ -21,7 +24,7 @@ if [ $T == 0 ]; then
 else
   F=$( echo "$D / $T" | bc -l )
 fi
-echo -e "$IN\t$C\t$D\t$T\t$F"
+echo -e "$G\t$C\t$D\t$T\t$F"
 
 
 rm $TMP
