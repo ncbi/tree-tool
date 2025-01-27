155fc8defabba2ca014c4f36c956f4d0c8e586c7
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2025-01-27 12:22:17 -0500

    rename

--- genetics/prot_match.sh
+++ genetics/prot_match.sh
@@ -1,14 +1,13 @@
 #!/bin/bash --noprofile
 THIS=$( dirname $0 )
 source $THIS/../bash_common.sh
-if [ $# -ne 6 ]; then
-  echo "Cluster protein sequences, if #1 = #2 then print representatives else print #1-#2 pairs"
+if [ $# -ne 5 ]; then
+  echo "Print matching protein pairs"
   echo "#1: query protein sequences"
   echo "#2: subject protein sequences"
   echo "#3: min. fraction of identity"
   echo "#4: min. fraction of coverage"
   echo "#5: cores"
-  echo "#6: output file with pairs of FASTA identfiers"
   exit 1
 fi
 QFASTA=$1
@@ -16,7 +15,6 @@ SFASTA=$2
 IDENT=$3
 COV=$4
 CORES=$5
-PAIR=$6
 
 
 TMP=$( mktemp )
@@ -26,20 +24,9 @@ TMP=$( mktemp )
 
 makeblastdb  -in $SFASTA   -dbtype prot  -blastdb_version 4  -logfile /dev/null  -out $TMP
 
-# $PAIR
-if [ $QFASTA == $SFASTA ]; then
-  $THIS/fa2list.sh $QFASTA | awk '{OFS="\t"; print $1, $1};' > $PAIR
-fi
 blastp  -query $QFASTA  -db $TMP  -comp_based_stats 0  -evalue 1e-10  -seg no  -dbsize 10000000  -max_target_seqs 100000 -outfmt '6 qseqid sseqid qlen slen nident length qstart qend sstart send'  -num_threads $CORES > $TMP.blastp
 #                                                                                                                                   1      2      3    4    5      6      7      8    9      10
-awk '$5 >= $6 * '$IDENT' && ($8 - $7 + 1) >= $3 * '$COV' && ($10 - $9 + 1) >= $4 * '$COV'' $TMP.blastp | cut -f 1,2 >> $PAIR
-
-if [ $QFASTA == $SFASTA ]; then
-  $THIS/../connectPairs $PAIR $TMP.out  -pairs 
-  cut -f 2 $TMP.out | sort -u
-else
-  cat $PAIR
-fi
+awk '$5 >= $6 * '$IDENT' && ($8 - $7 + 1) >= $3 * '$COV' && ($10 - $9 + 1) >= $4 * '$COV'' $TMP.blastp | cut -f 1,2 
 
 
 rm $TMP*
