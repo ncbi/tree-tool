6ff55d0f30d6a4bf22cfc7dee7784218548abad5
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2025-01-31 17:10:47 -0500

    skip genes with too variable length

--- genetics/fasta2fam.sh
+++ genetics/fasta2fam.sh
@@ -26,23 +26,29 @@ MEAN=$( grep -w "^mean" $TMP.count | cut -f 2 )
   SD=$( grep -w "^SD"   $TMP.count | cut -f 2 )
 # PAR
 LEN_MIN=$( echo "$MEAN - 2 * $SD" | bc -l | sed 's/\..*$//1' )
-$THIS/filterFasta $F -aa  -len_min $LEN_MIN > $TMP.fa
+NEG=$( echo "$LEN_MIN < 60" | bc -l )
+#
+if [ $NEG == 1 ]; then
+  cp /dev/null $OUT
+else
+  $THIS/filterFasta $F -aa  -len_min $LEN_MIN > $TMP.fa
 
-# $TMP.clust
-# PAR
-$THIS/prot_match.sh $TMP.fa $TMP.fa 0.8 0.8 $CORES > $TMP.match
-$THIS/fa2list.sh $TMP.fa | awk '{OFS="\t"; print $1, $1};' >> $TMP.match
-$THIS/../connectPairs $TMP.match $TMP.clust  -pairs -center
+  # $TMP.clust
+  # PAR
+  $THIS/prot_match.sh $TMP.fa $TMP.fa 0.8 0.8 $CORES > $TMP.match
+  $THIS/fa2list.sh $TMP.fa | awk '{OFS="\t"; print $1, $1};' >> $TMP.match
+  $THIS/../connectPairs $TMP.match $TMP.clust  -pairs -center
 
-echo -e "#item\trepr" >  $TMP.clust.tsv
-cat $TMP.clust        >> $TMP.clust.tsv
-$THIS/../tsv/tsv_group $TMP.clust.tsv  -by "repr"  -count "count" > $TMP.clust.grp
+  echo -e "#item\trepr" >  $TMP.clust.tsv
+  cat $TMP.clust        >> $TMP.clust.tsv
+  $THIS/../tsv/tsv_group $TMP.clust.tsv  -by "repr"  -count "count" > $TMP.clust.grp
 
-N=$( $THIS/fa2list.sh $TMP.fa | wc -l )
-tail -n +2 $TMP.clust.grp | awk '$2 / '$N' >= '$SHARE | cut -f 1 | sort > $TMP.repr
-sort -cu $TMP.repr
+  N=$( $THIS/fa2list.sh $TMP.fa | wc -l )
+  tail -n +2 $TMP.clust.grp | awk '$2 / '$N' >= '$SHARE | cut -f 1 | sort > $TMP.repr
+  sort -cu $TMP.repr
 
-$THIS/filterFasta $TMP.fa -aa  -target $TMP.repr > $OUT
+  $THIS/filterFasta $TMP.fa -aa  -target $TMP.repr > $OUT
+fi
 
 
 rm $TMP*  
