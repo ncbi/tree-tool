2bc61fb6bc944f6453bc7bfd67dbab9bf6119f51
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2025-02-07 17:37:24 -0500

    PART parameter

--- genetics/tblastn.sh
+++ genetics/tblastn.sh
@@ -1,20 +1,22 @@
 #!/bin/bash --noprofile
 THIS=$( dirname $0 )
 source $THIS/../bash_common.sh
-if [ $# -ne 5 ]; then
+if [ $# -ne 6 ]; then
   echo "Run tblastn"
   echo "#1: query protein FASTA file"
   echo "#2: subject nucleotide FASTA file, may be gzip'ped"
   echo "#3: tblastn parameters (except: -query -subject -db -out)"
-  echo "#4: cores"
-  echo "#5: output file"
+  echo "#4: part_size"
+  echo "#5: cores"
+  echo "#6: output file"
   exit 1
 fi
 PROT=$1
 DNA=$2
 PARM=$3
-CORES=$4
-OUT=$5
+PART=$4  # 50000000 ??
+CORES=$5
+OUT=$6
 
 
 TMP=$( mktemp )
@@ -26,7 +28,7 @@ if [ -z ${DNA##*.gz} ]; then
   DNA=$TMP
 fi
 mkdir $TMP.dna
-$THIS/splitFasta $DNA $TMP.dna -group_size 50000000
+$THIS/splitFasta $DNA $TMP.dna -group_size $PART
 mkdir $TMP.out
 $THIS/../trav $TMP.dna  -step 1  -threads $CORES  "tblastn  -query $PROT  -subject %d/%f  $PARM  > $TMP.out/%f" 
 #tblastn  -query $PROT  -subject $DNA  -comp_based_stats 0  -evalue 1e-10  -seg no  -max_target_seqs 10000  -task tblastn-fast  -threshold 100  -window_size 15  -db_gencode 11  -outfmt '6 sseqid qseqid sstart send slen qstart qend qlen nident length'
