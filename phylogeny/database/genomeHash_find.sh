8d308e2e9877f1178464d49cd55f77430d7a12dd
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2025-03-08 19:45:56 -0500

    -noprogress

--- phylogeny/database/genomeHash_find.sh
+++ phylogeny/database/genomeHash_find.sh
@@ -43,7 +43,7 @@ while [ $N -lt $MAX ]; do
       ;;
   esac
   if [ -e $DB.$SUF ]; then
-    $THIS/../../objHash_find $DB.$SUF $PREF.hash-$SUF $MIN_COMMON $MAX  -superset $SUBSET >> $TMP.out
+    $THIS/../../objHash_find $DB.$SUF $PREF.hash-$SUF $MIN_COMMON $MAX  -superset $SUBSET  -noprogress >> $TMP.out
     sort -u $TMP.out > $TMP.out1
     mv $TMP.out1 $TMP.out
     N=$( < $TMP.out  wc -l )
