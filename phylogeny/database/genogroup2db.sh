af253b4ea68c8e0e886cd67829ba5ebb0bd892d3
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2025-03-11 16:29:51 -0400

    new

---
+++ phylogeny/database/genogroup2db.sh
@@ -0,0 +1,24 @@
+#!/bin/bash --noprofile
+THIS=$( dirname $0 )
+source $THIS/../../bash_common.sh
+if [ $# -ne 2 ]; then
+  echo "Populate GenomeTaxroot.taxroot_outlier for genonogroup outliers"
+  echo "#1: inc/"
+  echo "#2: File genogroup_table"
+  exit 1
+fi
+INC=$1
+TAB=$2
+
+
+SERVER=$( cat $INC/server )
+DATABASE=$( cat $INC/database )
+BULK_REMOTE=$( cat $INC/bulk_remote )
+TAXROOT=$( cat $INC/../taxroot )
+
+$THIS/../../bulk.sh $SERVER $INC/bulk $BULK_REMOTE $TAB $DATABASE..ListC
+
+sqsh-ms  -S $SERVER  -D $DATABASE  -L exit_failcount=1  -L bcp_rowsep="" << EOF
+  EXEC Genogroup2outliers $TAXROOT, 0
+  go -m bcp
+EOF
