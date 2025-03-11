47ff89cb774cc722f33323dcdac34d031503b2a4
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2025-03-11 16:30:03 -0400

    recursive

--- dir_diff.sh
+++ dir_diff.sh
@@ -3,8 +3,8 @@ THIS=$( dirname $0 )
 source $THIS/bash_common.sh
 if [ $# -ne 2 ]; then
 echo "Compare the files of two directories, except .-files"
-  echo "#1: directory 1"
-  echo "#2: directory 2"
+  echo "#1: file/directory 1"
+  echo "#2: file/directory 2"
   exit 1
 fi
 D1=$1
@@ -14,25 +14,34 @@ D2=$2
 TMP=$( mktemp )
 
 
-ls $D1 > $TMP.1
-ls $D2 > $TMP.2
+if [ -d $D1 ] && [ ! -d $D2 ]; then
+  warning "$D1 is a directory, but $D2 is not"
+elif [ ! -d $D1 ] && [ -d $D2 ]; then
+  warning "$D2 is a directory, but $D1 is not"
+elif [ -d $D1 ] && [ -d $D2 ]; then
+  ls $D1 > $TMP.1
+  ls $D2 > $TMP.2
 
-section "In $D1, but not in $D2"
-$THIS/setMinus $TMP.1 $TMP.2 > $TMP.1-2
-if [ -s $TMP.1-2 ]; then
-  cat $TMP.1-2
+  $THIS/setMinus $TMP.1 $TMP.2 > $TMP.1-2
+  if [ -s $TMP.1-2 ]; then
+    warning "In $D1, but not in $D2"
+    cat $TMP.1-2
+  fi
+
+  $THIS/setMinus $TMP.2 $TMP.1 > $TMP.2-1
+  if [ -s $TMP.2-1 ]; then
+    warning "In $D2, but not in $D1"
+    cat $TMP.2-1
+  fi
+
+  $THIS/setIntersect.sh $TMP.1 $TMP.2 0 > $TMP.common
+  $THIS/trav -step 1  -noprogress  $TMP.common "$THIS/dir_diff.sh $D1/%f $D2/%f"
+else
+  diff $D1 $D2 > $TMP || true
+  if [ -s $TMP ]; then
+    warning "$D1 != $D2"
+  fi
 fi
-
-section "In $D2, but not in $D1"
-$THIS/setMinus $TMP.2 $TMP.1 > $TMP.2-1
-if [ -s $TMP.2-1 ]; then
-  cat $TMP.2-1
-fi
-
-
-section "Comparing files"
-$THIS/setIntersect.sh $TMP.1 $TMP.2 0 > $TMP.common
-$THIS/trav -step 1  $TMP.common "diff $D1/%f $D2/%f || true"
 
 
 rm $TMP*
