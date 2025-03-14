4798ab045204e042368e32b2b836a336bc8ffe1e
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2025-03-14 15:03:44 -0400

    move

--- dm/mergeSimilarity.cpp
+++ dm/mergeSimilarity.cpp
@@ -49,7 +49,7 @@ namespace
 {
 
 
-struct ThisApplication : Application
+struct ThisApplication final : Application
 {
   ThisApplication () 
     : Application ("Print a " + dmSuff + "-file with merged <Similarity> attrributes of a list of " + dmSuff + "-files")
