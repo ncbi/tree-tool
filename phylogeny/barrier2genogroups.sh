6507ec9072a1c04246899c9bd475afb04aaf68fb
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2025-03-19 11:53:05 -0400

    numbers can be fractional

--- phylogeny/barrier2genogroups.sh
+++ phylogeny/barrier2genogroups.sh
@@ -1,31 +1,35 @@
 #!/bin/bash --noprofile
 THIS=$( dirname $0 )
 source $THIS/../bash_common.sh
-source $THIS/../qsub_env.sh
-COEFF=100
-if [ $# -ne 5 ]; then
+if [ $# -ne 6 ]; then
   echo "Print dependence of genogroup size on genogroup barrier"
   echo "#1: distance tree"
-  echo "#2: min. barrier * $COEFF (integer)"
-  echo "#3: max. barrier * $COEFF (integer)" 
+  echo "#2: min. barrier"
+  echo "#3: max. barrier" 
   echo "#4: step"
   echo "#5: output directory"
+  echo "#6: output statistics: dist_max num_clusters"
   exit 1
 fi
 TREE=$1
 MIN=$2
 MAX=$3
 STEP=$4
-OUT=$5
+DIR=$5
+OUT=$6
 
 
-$THIS/../grid_wait.sh 1 >> /dev/stderr
-i=$MIN
-while [ $i -le $MAX ]; do
-  T=$( echo "$i/$COEFF" | bc -l | sed 's/0*$//1' | sed 's/^\./0./1' )
-  $QSUB_5 -N j$i "$THIS/tree2genogroup $TREE $T  -genogroup_table $OUT/$T" > /dev/null
-  i=$(( i + STEP ))
+T=$MIN
+while true
+do
+  R=$( echo "$T > $MAX" | bc )
+  if [ $R == 1 ]; then
+    break
+  fi
+  printf "\r$T" > /dev/stderr
+  $THIS/tree2genogroup $TREE $T  -genogroup_table $DIR/$T > /dev/null 
+  T=$( echo "$T + $STEP" | bc -l | sed 's/^\./0./1' )
 done
-$THIS/../qstat_wait.sh 2000 1
+echo "" > /dev/stderr
 
-$THIS/../trav -noprogress $OUT 'echo "%f %gcut -f 2 %d/%f | sort -u | wc -l%g"'
+$THIS/../trav $DIR 'echo -e "%f\t%D( cut -f 2 %d/%f | sort -u | wc -l )"' > $OUT
