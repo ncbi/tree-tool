bb8350b2928a88f24d998eba9b3800e23be50a35
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2025-03-27 12:12:39 -0400

    #3: HMM|tblastn

--- phylogeny/database/genome2univs.sh
+++ phylogeny/database/genome2univs.sh
@@ -1,15 +1,20 @@
 #!/bin/bash --noprofile
 THIS=$( dirname $0 )
 source $THIS/../../bash_common.sh
-if [ $# -ne 2 ]; then
+if [ $# -ne 3 ]; then
   echo "Print: <Genome.id> <univs>"
   echo "Input: hmm-univ.list"
   echo "#1: Genome.id"
   echo "#2: large (0/1)"
+  echo "#3: suffix: HMM|tblastn"
   exit 1
 fi
 G=$1
 LARGE=$2
+SUF=$3
+
+
+sort -cu hmm-univ.list
 
 
 H=""
@@ -17,13 +22,16 @@ if [ $LARGE -eq 1 ]; then
   H=$( $THIS/../../file2hash $G )
 fi
 
-$THIS/../../check_file.sh genome/$H/$G/$G.prot-univ 1
+F=genome/$H/$G/$G.prot-univ.$SUF
+if [ ! -e $F ]; then
+  exit 0
+fi
 
 
 TMP=$( mktemp )
 
 
-grep '^>' genome/$H/$G/$G.prot-univ | sed 's/^>//1' | sed 's/ .*$//1' | sort -u > $TMP || true
+grep '^>' $F | sed 's/^>//1' | sed 's/ .*$//1' | sort -u > $TMP || true
 N=$( join  -1 1  -2 1  $TMP  hmm-univ.list | wc -l )
 echo "$G $N"
 
