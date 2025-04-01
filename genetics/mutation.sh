6e77b471540f2a74db9b6add8b729a23fc3c0f17
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2025-04-01 17:44:13 -0400

    print all mutations

--- genetics/mutation.sh
+++ genetics/mutation.sh
@@ -1,21 +1,31 @@
 #!/bin/bash --noprofile
 THIS=$( realpath $( dirname $0 ) )
 source $THIS/../bash_common.sh
-if [ $# -ne 5 ]; then
+if [ $# -ne 10 ]; then
   echo "Print indels"
   echo "Cf. PUBMED 36677521"
   echo "#1: target DNA FASTA file"
   echo "#2: target name"
   echo "#3: reference DNA FASTA file"
   echo "#4: protein annotations in #2. Line format: <protein>\t<start>\t<stop> (1-based)"
-  echo "#5: debug (0/1)"
+  echo "#5: match_score, > 0"
+	echo "#6: mismatch_score, > 0"
+	echo "#7: gap_open, >= 0"
+	echo "#8: gap_extent, > 0"
+	echo "#9: indels only (0/1)"
+  echo "#10: debug (0/1)"
   exit 1
 fi
 IN=$1
 NAME=$2
 REF=$3
 ANNOT=$4
-DEBUG=$5
+MATCH=$5
+MISMATCH=$6
+GAP_OPEN=$7
+GAP_EXTENT=$8
+INDEL=$9
+DEBUG=${10}
 
 
 TMP=$( mktemp )
@@ -43,9 +53,13 @@ if (( $( echo "$AMBIG > 0.01" | bc -l ) )); then  # PAR
 fi
 
 # Unaligned target ends are not observed, probably because all targets have already been aligned to the reference before depositing to GenBank
-$THIS/../dissim/seq2dissim $IN $REF  -mutation $TMP.mut  -ambig_start  -noambig 100  $ALIGN > $TMP.dissim
-grep '^INS' $TMP.mut >  $TMP.indel || true
-grep 'DEL$' $TMP.mut >> $TMP.indel || true
+$THIS/../dissim/seq2dissim $IN $REF  -mutation $TMP.mut  -ambig_start  -noambig 100  $ALIGN  -match_score $MATCH  -mismatch_score $MISMATCH  -gap_open $GAP_OPEN  -gap_extent $GAP_EXTENT > $TMP.dissim
+if [ $INDEL == 1 ]; then
+  grep '^INS' $TMP.mut >  $TMP.indel || true
+  grep 'DEL$' $TMP.mut >> $TMP.indel || true
+else
+  mv $TMP.mut $TMP.indel
+fi
 
 $THIS/mutation_dna2prot $TMP.indel $REF $ANNOT  -annot_overlap > $TMP.out
 
