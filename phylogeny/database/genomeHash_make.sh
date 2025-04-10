ac488393c59c1d6515f5cd2273e518fa38f68343
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2025-04-10 17:25:03 -0400

    genomeHash_add.sh

--- phylogeny/database/genomeHash_make.sh
+++ phylogeny/database/genomeHash_make.sh
@@ -2,36 +2,32 @@
 THIS=$( dirname $0 )
 source $THIS/../../bash_common.sh
 if [ $# -ne 3 ]; then
-  echo "Create GenomeHash.{CDS,PRT,HMM}"
-  echo "#1: distance tree directory"
-  echo "#2: #1/../genome is large (0/1)"
+  echo "Create GenomeHash.#3"
+  echo "#1: genome/"
+  echo "#2: #1 is large (0/1)"
   echo "#3: Suffix: HMM, PRT, CDS"
   exit 1
 fi
-INC=$1
+GENOME=$1
 LARGE=$2
 SUF=$3
-
 
 
 TMP=$( mktemp )
 
 
-GENOME=$INC/../genome
 $THIS/../../check_file.sh $GENOME 0
 
-# $TMP, H
+# $TMP
 if [ $LARGE -eq 1 ]; then
   $THIS/../../trav $GENOME "ls %d/%f" > $TMP
-  H="%h/"
 else
   ls $GENOME > $TMP
-  H=""
 fi
 wc -l $TMP
 
-$THIS/../../trav $TMP "cat $GENOME/$H%f/%f.hash-$SUF | sed 's/^/%f\t/1'" > GenomeHash.$SUF
-
+cp /dev/null GenomeHash.$SUF
+$THIS/genomeHash_add.sh $GENOME $LARGE $TMP $SUF
 
 
 rm $TMP*
