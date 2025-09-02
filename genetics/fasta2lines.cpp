047b39afe8f72bc4ae1aacac4eee335228680f64
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2025-09-02 15:24:52 -0400

    fasta2lines.cpp

---
+++ genetics/fasta2lines.cpp
@@ -0,0 +1,105 @@
+// fasta2lines.cpp
+
+/*===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE                          
+*               National Center for Biotechnology Information
+*                                                                          
+*  This software/database is a "United States Government Work" under the   
+*  terms of the United States Copyright Act.  It was written as part of    
+*  the author's official duties as a United States Government employee and 
+*  thus cannot be copyrighted.  This software/database is freely available 
+*  to the public for use. The National Library of Medicine and the U.S.    
+*  Government have not placed any restriction on its use or reproduction.  
+*                                                                          
+*  Although all reasonable efforts have been taken to ensure the accuracy  
+*  and reliability of the software and data, the NLM and the U.S.          
+*  Government do not and cannot warrant the performance or results that    
+*  may be obtained by using this software or data. The NLM and the U.S.    
+*  Government disclaim all warranties, express or implied, including       
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.                                                                
+*                                                                          
+*  Please cite the author in any work or product based on this material.   
+*
+* ===========================================================================
+*
+* Author: Vyacheslav Brover
+*
+* File Description:
+*   Print each sequence of a FASTA file as one line
+*
+*/
+
+
+#undef NDEBUG
+
+#include "../common.hpp"
+using namespace Common_sp;
+#include "seq.hpp"
+using namespace Seq_sp;
+#include "../version.inc"
+
+#include "../common.inc"
+
+
+
+namespace 
+{
+	
+	
+struct ThisApplication final : Application
+{
+  ThisApplication ()
+    : Application ("Print protein sequences which are [not] in the target list")
+	  {
+      version = VERSION;
+		  addPositional ("in",  "Input FASTA file");
+  	  addFlag ("aa", "FASTA file contains protein sequences, otherwise DNA sequences");
+	  }
+
+  
+  
+	void body () const final
+  {
+	  const string inFName = getArg ("in");
+		const bool   aa      = getFlag ("aa");
+
+	  
+		{
+		  Multifasta fa (inFName, aa);
+		  Set<string> ids;
+		  while (fa. next ())
+		  {
+		    unique_ptr<Seq> seq;
+		    if (aa)
+		    {
+		      auto pep = new Peptide (fa, 1000/*PAR*/, true);
+		      pep->pseudo = true;
+		      seq. reset (pep);
+		    }
+		    else
+		      seq. reset (new Dna (fa, 100000/*PAR*/, true));
+		    ASSERT (seq);
+		    seq->qc ();	
+		    cout << '>' << seq->name << '\n' << seq->seq << '\n';
+		  }
+		}
+  }
+};
+
+
+}  // namespace
+
+
+
+
+int main (int argc, 
+          const char* argv[])
+{
+  ThisApplication app;
+  return app. run (argc, argv);
+}
+
+
+
