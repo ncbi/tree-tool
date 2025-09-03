d97ea7f77f2030515db704d80197ad6a45ab30fa
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2025-09-03 12:27:04 -0400

    only ASCII files are compared

--- dir_diff.sh
+++ dir_diff.sh
@@ -2,7 +2,7 @@
 THIS=$( dirname $0 )
 source $THIS/bash_common.sh
 if [ $# -ne 2 ]; then
-echo "Compare the files of two directories, except .-files"
+echo "Compare the files of two directories, except .-files and binary files"
   echo "#1: file/directory 1"
   echo "#2: file/directory 2"
   exit 1
@@ -37,9 +37,17 @@ elif [ -d $D1 ] && [ -d $D2 ]; then
   $THIS/setIntersect.sh $TMP.1 $TMP.2 0 > $TMP.common
   $THIS/trav -step 1  -noprogress  $TMP.common "$THIS/dir_diff.sh $D1/%f $D2/%f"
 else
-  diff $D1 $D2 > $TMP || true
-  if [ -s $TMP ]; then
-    warning "diff $D1 $D2"
+  file -b $D1 > $TMP.1
+  file -b $D2 > $TMP.2
+  if grep -wq "ASCII" $TMP.1 && grep -wq "ASCII" $TMP.2; then
+    diff $D1 $D2 > $TMP || true
+    if [ -s $TMP ]; then
+      warning "diff $D1 $D2"
+    fi
+  elif diff $TMP.1 $TMP.2 ; then
+    :
+  else
+    warning "$D1 and $D2 have different file types"
   fi
 fi
 
