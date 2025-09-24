3e43bad857f944eba3eca5121e461d3a0e9e54b4
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2025-09-24 11:42:54 -0400

    Seq_sp::terminatorWord

--- genetics/disruption2genesymbol.cpp
+++ genetics/disruption2genesymbol.cpp
@@ -70,7 +70,6 @@ struct SymbolRaw final : Root
   //
   Strand strand {0};
   bool stop {false};
-  static constexpr const char* stopS {"STOP"};
   string rest;
   
   // Output
@@ -146,7 +145,7 @@ struct SymbolRaw final : Root
       if (alleleStop)
       {
         termLen = allele_. size ();
-        allele_ += stopS;
+        allele_ += terminatorWord;
       }
       ASSERT (! contains (allele_, '*'));
       // Standard gene symbol
@@ -157,7 +156,7 @@ struct SymbolRaw final : Root
           ASSERT (ref. size () == 1)
           ASSERT (! allele_. empty ());
           if (termLen == 0)            
-            os << stopS;
+            os << terminatorWord;
           else
             os << allele_ [0];
           os << Disruption::typeNames [type];
@@ -233,7 +232,7 @@ struct ThisApplication final : Application
   
   ThisApplication ()
     : Application ("Convert Disruption::genesymbol_raw() to standard gene symbols according to https://hgvs-nomenclature.org/stable/recommendations/protein/frameshift/.\n\
-A stop codon is '" + string (SymbolRaw::stopS) + "'.\n\
+A stop codon is '" + string (terminatorWord) + "'.\n\
 Print: <tab row> where <genesymbol> is inserted before <Disruption::genesymbol_raw()>"
 )
 	  {
