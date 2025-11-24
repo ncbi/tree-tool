d90bff5e8f63873cf55b8d12aeb6b5e784dbb721
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2025-11-24 17:04:50 -0500

    csh -> bash

--- genetics/prots2partial_extended.sh
+++ genetics/prots2partial_extended.sh
@@ -1,6 +1,6 @@
 #!/bin/bash --noprofile
 THIS=$( dirname $0 )
-source $THIS/../cpp/bash_common.sh
+source $THIS/../bash_common.sh
 if [ $# != 3 ]; then
   echo "Find partial or extended proteins"
   echo "#1: Protein file in FASTA format"
@@ -8,18 +8,21 @@ if [ $# != 3 ]; then
   echo "#3: Output: Unresolved proteins"
   exit 1
 fi
+IN=$1
+PART_EXT=$2
+UNRES=$3
 
 
-TMP=`mktemp`
+TMP=$( mktemp )
+#comment $TMP
 
 
-# Make all start codons to be 'M'
-cat $1 | sed '/^>/\!s/[LIV]/M/g' > $TMP.fa
+cp $IN $TMP.fa
 
 makeblastdb  -in $TMP.fa  -dbtype prot  -logfile /dev/null
 
 # 6 min. for 4000 proteins
-section "Blastp"
+section "blastp"
 blastp  -task blastp-fast \
   -db $TMP.fa \
   -query $TMP.fa \
@@ -31,8 +34,8 @@ cat $TMP.blastp | awk '$4 > 60 && $4/$3 > 0.95 && $5 > $8 && $8 == 1' > $TMP.par
 
 
 section "Processing"
-echo -e "#name\tdecision\tcount" > $2
-echo -e "#qseqid\tsseqid\tlength\tnident\tqstart\tqend\tqlen\tsstart\tsend\tslen" > $3
+echo -e "#name\tdecision\tcount" > $PART_EXT
+echo -e "#qseqid\tsseqid\tlength\tnident\tqstart\tqend\tqlen\tsstart\tsend\tslen" > $UNRES
 while [ -s $TMP.partial ]; do
   wc -l $TMP.partial  
 
@@ -41,36 +44,36 @@ while [ -s $TMP.partial ]; do
 	cat $TMP.1 $TMP.2 > $TMP.name
 	
 	sort $TMP.name | uniq -c | sort -nr | head -1 > $TMP.top
-	L=(`cat $TMP.top`)
+	L=( $( cat $TMP.top ) )
 	COUNT=${L[0]}
 	NAME="${L[1]}"
 	
 	if [ $COUNT == 1 ]; then
-	  cat $TMP.partial >> $3
+	  cat $TMP.partial >> $UNRES
 	  break
 	fi
 	
-	N_EXTD=`grep -cw "$NAME" $TMP.1`
-	N_PART=`grep -cw "$NAME" $TMP.2`
-	N_EXTD_2 = $(( $N_EXTD * 2 ))  # PAR
-	N_PART_2 = $(( $N_PART * 2 ))   # PAR
+	N_EXTD=$( grep -cw "$NAME" $TMP.1 ) || true
+	N_PART=$( grep -cw "$NAME" $TMP.2 ) || true
+	N_EXTD_2=$(( N_EXTD * 2 ))  # PAR
+	N_PART_2=$(( N_PART * 2 ))  # PAR
 	DECISION=""
-	if [ $N_EXTD -gt $N_PART_2) ]; then  
+	if [ $N_EXTD -gt $N_PART_2 ]; then  
 	  DECISION="extended"
 	fi
 	if [ $N_PART -gt $N_EXTD_2 ]; then
 	  DECISION="partial"
 	fi
 	if [ "$DECISION" ]; then
-	  echo "$NAME $DECISION $COUNT" >> $2
+	  echo -e "$NAME\t$DECISION\t$COUNT" >> $PART_EXT
 	else
-		grep -w "$NAME" $TMP.partial >> $3
-	endif
+		grep -w "$NAME" $TMP.partial >> $UNRES
+	fi
 
-	grep -vw "$NAME" $TMP.partial > $TMP.partial1
+	grep -vw "$NAME" $TMP.partial > $TMP.partial1 || true
 	mv $TMP.partial1 $TMP.partial
 done
 
 
-rm -f $TMP*
+rm $TMP*
 
