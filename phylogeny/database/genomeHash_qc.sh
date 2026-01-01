2f4dfe002c04ef674852fd65914b01b76f379850
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2026-01-01 12:14:38 -0500

    odd ids are printed, no error

--- phylogeny/database/genomeHash_qc.sh
+++ phylogeny/database/genomeHash_qc.sh
@@ -1,8 +1,10 @@
 #!/bin/bash --noprofile
 THIS=$( dirname $0 )
 source $THIS/../../bash_common.sh
+DB=GenomeHash
 if [ $# -ne 2 ]; then
-  echo "QC GenomeHash.*"
+  echo "QC $DB.*"
+  echo "Output: $DB.<SUF>.odd"
   echo "#1: genome/"
   echo "#2: #1 is large (0/1)"
   exit 1
@@ -30,15 +32,15 @@ sort -cu $TMP
 function qc_suf
 {
   local SUF=$1
-  if [ ! -e GenomeHash.$SUF ]; then
+  if [ ! -e $DB.$SUF ]; then
     return
   fi
-  section "GenomeHash.$SUF"
-  cut -f 1 GenomeHash.$SUF | sort -u > $TMP.hash
-  $THIS/../../setMinus $TMP $TMP.hash > $TMP.odd
-  if [ -s $TMP.odd ]; then
-    wc -l $TMP.odd
-    exit 1
+  section "$DB.$SUF"
+  cut -f 1 $DB.$SUF | sort -u > $TMP.hash
+  $THIS/../../setMinus $TMP $TMP.hash > $DB.$SUF.odd
+  wc -l $DB.$SUF.odd
+  if [ ! -s $DB.$SUF.odd ]; then
+    rm $DB.$SUF.odd
   fi
 }
 qc_suf CDS
