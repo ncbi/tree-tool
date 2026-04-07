21d35b67b46ed79638155670503f6922df93d7ee
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2026-04-07 16:39:48 -0400

    longer grid time is required

--- qsub_cmds.sh
+++ qsub_cmds.sh
@@ -5,8 +5,8 @@ N=100  # PAR
 if [ $# -ne 4 ]; then
   echo "Submit a list of commands to the grid in the batches of $N and create #3.tsv"
   echo "#1: list of commands producing one-line output"
-  echo "#2: intermediary input directory (not in /tmp, does not exist)"
-  echo "#3: output directory (not in /tmp)"
+  echo "#2: intermediary input directory (not in /tmp, to be created)"
+  echo "#3: output directory (not in /tmp, to be created)"
   echo "#4: header file for the .tsv-output"
   exit 1
 fi
@@ -17,6 +17,7 @@ H=$3
 
 
 TMP=$( mktemp )
+comment $TMP
 
 
 mkdir $IN
@@ -27,9 +28,10 @@ ls $IN > $TMP.in
 while [ -s $TMP.in ] 
 do
   $THIS/grid_wait.sh 1
-  $THIS/trav $TMP.in "$QSUB_5 -N j%f %Qsource $IN/%f > $OUT/%f%Q > /dev/null"
+  $THIS/trav $TMP.in "$QSUB_L -N j%f %Qsource $IN/%f > $OUT/%f%Q > /dev/null"
   $THIS/qstat_wait.sh 360000 1  # PAR
-  $THIS/trav $TMP.in "[ %D( < out/%f wc -l ) -eq %D( < cmd/%f wc -l ) ] || echo %f" > $TMP.in-new
+  rm -f $TMP.in-new
+  $THIS/trav $TMP.in "[ %D( < $IN/%f wc -l ) -eq %D( < $OUT/%f wc -l ) ]"  -errors $TMP.in-new
   mv $TMP.in-new $TMP.in
 done
 
@@ -40,3 +42,6 @@ rm -r $OUT/ &
 
 
 rm $TMP*
+wait
+
+
