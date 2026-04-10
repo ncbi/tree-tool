32d0d49a59acda658e28d46056c6264955fc0680
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2026-04-10 16:29:14 -0400

    fasta_sort.cpp

---
+++ genetics/fasta_sort.cpp
@@ -0,0 +1,107 @@
+// fasta_sort.cpp
+
+/*===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE                          
+*               National Center for Biotechnology Information
+*                                                                          
+*  This software/database is a "United States Government Work" under the   
+*  terms of the United States Copyright Act.  It was written as part of    
+*  the author's official duties as a United States Government employee and 
+*  thus cannot be copyrighted.  This software/database is freely available 
+*  to the public for use. The National Library of Medicine and the U.S.    
+*  Government have not placed any restriction on its use or reproduction.  
+*                                                                          
+*  Although all reasonable efforts have been taken to ensure the accuracy  
+*  and reliability of the software and data, the NLM and the U.S.          
+*  Government do not and cannot warrant the performance or results that    
+*  may be obtained by using this software or data. The NLM and the U.S.    
+*  Government disclaim all warranties, express or implied, including       
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.                                                                
+*                                                                          
+*  Please cite the author in any work or product based on this material.   
+*
+* ===========================================================================
+*
+* Author: Vyacheslav Brover
+*
+* File Description:
+*   Sort FASTA file alphabetically
+*
+*/
+
+
+#undef NDEBUG
+
+#include "../common.hpp"
+using namespace Common_sp;
+#include "seq.hpp"
+using namespace Seq_sp;
+#include "../version.inc"
+
+#include "../common.inc"
+
+
+
+namespace 
+{
+  
+  
+struct ThisApplication final : Application
+{
+  ThisApplication ()
+    : Application ("Print an apphabetically sorted FASTA file")
+    {
+      version = VERSION;
+  	  addPositional ("in", "FASTA file");
+  	  addFlag ("aa", "Protein sequence, otherwise DNA");
+    }
+
+
+	
+	void body () const final
+  {
+	  const string inFName  = getArg ("in");
+	  const bool aa         = getFlag ("aa");
+
+	  
+    VectorOwn<Seq> seqs;
+    {
+  	  Multifasta faIn (inFName, aa);
+  	  while (faIn. next ())
+  	  {
+  	  	const Seq* seq = nullptr;
+  	  	if (aa)
+  	  	  seq = new Peptide (faIn, 1024 * 1024, false);
+  	  	else
+  	  	  seq = new Dna     (faIn, 1024 * 1024, false);
+  	  	ASSERT (seq);
+  	    seq->qc ();  	    
+  	    seqs << seq;
+  	  }
+  	}
+  	seqs. sortPtr ();
+  	for (const Seq* seq : seqs)
+  	{
+  	  ASSERT (seq);
+  	  seq->saveText (cout);
+  	}
+  }
+};
+
+
+}  // namespace
+
+
+
+
+int main (int argc, 
+          const char* argv[])
+{
+  ThisApplication app;
+  return app. run (argc, argv);
+}
+
+
+
