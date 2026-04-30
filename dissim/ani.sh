bf663f37aa9a0c5b1f059266b4c4f1dba49cf274
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2026-04-30 09:46:46 -0400

    70% coverage cutoff

--- dissim/ani.sh
+++ dissim/ani.sh
@@ -16,13 +16,27 @@ TMP=$( mktemp )
 #echo $TMP 
 
 
+function len
+{
+  local F=$1
+  $THIS/../genetics/fasta2len $F -noprogress | cut -f 2 | count | grep -w "^sum" | cut -f 2
+}
+
+
+ANI="nan"
 $THIS/../genetics/dna_coverage.sh $DNA1 $DNA2 "combine" 0 100 '' 0 0 > $TMP
 M=$( tail -n +2 $TMP | awk -F '\t' '{print $6 * $8};' | count | grep -w ^sum | cut -f 2 )
 N=$( tail -n +2 $TMP | cut -f 6                       | count | grep -w ^sum | cut -f 2 )
-if [ $N -lt 1000000 ]; then  # PAR
-  ANI="nan"
-else
-  ANI=$( echo "scale=2; $M / $N" | bc )
+if [ $N -gt 1000 ]; then  # PAR
+  L1=$( len $DNA1 )
+  L2=$( len $DNA2 )
+  if [ $L1 -lt $L2 ]; then 
+    L1=$L2
+  fi
+  GOOD=$( echo "$N / $L1 >= 0.7" | bc -l )  # PAR (Barbara)
+  if [ $GOOD -eq 1 ]; then
+    ANI=$( echo "scale=2; $M / $N" | bc )
+  fi
 fi
 
 NAME1=$( basename $DNA1 )
