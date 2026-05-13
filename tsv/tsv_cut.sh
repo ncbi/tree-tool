13853301acbada29d05a216fcdfe35a710b4653b
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2026-05-13 14:15:13 -0400

    suppress empty lines

--- tsv/tsv_cut.sh
+++ tsv/tsv_cut.sh
@@ -5,7 +5,7 @@ if [ $# -ne 3 ]; then
   echo "Select rows by awk from a .tsv-file"
   echo "#1: .tsv-file name"
   echo "#2: comma-separated list of column numbers"
-  echo "#3: make rows unique (0/1)"
+  echo "#3: make rows unique (0/1) and non-empty"
   exit 1
 fi
 F=$1
@@ -16,12 +16,17 @@ UNIQ=$3
 TMP=$( mktemp )
 
 
+COL1=$( echo $C | sed 's/,.*$//1' )
+if [ $COL1 -ne 1 ]; then
+  printf "#"
+fi
+
 AWKCOL=$( echo $C | sed 's/,/,$/g' | sed 's/^/$/1' )
 
 head -1 $F    | awk -F '\t' '{OFS="\t"; print '$AWKCOL'};' 
 tail -n +2 $F | awk -F '\t' '{OFS="\t"; print '$AWKCOL'};' > $TMP
 if [ $UNIQ == 1 ]; then
-  sort -u $TMP
+  sort -u $TMP | grep -v '^\t*$'
 else
   cat $TMP
 fi
