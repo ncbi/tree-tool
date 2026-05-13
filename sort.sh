2bec230e19354094a78cc23339bf1542a0ce47ee
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2026-05-13 14:11:17 -0400

    unique sort

--- sort.sh
+++ sort.sh
@@ -2,10 +2,10 @@
 THIS=$( dirname $0 )
 source $THIS/bash_common.sh
 if [ $# -ne 1 ]; then
-  echo "#1: File to sort"
+  echo "#1: File to sort (uniquely)"
   exit 1
 fi
 
 TMP=$( mktemp )
-sort $1 > $TMP
+sort $1 -u > $TMP
 mv $TMP $1
