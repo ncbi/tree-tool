38ed01ed273e371e95fd44f2194c27738c1e38f4
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2026-06-04 14:53:17 -0400

    Progress step increased

--- tsv/tsv_check_key.cpp
+++ tsv/tsv_check_key.cpp
@@ -64,7 +64,7 @@ struct ThisApplication final : Application
 		const string keysS = getArg ("key");
 
     
-    const TextTable tab (fName, noString, true, 10000);  // PAR
+    const TextTable tab (fName, noString, true, 1000000);  // PAR
     tab. qc ();
 
     const StringVector keysVec (keysS, ',', true);
