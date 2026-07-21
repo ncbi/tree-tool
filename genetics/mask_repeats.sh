3335543084df8c0e9c65f16a1105b6eaf7f21e2e
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2026-07-21 16:53:54 -0400

    -merged_mask -mask_min THREADS

--- genetics/mask_repeats.sh
+++ genetics/mask_repeats.sh
@@ -1,16 +1,18 @@
 #!/bin/bash --noprofile
 THIS=$( dirname $0 )
 source $THIS/../bash_common.sh
-if [ $# -ne 3 ]; then
+if [ $# -ne 4 ]; then
   echo "Find and mask repeats in #1"
   echo "#1: DNA multi-FASTA"
-  echo "#2: output file with the coordinates of masked segments"
-  echo "#3: output masked #1"
+  echo "#2: threads"
+  echo "#3: output file with the coordinates of masked segments"
+  echo "#4: output masked #1"
   exit 1
 fi
 ASM=$1
-MSK=$2
-OUT=$3
+THREADS=$2
+MSK=$3
+OUT=$4
 
 
 # PAR
@@ -32,7 +34,7 @@ N=0
 while read -r F
 do
   N=$(( N + 1 ))
-  printf "\r$N"
+  printf "\r%s" "$N"
   $THIS/fragmentize $TMP.seq/$F $FRAG  -qc -noprogress > $TMP.fragmented
   $THIS/splitFasta $TMP.fragmented $TMP.frag  -len_min $FRAG  -large  -qc -noprogress
 done < $TMP.seqs
@@ -43,13 +45,15 @@ mkdir $TMP.out
 makeblastdb  -in $ASM   -dbtype nucl  -blastdb_version 4  -logfile /dev/null  -out $TMP.db
 $THIS/../trav $TMP.frag "ls %d/%f" > $TMP.frags
 wc -l $TMP.frags
-$THIS/../trav $TMP.frags -step 1 "$THIS/find_repeats_.sh $TMP.frag/%h/%f $TMP.db $HANG $TMP.out"  -threads 15  -errors $TMP.err
+$THIS/../trav $TMP.frags -step 1 "$THIS/find_repeats_.sh $TMP.frag/%h/%f $TMP.db $HANG $TMP.out"  -threads $THREADS  -errors $TMP.err
 if [ -s $TMP.err ]; then
   error "$TMP.err"
 fi
-trav $TMP.out "cat %d/%f" > $MSK
+trav $TMP.out "cat %d/%f" > $TMP.msk
 
-$THIS/dna_mask $ASM $MSK -qc > $OUT
+section "dna_mask"
+MASK_MIN=$(( FRAG - 2 * HANG ))
+$THIS/dna_mask $ASM $TMP.msk  -merged_mask $MSK  -mask_min $MASK_MIN  -qc > $OUT
 
 
 rm -r $TMP*
