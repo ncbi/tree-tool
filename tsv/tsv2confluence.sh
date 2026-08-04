24fe44d288352624a86329e941cff02c59d025f0
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2026-08-04 10:39:24 -0400

    Parameter #2

--- tsv/tsv2confluence.sh
+++ tsv/tsv2confluence.sh
@@ -1,18 +1,25 @@
 #!/bin/bash --noprofile
 THIS=$( dirname $0 )
 source $THIS/../bash_common.sh
-if [ $# -ne 1 ]; then
+if [ $# -ne 2 ]; then
   echo "#1: tsv-file"
+  echo "#2: replace underscores by spaces in the title (0/1)"
   exit 1
 fi
 F=$1
+U2S=$2
 
 
 TMP=$( mktemp )
 
 
 sed 's/|/\\|/g' $F | tr '\t' '|' | sed 's/^/|/1' | sed 's/$/|/1' | sed 's/||/| |/g' | sed 's/||/| |/g' > $TMP
-head -1 $TMP | sed 's/^|#/|/1' | sed 's/|/||/g'
+head -1 $TMP | sed 's/^|#/|/1' | sed 's/|/||/g' > $TMP.head
+if [ $U2S -eq 1 ]; then
+  sed 's/_/ /g' $TMP.head
+else
+  cat $TMP.head
+fi
 tail -n +2 $TMP
 
 
