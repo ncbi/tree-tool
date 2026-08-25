af63072df136c40572568e582329877286d6e206
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2026-08-25 12:03:06 -0400

    instruction

--- tsv/tsv_cut.sh
+++ tsv/tsv_cut.sh
@@ -2,7 +2,7 @@
 THIS=$( dirname $0 )
 source $THIS/../bash_common.sh
 if [ $# -ne 3 ]; then
-  echo "Select rows by awk from a .tsv-file"
+  echo "Select columns by awk from a .tsv-file"
   echo "#1: .tsv-file name"
   echo "#2: comma-separated list of column numbers"
   echo "#3: make rows unique (0/1) and non-empty"
