6cc15cde93a6d3a5a8849c5dbd4dae7ddef42cf3
Vyacheslav Brover <vyacheslav.brover@nih.gov>
2026-09-11 17:09:27 -0400

    final

--- effectiveSize.cpp
+++ effectiveSize.cpp
@@ -46,7 +46,7 @@ namespace
 {
 	
 	
-struct ThisApplication : Application
+struct ThisApplication final : Application
 {
   ThisApplication ()
     : Application ("Print effective number of different classes")
